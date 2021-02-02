95379b2089adb34e5d3f7a09269acdf82cd2c8d3
Jeremías <jeremias.tincan@gmail.com>
2021-02-02 18:53:14 -0300

    ecr update base docker image

--- docker/ecr-push.sh
+++ docker/ecr-push.sh
@@ -2,7 +2,8 @@
 set -eu
 IMG=${1:?'image name?'}
 
-docker build -t uws/${IMG} ./docker/${IMG}
+./docker/${IMG}/build.sh
+
 docker tag uws/${IMG} 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG}
 docker push 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG}
 
