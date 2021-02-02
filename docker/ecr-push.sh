645c90537da53c76210ed06bfb0c460c67d5216a
Jeremías <jeremias.tincan@gmail.com>
2021-02-02 19:34:33 -0300

    ecr remove local image before to tag and push

--- docker/ecr-push.sh
+++ docker/ecr-push.sh
@@ -4,6 +4,7 @@ IMG=${1:?'image name?'}
 
 ./docker/${IMG}/build.sh
 
+docker rmi 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG}
 docker tag uws/${IMG} 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG}
 docker push 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG}
 
