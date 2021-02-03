b3dcef2edcd9d7ecb54a7e1802d7137d88eb3296
Jeremías <jeremias.tincan@gmail.com>
2021-02-03 16:59:34 -0300

    munin volumes and log to stderr

--- munin/run.sh
+++ munin/run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -eu
 mkdir -vp ${PWD}/munin/files
+mkdir -vp ~/uws/munin/var ~/uws/munin/cache ~/uws/munin/log
 exec docker run -it --rm --name uws-munin \
 	--hostname munin.uws.local \
 	-v ${PWD}/munin/files:/home/uws/files \
+	-v ~/uws/munin/var:/var/lib/munin \
+	-v ~/uws/munin/cache:/var/cache/munin/www \
+	-v ~/uws/munin/log:/var/log/munin \
 	-u root uws/munin
