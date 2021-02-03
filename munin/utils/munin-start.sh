8eb2724048cd84affcbd6fa65d2e5b59af54f522
Jeremías <jeremias.tincan@gmail.com>
2021-02-03 17:10:57 -0300

    munin logs to stderr

--- munin/utils/munin-start.sh
+++ munin/utils/munin-start.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -eux
+
 /etc/init.d/munin start
 /etc/init.d/munin-node start
+
 /etc/init.d/cron start
+
+rm -vf /var/log/munin/*.log \
+	&& ln -vsf /dev/stderr /var/log/munin/munin-node.log \
+	&& ln -vsf /dev/stderr /var/log/munin/munin-update.log
+
 exec rsyslogd -n
