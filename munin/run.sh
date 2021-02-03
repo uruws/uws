026822309fe603a104a47fa617c660b763f1bcdd
Jeremías <jeremias.tincan@gmail.com>
2021-02-03 17:40:01 -0300

    munin log setup

--- munin/run.sh
+++ munin/run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -eu
 mkdir -vp ${PWD}/munin/files
-mkdir -vp ~/uws/munin/var ~/uws/munin/cache ~/uws/munin/log
+mkdir -vp ~/uws/munin/var/lib ~/uws/munin/cache/www
 exec docker run -it --rm --name uws-munin \
 	--hostname munin.uws.local \
 	-v ${PWD}/munin/files:/home/uws/files \
-	-v ~/uws/munin/var:/var/lib/munin \
-	-v ~/uws/munin/cache:/var/cache/munin/www \
-	-v ~/uws/munin/log:/var/log/munin \
+	-v ~/uws/munin/var/lib:/var/lib/munin \
+	-v ~/uws/munin/cache/www:/var/cache/munin/www \
 	-u root uws/munin
--- munin/utils/munin-start.sh
+++ munin/utils/munin-start.sh
@@ -6,8 +6,8 @@ set -eux
 
 /etc/init.d/cron start
 
-rm -vf /var/log/munin/*.log \
-	&& ln -vsf /dev/stderr /var/log/munin/munin-node.log \
-	&& ln -vsf /dev/stderr /var/log/munin/munin-update.log
+#~ rm -vf /var/log/munin/*.log \
+	#~ && ln -vsf /dev/stderr /var/log/munin/munin-node.log \
+	#~ && ln -vsf /dev/stderr /var/log/munin/munin-update.log
 
 exec rsyslogd -n
