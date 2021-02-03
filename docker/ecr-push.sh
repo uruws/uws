ab86dbdfc44d245b08f9058ff5ce9d2db9d19e37
Jeremías <jeremias.tincan@gmail.com>
2021-02-03 15:40:59 -0300

    fix ecr push

--- docker/ecr-push.sh
+++ docker/ecr-push.sh
@@ -2,9 +2,13 @@
 set -eu
 IMG=${1:?'image name?'}
 
-./docker/${IMG}/build.sh
+if test "${IMG}" = 'base'; then
+	./docker/${IMG}/build.sh
+else
+	./${IMG}/build.sh
+fi
 
-docker rmi 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG}
+docker rmi 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG} || true
 docker tag uws/${IMG} 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG}
 docker push 789470191893.dkr.ecr.us-east-1.amazonaws.com/uws:${IMG}
 
