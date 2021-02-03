70caf801917be3d2dfca061243d2fef13cd45515
Jeremías <jeremias.tincan@gmail.com>
2021-02-03 09:14:41 -0300

    aws config dir local path

--- docker/awscli/run.sh
+++ docker/awscli/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
-mkdir -vp ~/.aws
+mkdir -vp ~/.uws/aws
 docker run -it --rm --name uws-awscli --hostname awscli.uws.local -u uws \
-	-v ~/.aws:/home/uws/.aws \
+	-v ~/.uws/aws:/home/uws/.aws \
 	uws/awscli $@
 exit 0
