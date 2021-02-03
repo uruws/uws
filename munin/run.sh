35b9a0414b8887b624fd3cbab2323a28e3273120
Jeremías <jeremias.tincan@gmail.com>
2021-02-03 20:48:09 -0300

    munin fix data dirs permissions

--- munin/run.sh
+++ munin/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
 mkdir -vp ${PWD}/munin/files
-mkdir -vp ~/uws/munin/var/lib ~/uws/munin/cache/www
+mkdir -vp ~/uws/munin/var/lib ~/uws/munin/cache/www ~/uws/munin/var/log
 exec docker run -it --rm --name uws-munin \
 	--hostname munin.uws.local \
-	-v ${PWD}/munin/files:/home/uws/files \
 	-v ~/uws/munin/var/lib:/var/lib/munin \
 	-v ~/uws/munin/cache/www:/var/cache/munin/www \
+	-v ~/uws/munin/var/log:/var/log/munin \
 	-u root uws/munin
--- munin/utils/munin-start.sh
+++ munin/utils/munin-start.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 set -eux
 
+mkdir -vp /var/lib/munin/cgi-tmp
+chown -v munin:munin /var/lib/munin
+chmod -v 0755 /var/lib/munin
+
+chown -v munin:www-data /var/lib/munin/cgi-tmp
+chmod -v 0775 /var/lib/munin/cgi-tmp
+
+chown -v munin:munin /var/cache/munin/www
+chmod -v 0755 /var/cache/munin/www
+
+chown -v munin:adm /var/log/munin
+chmod -v 0755 /var/log/munin
+
+chown -v root:root /var/log/munin/munin-node.log
+chown -v munin:munin /var/log/munin/munin-update.log
+chmod -v 0644 /var/log/munin/*.log
+
 /etc/init.d/munin start
 /etc/init.d/munin-node start
 
 /etc/init.d/cron start
-
-#~ rm -vf /var/log/munin/*.log \
-	#~ && ln -vsf /dev/stderr /var/log/munin/munin-node.log \
-	#~ && ln -vsf /dev/stderr /var/log/munin/munin-update.log
 
 exec rsyslogd -n
