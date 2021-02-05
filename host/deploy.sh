5d53e3110a245b5c6695a6f317c845f1ff9843c0
Jeremías <jeremias.tincan@gmail.com>
2021-02-05 19:10:45 -0300

    cloud-init fix deps errors

--- host/deploy.sh
+++ host/deploy.sh
@@ -13,13 +13,12 @@ for fn in $(ls ./host/config/??_all_*.cfg ./host/config/??_${HOST}_*.cfg); do
 	cp -vf ${fn} ${dst}
 done
 
-echo "setup..."
 ssh -i ~/.ssh/uws-host.pem -l admin ${FQDN} 'sudo chgrp -v admin /etc/cloud/cloud.cfg.d && sudo chmod -v g+w /etc/cloud/cloud.cfg.d && sudo rm -vf /etc/cloud/cloud.cfg.d/99zzzuws_*.cfg'
 
-echo "sync..."
 rsync -vax -e 'ssh -i ~/.ssh/uws-host.pem -l admin' ${TMP}/*.cfg \
 	${FQDN}:/etc/cloud/cloud.cfg.d/
 
+sleep 1
 echo "reboot..."
 ssh -i ~/.ssh/uws-host.pem -l admin ${FQDN} 'sudo reboot'
 
