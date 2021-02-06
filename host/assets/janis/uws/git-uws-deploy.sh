d4c30ccea0d6947ea392d6c0d61cf0e653ba1440
Jeremías <jeremias.tincan@gmail.com>
2021-02-06 20:11:43 -0300

    git deploy janis repo settings

--- host/assets/janis/uws/git-uws-deploy.sh
+++ host/assets/janis/uws/git-uws-deploy.sh
@@ -2,9 +2,17 @@
 set -eu
 newrev=${1}
 umask 0027
+
 export GIT_DIR=.git
 cd /home/uws/deploy
+
 git fetch --all
 git checkout ${newrev}
-echo "hola mundo"
+
+./env/make.sh prod all
+./env/make.sh prod publish
+./env/make.sh prod prune
+
+./deploy.sh local janis
+
 exit 0
