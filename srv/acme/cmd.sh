9c428e7313e81cf5ddad5ae517c679187192ceb1
Jeremías <jeremias.tincan@gmail.com>
2021-02-09 16:52:17 -0300

    acme split run and data dirs

--- srv/acme/cmd.sh
+++ srv/acme/cmd.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -eu
 ACME_HOME=${ACME_HOME:-${HOME}/uws/acme}
+ACME_RUN=${ACME_RUN:-${HOME}/uws/acme.run}
 umask 0027
-mkdir -vp ${ACME_HOME}
+mkdir -vp ${ACME_HOME} ${ACME_RUN}
 exec docker run --rm --name uws-acme-cmd \
 	--hostname acme-cmd.uws.local \
 	-v ${ACME_HOME}:/srv/acme \
+	-v ${ACME_RUN}:/srv/run/acme \
 	-u uws uws/acme $@
--- srv/acme/utils/getcert.sh
+++ srv/acme/utils/getcert.sh
@@ -4,20 +4,20 @@ set -eu
 DOM=${1:?'domain?'}
 
 umask 0027
-mkdir -vp /srv/acme/tmp /srv/acme/cert
+mkdir -vp /srv/run/acme/tmp /srv/acme/cert
 
 AK=/srv/acme/key/account.key
-CDIR=/srv/acme/challenges/
+CDIR=/srv/run/acme/challenge/
 
 CSR=/srv/acme/req/${DOM}.csr
 CRT=/srv/acme/cert/${DOM}.crt
-TMP=/srv/acme/tmp/${DOM}.crt
+TMP=/srv/run/acme/tmp/${DOM}.crt
 
 rm -f ${TMP}
 acme-tiny --account-key ${AK} --csr ${CSR} --acme-dir ${CDIR} >${TMP} || {
 	echo "acme gen ${CRT}: failed!" >&2
 	exit 1
 }
-mv -f ${TMP} ${CRT}
 
+mv -vf ${TMP} ${CRT}
 exit 0
