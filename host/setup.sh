07f2c898290fa0bd86023cb72b46766c24e9aec2
Jeremías <jeremias.tincan@gmail.com>
2021-02-10 12:43:16 -0300

    host setup set restrictive umask

--- host/setup.sh
+++ host/setup.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 set -eu
 
-echo 'x - setup'
+echo 'i - setup'
+umask 0027
+oldwd=${PWD}
 
 echo 'x - extract assets'
 chmod -v 0750 /etc/cloud/cloud.cfg.d/99zzzuws_assets.sh
-oldwd=${PWD}
 cd /
 /etc/cloud/cloud.cfg.d/99zzzuws_assets.sh -c
 cd ${oldwd}
 
+echo 'i - run init scripts'
 if test -d /uws/init; then
 	echo 'x - run-parts /uws/init scripts'
-	chmod -v 0755 /uws/init/* || true
+	chmod -v 0755 /uws/init/*
 	run-parts --verbose --exit-on-error /uws/init
 	rm -rfv /uws/init
 fi
+cd ${oldwd}
 
 exit 0
