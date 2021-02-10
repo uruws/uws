62f7a143dd102395f3147f5781ac4cd46081f1c4
Jeremías <jeremias.tincan@gmail.com>
2021-02-10 12:53:52 -0300

    fix calling docker login script

--- host/ecr-login.sh
+++ host/ecr-login.sh
@@ -7,7 +7,7 @@ tmpfn=$(mktemp /var/tmp/uws-docker-login.XXXXXXXX)
 aws ecr get-login --region us-east-1 | sed 's/-e none //' >${tmpfn}
 
 chmod -v 0755 ${tmpfn}
-${uws} ${tmpfn}
+/bin/sh ${tmpfn}
 rm -vf ${tmpfn}
 
 exit 0
