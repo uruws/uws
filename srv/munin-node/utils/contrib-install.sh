7cc78eb0844008e08eb54c3ee785cd7a4ded476c
Jeremías <jeremias.tincan@gmail.com>
2021-02-11 20:40:52 -0300

    munin node service install contrib plugins

---
+++ srv/munin-node/utils/contrib-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eu
+rm -fr /mnt/munin/contrib
+mkdir -vp /mnt/munin/contrib
+tar -C /uws/munin/contrib -cf - . | tar -C /mnt/munin/contrib -xf -
+install -m 0755 -o root -g root /root/bin/plugin-*.sh /mnt/munin
+exit 0
