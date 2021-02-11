bbd78c856a4454c9df75b7a22bdf58f50f0f7c2f
Jeremías <jeremias.tincan@gmail.com>
2021-02-10 21:08:01 -0300

    docker mkcert devel run script

---
+++ docker/mkcert/devel.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+exec docker run -it --rm --name uws-mkcert-devel \
+	--hostname mkcert-devel.uws.local \
+	-v ${PWD}/docker/mkcert/utils:/home/uws/utils \
+	--workdir /home/uws/utils \
+	-u uws uws/mkcert /bin/bash -l
