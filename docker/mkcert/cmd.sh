77f0da39db4194395d7eb39880edf00e6f007041
Jeremías <jeremias.tincan@gmail.com>
2021-02-10 22:25:46 -0300

    version CA files every quarter of the year

--- docker/mkcert/cmd.sh
+++ docker/mkcert/cmd.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 CAROOT=${CAROOT:-${HOME}/.uws/ca}
-mkdir -vp ${CAROOT}
 exec docker run --rm --network none --name uws-mkcert \
-	--hostname uwsca.talkingpts.org \
+	--hostname ca.talkingpts.org \
 	-v ${CAROOT}:/home/uws/ca \
 	-u uws uws/mkcert $@
