46b61aafd2896e21d89758355a55aa802db6d0a9
Jeremías <jeremias.tincan@gmail.com>
2021-02-12 15:14:02 -0300

    munin patch template

--- srv/munin/devel.sh
+++ srv/munin/devel.sh
@@ -2,4 +2,8 @@
 set -eu
 exec docker run -it --rm --name uws-munin-devel \
 	--hostname munin-devel.uws.local \
-	-u root --entrypoint /bin/bash uws/munin
+	-v ${PWD}/srv/munin:/home/uws/srv/munin \
+	-u uws --entrypoint /bin/bash \
+	-e USER=uws -e HOME=/home/uws \
+	--workdir /home/uws \
+	$@ uws/munin
