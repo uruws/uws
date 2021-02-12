00a5e754904461637fe4376650bc9886ae447ed6
Jeremías <jeremias.tincan@gmail.com>
2021-02-12 19:48:19 -0300

    setup docker golang devel env

--- docker/golang/cmd.sh
+++ docker/golang/cmd.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-exec docker run --rm --name uws-golang-cmd \
+BUILD_CACHE=${PWD}/build/uws/cache
+mkdir -vp ${BUILD_CACHE}
+exec docker run --rm --network none --name uws-golang-cmd \
 	--hostname golang-cmd.uws.local -u uws \
 	-v ${PWD}/src/uws:/go/src/uws \
+	-v ${BUILD_CACHE}:/go/.cache \
 	uws/golang $@
--- docker/golang/devel.sh
+++ docker/golang/devel.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+BUILD_CACHE=${PWD}/build/uws/cache
+mkdir -vp ${BUILD_CACHE}
 exec docker run -it --rm --name uws-golang-devel \
 	--hostname golang-devel.uws.local -u uws \
 	--entrypoint /usr/local/bin/uws-login.sh \
 	-v ${PWD}/src/uws:/go/src/uws \
+	-v ${BUILD_CACHE}:/go/.cache \
 	uws/golang
