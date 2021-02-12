5c5f7704767b7abf34fcc9afb24ad81d807c10ba
Jeremías <jeremias.tincan@gmail.com>
2021-02-12 02:39:37 -0300

    fix systemctl iptables restart

--- host/assets/janis/uws/iptables/fallback.sh
+++ host/assets/janis/uws/iptables/fallback.sh
@@ -4,5 +4,5 @@ srcd=/uws/iptables
 dstd=/etc/iptables
 cat ${srcd}/rules.v4 >${dstd}/rules.v4
 cat ${srcd}/rules.v6 >${dstd}/rules.v6
-systemctl reload netfilter-persistent || /etc/init.d/netfilter-persistent flush
+systemctl restart netfilter-persistent || /etc/init.d/netfilter-persistent flush
 exit 0
