b47a9dc8fd9ee8c24054c57c39fe257b9bb53b79
Jeremías <jeremias.tincan@gmail.com>
2021-02-12 19:31:09 -0300

    gopath setup

--- docker/golang/cmd.sh
+++ docker/golang/cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 exec docker run --rm --name uws-golang-cmd \
 	--hostname golang-cmd.uws.local -u uws \
-	-v ${PWD}:/uws \
+	-v ${PWD}/src/uws:/go/src/uws \
 	uws/golang $@
--- docker/golang/devel.sh
+++ docker/golang/devel.sh
@@ -2,5 +2,5 @@
 exec docker run -it --rm --name uws-golang-devel \
 	--hostname golang-devel.uws.local -u uws \
 	--entrypoint /usr/local/bin/uws-login.sh \
-	-v ${PWD}:/uws \
+	-v ${PWD}/src/uws:/go/src/uws \
 	uws/golang
