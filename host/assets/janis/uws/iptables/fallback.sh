8f82e4aa26c09b6c2969477d7b37aafad13f4925
Jeremías <jeremias.tincan@gmail.com>
2021-02-11 21:22:53 -0300

    use systemctl util

--- host/assets/janis/uws/iptables/fallback.sh
+++ host/assets/janis/uws/iptables/fallback.sh
@@ -4,5 +4,5 @@ srcd=/uws/iptables
 dstd=/etc/iptables
 cat ${srcd}/rules.v4 >${dstd}/rules.v4
 cat ${srcd}/rules.v6 >${dstd}/rules.v6
-service netfilter-persistent reload || service netfilter-persistent flush
+systemctl reload netfilter-persistent || /etc/init.d/netfilter-persistent flush
 exit 0
