dc4045e5858fd9ae38b6b72248b1c90a0255c965
Jeremías <jeremias.tincan@gmail.com>
2021-02-18 19:16:57 -0300

    fix checking branch name

--- host/assets/jsbatch/uws/git-iss72-update.sh
+++ host/assets/jsbatch/uws/git-iss72-update.sh
@@ -3,10 +3,10 @@ set -eu
 refname="$1"
 oldrev="$2"
 newrev="$3"
-if test 'Xheads/iss72' = "X${refname}"; then
+if test 'Xrefs/heads/iss72' = "X${refname}"; then
 	export NQDIR=/home/uwsrun/nq
 	echo "i - NQ $(nq -c /uws/git-iss72-deploy.sh ${refname} ${oldrev} ${newrev})"
 else
-	echo "${refname}: invalid branch, not deploying..."
+	echo "ERROR: ${refname} - invalid branch, not deploying..."
 fi
 exit 0
