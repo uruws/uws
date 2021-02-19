e9993bcddb23670e134ab54e858a2aa1f315dd57
Jeremías <jeremias.tincan@gmail.com>
2021-02-19 13:22:05 -0300

    uwsca hostname based

--- docker/mkcert/cmd.sh
+++ docker/mkcert/cmd.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-CAROOT=${CAROOT:-${HOME}/.uws/ca}
+CAHOST="$(hostname -s)"
+CAROOT="${CAROOT:-${HOME}/.uws/ca}"
+if test 'X' = "X${CAHOST}"; then
+	CAHOST='uwsca'
+else
+	CAHOST="${CAHOST}.uwsca"
+fi
 exec docker run --rm --network none --name uws-mkcert \
-	--hostname ca.talkingpts.org \
-	-v ${CAROOT}:/home/uws/ca \
-	-u uws uws/mkcert $@
+	--hostname "${CAHOST}.talkingpts.org" \
+	-v "${CAROOT}:/home/uws/ca" \
+	-u uws uws/mkcert "$@"
