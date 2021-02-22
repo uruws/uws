39665f5c637e8e4636db93ccea1db372ad265b9a
Jeremías <jeremias@talkingpts.org>
2021-02-22 18:07:47 -0300

    uwsenv initial commit

---
+++ go/env/env.go
@@ -0,0 +1,9 @@
+// Copyright (c) Jeremías Casteglione <jeremias.tincan@gmail.com>
+// See LICENSE file.
+
+// Package env implements a config manager unsing files and env vars.
+package env
+
+func Main() {
+	println("hello world!")
+}
