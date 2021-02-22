4c5727a0ebd41512d41be0a43162f5420856c794
Jeremías <jeremias.tincan@gmail.com>
2021-02-22 17:47:10 -0300

    uwsbot initial commit

---
+++ src/uws/bot/bot.go
@@ -0,0 +1,8 @@
+// Copyright (c) Jeremías Casteglione <jeremias.tincan@gmail.com>
+// See LICENSE file.
+
+// Package bot implements a monitoring bot.
+package bot
+
+func Main() {
+}
---
+++ src/uws/cmd/uwsbot/main.go
@@ -0,0 +1,13 @@
+// Copyright (c) Jeremías Casteglione <jeremias.tincan@gmail.com>
+// See LICENSE file.
+
+// Package main.
+package main
+
+import (
+	"uws/bot"
+)
+
+func main() {
+	bot.Main()
+}
