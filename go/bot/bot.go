e766f0365337a678ad346a8c9beced9c8748c20b
Jeremías <jeremias@talkingpts.org>
2021-02-22 19:12:20 -0300

    uwsenv read a yaml file

--- go/bot/bot.go
+++ go/bot/bot.go
@@ -5,4 +5,5 @@
 package bot
 
 func Main() {
+	println("hello world!")
 }
--- go/env/env.go
+++ go/env/env.go
@@ -4,6 +4,65 @@
 // Package env implements a config manager unsing files and env vars.
 package env
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+var prefix string;
+var localPrefix string;
+
+func parseFile(fn string) {
+	blob, err := ioutil.ReadFile(fn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "uwsenv[%d] ERROR: %s\n", os.Getpid(), err.Error())
+	} else {
+		var e map[string]string
+		yaml.Unmarshal(blob, &e)
+		for k, v := range e {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+	}
+}
+
+func loadFile(fn string) {
+	fn = filepath.Clean(filepath.FromSlash(fn))
+	if fn != "." {
+		fn = filepath.Join(prefix, "etc", "env", fn)
+		localFn := filepath.Join(localPrefix, "etc", "env", fn)
+		parseFile(fn)
+		parseFile(localFn)
+	}
+}
+
+func loadEnv() {
+	name := path.Clean(strings.TrimSpace(os.Getenv("UWSENV")))
+	loadFile("default")
+	if name != "." {
+		loadFile(name)
+	}
+	loadFile("override")
+}
+
+func init() {
+	prefix = filepath.Clean(os.Getenv("UWS_PREFIX"))
+	if prefix == "." {
+		prefix = filepath.FromSlash("/uws")
+	}
+	localPrefix = filepath.Clean(os.Getenv("UWS_LOCAL_PREFIX"))
+	if localPrefix == "." {
+		localPrefix = filepath.FromSlash("/uws/local")
+	}
+	loadEnv()
+}
+
+// Main prints current env to stdout.
 func Main() {
 	println("hello world!")
 }
