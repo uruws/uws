ac2fe6ed617664481b8cfd975ca9e0abc7a31ca1
Jeremías <jeremias@talkingpts.org>
2021-02-23 00:53:21 -0300

    logger init as discard and lock when new/get

--- go/logger/logger.go
+++ go/logger/logger.go
@@ -6,26 +6,33 @@ package logger
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 )
 
 type Logger struct {
 	*log.Logger
 }
 
+var l *Logger
+var lmx *sync.Mutex
+
+func init() {
+	l = &Logger{Logger: log.New(ioutil.Discard, "", log.Ldate | log.Ltime)}
+	lmx = new(sync.Mutex)
+}
+
 func newLogger() *Logger {
 	return &Logger{Logger: log.New(os.Stderr, "", log.Ldate | log.Lmsgprefix | log.Lmicroseconds)}
 }
 
-var l *Logger
-
-func init() {
-	l = newLogger()
-}
-
 // New initializes a new Logger.
 func New(name string) *Logger {
+	lmx.Lock()
+	defer lmx.Unlock()
+	l = newLogger()
 	if os.Getenv("UWS_LOG") == "debug" {
 		l.SetFlags(log.Lmsgprefix | log.Lmicroseconds | log.Llongfile)
 	}
@@ -35,5 +42,7 @@ func New(name string) *Logger {
 
 // Get returns the current Logger instance.
 func Get() *Logger {
+	lmx.Lock()
+	defer lmx.Unlock()
 	return l
 }
