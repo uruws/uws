5ee092f522a33cbc0baefa8144c3472b8fadcdd9
Jeremías <jeremias@talkingpts.org>
2021-02-24 14:16:48 -0300

    docker golang mount src dir for development

--- docker/golang/devel.sh
+++ docker/golang/devel.sh
@@ -4,6 +4,7 @@ mkdir -vp ${BUILD_CACHE}
 exec docker run -it --rm --name uws-golang-devel \
 	--hostname go-devel.uws.local -u uws \
 	-v ${PWD}/go:/go/src/uws \
+	-v ${PWD}/src:/uws/src \
 	-v ${BUILD_CACHE}:/go/.cache \
 	--entrypoint /usr/local/bin/uws-login.sh \
 	uws/golang
