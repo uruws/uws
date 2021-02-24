939d0373003c3879c1b435e8e17be0b22c1c08d3
Jeremías <jeremias@talkingpts.org>
2021-02-24 15:52:53 -0300

    docker golang mount external repo sources

--- docker/golang/devel.sh
+++ docker/golang/devel.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+set -eu
+
 BUILD_CACHE=${PWD}/build/go-cache
 mkdir -vp ${BUILD_CACHE}
+
+SRCDIR=${UWS_SRCDIR:-'/opt/src/TalkingPts'}
+mkdir -vp ${SRCDIR}/MonitoringBots
+
 exec docker run -it --rm --name uws-golang-devel \
 	--hostname go-devel.uws.local -u uws \
 	-v ${PWD}/go:/go/src/uws \
-	-v ${PWD}/src:/uws/src \
+	-v ${SRCDIR}:/uws/src \
 	-v ${BUILD_CACHE}:/go/.cache \
 	--entrypoint /usr/local/bin/uws-login.sh \
 	uws/golang
