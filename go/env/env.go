ef65e311f998f4f648cc77e8aa40bee3f544969e
Jeremías <jeremias@talkingpts.org>
2021-02-25 23:00:59 -0300

    env use config package as parse and storage engine

--- go/env/env.go
+++ go/env/env.go
@@ -5,67 +5,31 @@
 package env
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
-	"sync"
 
-	"gopkg.in/yaml.v3"
-
+	"uws/config"
 	"uws/log"
 )
 
-var prefix string
-var localPrefix string
-var e map[string]string
-var emx *sync.Mutex
+var (
+	prefix string
+	localPrefix string
+	e *config.Config
+)
 
-func includeFiles(list string) error {
-	for _, n := range strings.Split(list, " ") {
-		if err := loadFile(n, true, false); err != nil {
-			err = fmt.Errorf("include %s", err)
-			log.Error("%s", err)
-			return err
-		}
-	}
-	return nil
-}
-
-func parseFile(fn string, incEnable bool) error {
-	blob, err := ioutil.ReadFile(fn)
-	if err != nil {
-		return err
-	}
-	p := make(map[string]string)
-	if err := yaml.Unmarshal(blob, &p); err != nil {
-		return err
-	} else {
-		for k, v := range p {
-			if k == "include" && incEnable {
-				if err := includeFiles(v); err != nil {
-					return err
-				}
-			} else {
-				e[k[:]] = v[:]
-			}
-		}
-	}
-	return nil
-}
-
-func loadFile(name string, reportError bool, incEnable bool) error {
+func loadFile(name string, reportError bool) error {
 	n := filepath.Clean(filepath.FromSlash(name))
 	if n != "." {
 		var found bool = false
 		fn := filepath.Join(prefix, "etc", "env", n)
 		localFn := filepath.Join(localPrefix, "etc", "env", n)
 		for _, fn := range []string{fn, localFn} {
-			if err := parseFile(fn, incEnable); err != nil {
+			if err := e.Load(fn); err != nil {
 				if reportError {
-					log.Debug("%v", err)
+					log.Debug("env load: %s", err)
 				}
 			} else {
 				if reportError {
@@ -75,20 +39,10 @@ func loadFile(name string, reportError bool, incEnable bool) error {
 			}
 		}
 		if reportError && !found {
-			return fmt.Errorf("could not load env: %s", name)
+			return log.NewError("could not load env: %s", name)
 		}
 	}
 	return nil
-}
-
-func loadEnv() {
-	name := path.Clean(strings.TrimSpace(os.Getenv("UWSENV")))
-	loadFile("default", false, false)
-	if name != "." {
-		loadFile(name, false, true)
-	}
-	loadVars(name)
-	loadFile("override", false, false)
 }
 
 func loadVars(name string) {
@@ -104,17 +58,26 @@ func loadVars(name string) {
 			n = strings.TrimSpace(strings.Replace(n, "UWS_", "", 1))
 			if n != "" {
 				k := "UWS_" + n
-				e[n] = os.Getenv(k)
+				e.Set(n, os.Getenv(k))
 				i += 1
 			}
 		}
 	}
-	e["ENV"] = name
+	e.Set("ENV", name)
+}
+
+func loadEnv() {
+	name := path.Clean(strings.TrimSpace(os.Getenv("UWSENV")))
+	loadFile("default", false)
+	if name != "." {
+		loadFile(name, true)
+	}
+	loadVars(name)
+	loadFile("override", false)
 }
 
 func init() {
-	e = make(map[string]string)
-	emx = new(sync.Mutex)
+	e = config.New()
 	prefix = filepath.Clean(os.Getenv("UWS_PREFIX"))
 	if prefix == "." {
 		prefix = filepath.FromSlash("/uws")
@@ -128,58 +91,38 @@ func init() {
 	loadEnv()
 }
 
-func get(keyName string) string {
-	if v, ok := os.LookupEnv("UWS_" + keyName); ok {
-		return v
-	}
-	return e[keyName]
-}
-
-func expand(v string) string {
-	return os.Expand(v, get)
-}
-
-// Set sets env key/value. No checks are done.
-func Set(key, value string) {
-	emx.Lock()
-	defer emx.Unlock()
-	e[key[:]] = value[:]
-}
-
-// Get returns the value of keyName, it returns an empty string if not set.
-func Get(keyName string) string {
-	emx.Lock()
-	defer emx.Unlock()
-	return expand(get(keyName))
+// Load searchs for envName file and loads it to current env.
+func Load(envName ...string) error {
+	n := path.Join(envName...)
+	return loadFile(n, true)
 }
 
 // List lists env keys.
 func List() []string {
-	emx.Lock()
-	defer emx.Unlock()
-	l := make([]string, 0)
-	for k := range e {
-		l = append(l, k)
-	}
-	return l
+	return e.List()
 }
 
-// Load searchs for envName file and loads it to current env.
-func Load(envName ...string) error {
-	emx.Lock()
-	defer emx.Unlock()
-	n := path.Join(envName...)
-	return loadFile(n, true, true)
+// Set sets env key/value. No checks are done.
+func Set(key, value string) {
+	e.Set(key, value)
+}
+
+// Get returns the value of keyName, it returns an empty string if not set.
+func Get(keyName string) string {
+	if v, ok := os.LookupEnv("UWS_" + keyName); ok {
+		return v
+	}
+	return e.Get(keyName)
 }
 
 // GetFilepath returns a Clean'ed and Abs if possible filepath value, if not empty.
 func GetFilepath(keyName string) string {
-	v := Get(keyName)
-	if v != "" {
-		v = filepath.Clean(v)
-		if abs, err := filepath.Abs(v); err != nil {
-			v = abs
+	if p, ok := os.LookupEnv("UWS_" + keyName); ok {
+		p = filepath.Clean(p)
+		if abs, err := filepath.Abs(p); err != nil {
+			p = abs
 		}
+		return p
 	}
-	return v
+	return e.GetFilepath(keyName)
 }
