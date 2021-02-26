1df8466d409f1c5298b689ed4bbb92a824093ea2
Jeremías <jeremias@talkingpts.org>
2021-02-25 22:19:34 -0300

    config package

---
+++ go/cmd/uwscfg/main.go
@@ -0,0 +1,36 @@
+// Copyright (c) Jeremías Casteglione <jeremias.tincan@gmail.com>
+// See LICENSE file.
+
+// Package main.
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"uws/config"
+	"uws/log"
+)
+
+func main() {
+	log.Init("uwscfg")
+	var filename string
+	flag.StringVar(&filename, "file", "", "load file `name`")
+	flag.Parse()
+
+	if filename != "" {
+		if err := config.Load(filename); err != nil {
+			log.Fatal("%s", err)
+		}
+	}
+
+	if flag.NArg() > 0 {
+		for _, getVar := range flag.Args() {
+			fmt.Printf("%s\n", config.Get(getVar))
+		}
+	} else {
+		for _, k := range config.List() {
+			fmt.Printf("%s=\"%s\"\n", k, config.Get(k))
+		}
+	}
+}
---
+++ go/config/config.go
@@ -0,0 +1,154 @@
+// Copyright (c) Jeremías Casteglione <jeremias.tincan@gmail.com>
+// See LICENSE file.
+
+// Package config implements a config files manager.
+package config
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	d  map[string]string
+	dx *sync.Mutex
+}
+
+// New creates a new Config instance.
+func New() *Config {
+	return &Config{
+		d: make(map[string]string),
+		dx: new(sync.Mutex),
+	}
+}
+
+// List lists config keys.
+func (c *Config) List() []string {
+	c.dx.Lock()
+	defer c.dx.Unlock()
+	l := make([]string, 0)
+	for k := range c.d {
+		l = append(l, k[:])
+	}
+	return l
+}
+
+// Set sets key/value. No checks are done.
+func (c *Config) Set(key, value string) {
+	c.dx.Lock()
+	defer c.dx.Unlock()
+	c.d[key[:]] = value[:]
+}
+
+func (c *Config) get(keyName string) string {
+	return c.d[keyName]
+}
+
+func (c *Config) expand(v string) string {
+	if v != "" {
+		return os.Expand(v, c.get)
+	}
+	return ""
+}
+
+// Get returns the value of keyName, it returns an empty string if not set.
+func (c *Config) Get(keyName string) string {
+	c.dx.Lock()
+	defer c.dx.Unlock()
+	return c.expand(c.d[keyName])
+}
+
+// GetFilepath returns a Clean'ed and Abs if possible filepath value, if not empty.
+func (c *Config) GetFilepath(keyName string) string {
+	v := c.Get(keyName)
+	if v != "" {
+		v = filepath.Clean(v)
+		if abs, err := filepath.Abs(v); err != nil {
+			v = abs
+		}
+	}
+	return v
+}
+
+func includeFiles(c *Config, list string) error {
+	for _, n := range strings.Split(list, " ") {
+		if err := parseFile(c, n, false); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func parseFile(c *Config, fn string, incEnable bool) error {
+	c.dx.Lock()
+	defer c.dx.Unlock()
+	blob, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return err
+	}
+	p := make(map[string]string)
+	if err := yaml.Unmarshal(blob, &p); err != nil {
+		return err
+	} else {
+		for k, v := range p {
+			if k == "include" && incEnable {
+				if err := includeFiles(c, v); err != nil {
+					return err
+				}
+			}
+			if k != "include" {
+				c.d[k[:]] = v[:]
+			}
+		}
+	}
+	return nil
+}
+
+func loadFile(c *Config, filename ...string) error {
+	n := filepath.Join(filename...)
+	return parseFile(c, n, true)
+}
+
+// Open parses filename and creates a new config instance.
+func Open(filename ...string) (*Config, error) {
+	c := New()
+	return c, loadFile(c, filename...)
+}
+
+// global config instance
+
+var cfg *Config
+
+func init() {
+	cfg = New()
+}
+
+// Load parses filename and loads it to global config.
+func Load(filename ...string) error {
+	return loadFile(cfg, filename...)
+}
+
+// List global config.
+func List() []string {
+	return cfg.List()
+}
+
+// Set to global config.
+func Set(key, value string) {
+	cfg.Set(key, value)
+}
+
+// Get from global config.
+func Get(keyName string) string {
+	return cfg.Get(keyName)
+}
+
+// GetFilepath from global config.
+func GetFilepath(keyName string) string {
+	return cfg.GetFilepath(keyName)
+}
