ed6fa9cc73066bb143cad97aca1a9e04ddd54192
Jeremías <jeremias@talkingpts.org>
2021-02-26 17:20:02 -0300

    bot stats clean field name

--- go/bot/stats/stats.go
+++ go/bot/stats/stats.go
@@ -3,3 +3,32 @@
 
 // Package stats implements bot stats generator/manager.
 package stats
+
+import (
+	"regexp"
+	"strings"
+
+	"uws/env"
+	"uws/log"
+)
+
+var (
+	statsDir string
+	fieldRe  *regexp.Regexp
+)
+
+func init() {
+	statsDir = env.GetFilepath("STATSDIR")
+	// regexp: \W not word characters (== [^0-9A-Za-z_])
+	fieldRe = regexp.MustCompile(`\W`)
+}
+
+func cleanFieldName(n ...string) string {
+	f := strings.Join(n, "_")
+	return fieldRe.ReplaceAllString(f, "_")
+}
+
+func Init(benv, bname string) {
+	f := cleanFieldName(benv, bname)
+	log.Debug("init %s %s: %s", benv, bname, f)
+}
