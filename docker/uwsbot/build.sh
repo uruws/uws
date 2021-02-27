508070af0b325f1a4153efb6fc57bab8e4234143
Jeremías <jeremias@talkingpts.org>
2021-02-27 02:05:43 -0300

    build uwsbot container config and mount bots srcdir

--- docker/uwsbot/build.sh
+++ docker/uwsbot/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -eu
+mkdir -vp ./docker/uwsbot/build
+cp -va ./go/etc/env/bot/default ./docker/uwsbot/build/env.default
 exec docker build $@ --rm -t uws/uwsbot ./docker/uwsbot
--- docker/uwsbot/run.sh
+++ docker/uwsbot/run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-exec docker run --rm --network none --name uws-uwsbot \
-	--hostname uwsbot.uws.local -u uws uws/uwsbot $@
+set -eu
+SRCDIR=${UWS_SRCDIR:-'/srv/deploy/monbots'}
+if ! test -d "${SRCDIR}"; then
+	echo "${SRCDIR}: dir not found" >&2
+	exit 1
+fi
+exec docker run --rm --name uws-uwsbot \
+	--hostname uwsbot.uws.local \
+	-v ${SRCDIR}:/uws/share/uwsbot:ro \
+	-u uws uws/uwsbot $@
