c0b373c9c210788ffdefbe8b8c42744cdb6d19ce
Jeremías <jeremias@talkingpts.org>
2021-02-27 04:46:14 -0300

    run uwsbot container app in quiet mode

--- docker/uwsbot/run.sh
+++ docker/uwsbot/run.sh
@@ -15,4 +15,5 @@ exec docker run --rm --name "uws-bot-${BOT_ENV}-${BOT_NAME}" \
 	-v "${SRCDIR}:/uws/share/uwsbot:ro" \
 	-v "${CFGDIR}:/home/uws/.config/uws/bot:ro" \
 	-v "${STATSDIR}:/uws/var/uwsbot/stats" \
+	-e "UWS_LOG=quiet" \
 	-u root uws/uwsbot -env "${BOT_ENV}" -name "${BOT_NAME}"
