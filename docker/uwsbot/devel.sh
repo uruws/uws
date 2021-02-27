d75aa45d32a008d41f0df1845c7f0dbd254d275a
Jeremías <jeremias@talkingpts.org>
2021-02-27 04:36:26 -0300

    reproduce docker issue with golang os.Rename

--- docker/uwsbot/devel.sh
+++ docker/uwsbot/devel.sh
@@ -7,7 +7,7 @@ mkdir -vp ${BUILD_CACHE}
 SRCDIR=${UWS_SRCDIR:-'/opt/src/TalkingPts'}
 mkdir -vp ${SRCDIR}/MonitoringBots
 
-mkdir -vp ${HOME}/.uws/golang
+mkdir -vp ${HOME}/.uws/golang ${PWD}/build/uwsbot/stats
 
 exec docker run -it --rm --name uwsbot-devel \
 	--hostname uwsbot-devel.uws.local \
@@ -15,6 +15,7 @@ exec docker run -it --rm --name uwsbot-devel \
 	-v ${SRCDIR}:/uws/src \
 	-v ${BUILD_CACHE}:/go/.cache \
 	-v ${HOME}/.uws/golang/bot:/home/uws/.config/uws/bot:ro \
+	-v ${PWD}/build/uwsbot/stats:/uws/var/uwsbot/stats \
 	-p 127.0.0.1:6060:6060 \
 	--entrypoint /usr/local/bin/uws-login.sh \
 	-u uws uws/golang
