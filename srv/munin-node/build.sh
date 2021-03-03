b019c4f4206e93df3ed9a7311db3698a4933d3b1
Jeremías <jeremias@talkingpts.org>
2021-03-03 18:52:31 -0300

    munin-node container install uwsbot plugin conf

--- srv/munin-node/build.sh
+++ srv/munin-node/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -eu
+mkdir -vp ./srv/munin-node/build
+cp -va ./go/etc/munin/plugin-conf.d/uwsbot ./srv/munin-node/build/uwsbot-plugin.conf
 exec docker build $@ --rm -t uws/munin-node ./srv/munin-node
