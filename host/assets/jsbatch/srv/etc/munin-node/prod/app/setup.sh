1ce0d668af976a59553b033b47ea8d67ab2cea22
Jeremías <jeremias@talkingpts.org>
2021-03-03 20:51:41 -0300

    munin-node container install uwsbot-stats under /uws

--- host/assets/jsbatch/srv/etc/munin-node/prod/app/setup.sh
+++ host/assets/jsbatch/srv/etc/munin-node/prod/app/setup.sh
@@ -5,6 +5,6 @@ pl_ena=/root/bin/plugin-enable.sh
 ${pl_ena} http_loadtime http_loadtime
 
 rm -vf /etc/munin/plugins/uwsbot_stats
-ln -svf /usr/local/bin/uwsbot-stats /etc/munin/plugins/uwsbot_stats
+ln -svf /uws/bin/uwsbot-stats /etc/munin/plugins/uwsbot_stats
 
 exit 0
