aeedbaf45af6715e4b5295e50152e90048004155
Jeremías <jeremias@talkingpts.org>
2021-03-03 18:57:20 -0300

    enable bot stats plugin for staging and prod munin-node

--- host/assets/jsbatch/srv/etc/munin-node/prod/app/setup.sh
+++ host/assets/jsbatch/srv/etc/munin-node/prod/app/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -eu
+
 pl_ena=/root/bin/plugin-enable.sh
 ${pl_ena} http_loadtime http_loadtime
+
+rm -vf /etc/munin/plugins/uwsbot_stats
+ln -svf /uws/bin/uwsbot-stats /etc/munin/plugins/uwsbot_stats
+
 exit 0
