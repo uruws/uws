2e9480eb5312c7c334ea853e6e133547f9b68594
Jeremías <jeremias@talkingpts.org>
2021-03-04 12:12:53 -0300

    install less cmd in docker golang image

--- docker/uwsbot/build.sh
+++ docker/uwsbot/build.sh
@@ -3,5 +3,4 @@ set -eu
 rm -vfr ./docker/uwsbot/build/env/bot
 mkdir -vp ./docker/uwsbot/build/env/bot
 cp -va ./go/etc/env/bot ./docker/uwsbot/build/env/bot
-exec docker build $@ --rm -t uws/uwsbot \
-	./docker/uwsbot
+exec docker build $@ --rm -t uws/uwsbot ./docker/uwsbot
