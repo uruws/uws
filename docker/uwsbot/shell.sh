f813ad788a088793fd90e9cb459e2fe615c03b0a
Jeremías <jeremias@talkingpts.org>
2021-03-09 17:14:32 -0300

    uwsbot docker shell access

---
+++ docker/uwsbot/shell.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+exec docker run -it --rm --name uws-bot-shell \
+	--hostname bot-shell.uws.local \
+	-e "UWS_LOG=quiet" \
+	-e "UWS_PREFIX=/uws" \
+	--entrypoint /bin/bash \
+	-u root uws/uwsbot
