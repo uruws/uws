3d6a1b5c3ea632f6b4aef5a0783784379da0b63c
Jeremías <jeremias@talkingpts.org>
2021-03-09 18:05:22 -0300

    improve munin-node build deps

--- srv/munin-node/build.sh
+++ srv/munin-node/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 mkdir -vp ./srv/munin-node/build
-cp -va ./go/etc/env/bot/stats ./srv/munin-node/build/uwsbot-stats.env
-cp -va ./go/etc/munin/plugin-conf.d/uwsbot ./srv/munin-node/build/uwsbot-plugin.conf
+install -C -v -m 644 ./go/etc/env/bot/stats ./srv/munin-node/build/uwsbot-stats.env
+install -C -v -m 644 ./go/etc/munin/plugin-conf.d/uwsbot ./srv/munin-node/build/uwsbot-plugin.conf
 exec docker build $@ --rm -t uws/munin-node ./srv/munin-node
