819b69cdacdccc139ffeaccad885caf606c18e33
Jeremías <jeremias@talkingpts.org>
2021-03-10 20:25:57 -0300

    load heroku api key from env var if set

--- docker/heroku/devel.sh
+++ docker/heroku/devel.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -eu
+HEROKU_API_KEY=${HEROKU_API_KEY:-''}
 exec docker run -it --rm --name uws-heroku-devel \
 	--hostname heroku-devel.uws.local -u uws \
+	-e HEROKU_API_KEY=${HEROKU_API_KEY} \
 	uws/heroku $@
