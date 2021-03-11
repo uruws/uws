a6ede17fdde88e1afc83342f9ae711a29d10f231
Jeremías <jeremias@talkingpts.org>
2021-03-11 14:02:43 -0300

    heroku devel mount download dir

--- docker/heroku/devel.sh
+++ docker/heroku/devel.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -eu
 HEROKU_API_KEY=${HEROKU_API_KEY:-''}
+userdir=${HOME}/uws/heroku
+mkdir -vp ${userdir}
 exec docker run -it --rm --name uws-heroku-devel \
 	--hostname heroku-devel.uws.local -u uws \
 	-e HEROKU_API_KEY=${HEROKU_API_KEY} \
+	-v ${userdir}:/home/uws/download \
 	uws/heroku $@
