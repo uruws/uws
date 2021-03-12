679cc6651e1d2545f90510f65e25035c613e86b1
Jeremías <jeremias@talkingpts.org>
2021-03-12 12:43:53 -0300

    fix meteor app path

--- srv/api/utils/meteor-start.sh
+++ srv/api/utils/meteor-start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eu
-if ! test -s /home/uws/api/app.env; then
+if ! test -s /home/uws/meteor/app.env; then
 	echo "api/app.env: file not found" >&2
 	exit 1
 fi
-. /home/uws/api/app.env
-cd /home/uws/api/app
+. /home/uws/meteor/app.env
+cd /home/uws/meteor/app
 exec ./.meteor/heroku_build/bin/node .meteor/heroku_build/app/main.js
