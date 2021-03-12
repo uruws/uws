1d7f4c5b6f9f4e55f684e4e9b13cafff4d3e83d1
Jeremías <jeremias@talkingpts.org>
2021-03-12 15:17:42 -0300

    api service no syslog driver and map meteor port

--- srv/api/run.sh
+++ srv/api/run.sh
@@ -5,5 +5,5 @@ mkdir -vp ${HRKDIR}
 exec docker run --rm --name uws-meteor \
 	--hostname meteor.uws.local \
 	-v "${HRKDIR}:/home/uws/meteor:ro" \
-	--log-driver syslog \
+	-p 127.0.0.1:4080:4080 \
 	-u uws uws/api
