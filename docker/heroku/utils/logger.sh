0fcb3300aae27d9733fe6c045ea328344695acb1
Jeremías <jeremias@talkingpts.org>
2021-03-17 23:45:04 -0300

    heroku logger filter web dyno messages only

--- docker/heroku/utils/logger.sh
+++ docker/heroku/utils/logger.sh
@@ -2,4 +2,4 @@
 set -eu
 env=${1:?'app env?'}
 . /home/uws/auth/heroku.env
-exec heroku logs -a "tapo${env}" -s app | grep -F ': PARSER_'
+exec heroku logs -a "tapo${env}" -s app -d web | grep -F ': PARSER_'
