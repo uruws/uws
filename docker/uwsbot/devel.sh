80900b18394799622bbf7a12e75303df219c2da0
Jeremías <jeremias@talkingpts.org>
2021-03-18 20:34:29 -0300

    uwsbot docker devel no export godoc port

--- docker/uwsbot/devel.sh
+++ docker/uwsbot/devel.sh
@@ -16,6 +16,5 @@ exec docker run -it --rm --name uwsbot-devel \
 	-v ${BUILD_CACHE}:/go/.cache \
 	-v ${HOME}/.uws/golang/bot:/home/uws/.config/uws/bot:ro \
 	-v ${PWD}/build/uwsbot/stats:/uws/var/uwsbot/stats \
-	-p 127.0.0.1:6060:6060 \
 	--entrypoint /usr/local/bin/uws-login.sh \
 	-u uws uws/golang
