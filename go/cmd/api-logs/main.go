be873aea4c0d7fdc6e5208c69768130bd1345a5c
Jeremías <jeremias@talkingpts.org>
2021-03-18 00:24:18 -0300

    api-logs cmd Filter log lines to avoid dups

--- go/cmd/api-logs/main.go
+++ go/cmd/api-logs/main.go
@@ -6,13 +6,54 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
+	"os"
 	"path/filepath"
+	"strings"
+
+	"uws/log"
 )
 
 func main() {
+	log.Init("api-logs")
 	var (
 		statedir string = filepath.FromSlash("/uws/var/api/logs")
+		logsdir  string = filepath.FromSlash("/uws/var/api/logs")
+		env      string = "default"
+		filter   string = "heroku.log"
 	)
-	flag.StringVar(&statedir, "statedir", statedir, "directory where to keep state info")
+	flag.StringVar(&statedir, "statedir", statedir, "directory `where` to keep state info")
+	flag.StringVar(&logsdir, "logsdir", logsdir, "directory `where` to read logs from")
+	flag.StringVar(&env, "env", env, "env `name`")
+	flag.StringVar(&filter, "filter", filter, "filter log `file`")
 	flag.Parse()
+
+	lastfn := filepath.Join(filepath.Clean(statedir), filepath.Clean(env) + ".last")
+	last := ""
+	if blob, err := ioutil.ReadFile(lastfn); err != nil {
+		log.Debug("read lastfn: %s", err)
+	} else {
+		last = strings.TrimSpace(string(blob))
+	}
+
+	if filter != "" {
+		Filter(last, filter, logsdir, env)
+	}
+}
+
+// Filter parses log lines from filename (stdin if - provided) and avoid duplicates from previous runs.
+func Filter(last, filename, logsdir, env string) {
+	var fh *os.File
+	if filename == "-" {
+		fh = os.Stdin
+	} else {
+		var err error
+		fn := filepath.Join(filepath.Clean(logsdir),
+			filepath.Clean(env), filepath.Clean(filename))
+		fh, err = os.Open(fn)
+		if err != nil {
+			log.Fatal("%s", err)
+		}
+		defer fh.Close()
+	}
 }
