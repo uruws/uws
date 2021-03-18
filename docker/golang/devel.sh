5b71e2e05644337a2c8cba08a5dee656df33e0d7
Jeremías <jeremias@talkingpts.org>
2021-03-18 00:00:55 -0300

    golang devel mount tmp dir

--- docker/golang/devel.sh
+++ docker/golang/devel.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -eu
 BUILD_CACHE=${PWD}/build/go-cache
-mkdir -vp ${BUILD_CACHE}
+TMPDIR=${PWD}/tmp
+mkdir -vp ${BUILD_CACHE} ${TMPDIR}
 exec docker run -it --rm --name uws-golang-devel \
 	--hostname go-devel.uws.local -u uws \
+	-v ${TMPDIR}:/go/tmp \
 	-v ${PWD}/go:/go/src/uws \
 	-v ${BUILD_CACHE}:/go/.cache \
 	-p 127.0.0.1:6060:6060 \
