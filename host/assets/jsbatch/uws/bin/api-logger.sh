e3cdd5f0bd68dcde1e45d2db8b3eb5dfbcecb354
Jeremías <jeremias@talkingpts.org>
2021-03-19 20:50:55 -0300

    api logger fix PARSER filter

--- host/assets/jsbatch/uws/bin/api-logger.sh
+++ host/assets/jsbatch/uws/bin/api-logger.sh
@@ -5,5 +5,5 @@ env=${2:?'app env?'}
 stdir=${3:-"${HOME}/uws/api/stats"}
 export UWS_LOG=quiet
 mkdir -p "${stdir}/${env}"
-exec docker logs -t --since 4m "${container}" | grep -F ': PARSER_' | sort -k1,1 -u |
+exec docker logs -t --since 4m "${container}" | grep -F ' PARSER_' | sort -k1,1 -u |
 	/uws/bin/api-logs -env "${env}" -statedir "${stdir}" -statsdir "${stdir}" -kind docker -filter -
