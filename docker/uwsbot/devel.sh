d020e6642dac5d9e6582945fed9084760ea3f586
Jeremías <jeremias@talkingpts.org>
2021-03-22 13:04:27 -0300

    docker uwsbot mount tmp dir

--- docker/uwsbot/devel.sh
+++ docker/uwsbot/devel.sh
@@ -9,9 +9,13 @@ mkdir -vp ${SRCDIR}/MonitoringBots
 
 mkdir -vp ${HOME}/.uws/golang ${PWD}/build/uwsbot/stats
 
+TMPDIR=${PWD}/tmp
+mkdir -vp ${TMPDIR}
+
 exec docker run -it --rm --name uwsbot-devel \
 	--hostname uwsbot-devel.uws.local \
 	-v ${PWD}/go:/go/src/uws \
+	-v ${TMPDIR}:/go/tmp \
 	-v ${SRCDIR}:/uws/src \
 	-v ${BUILD_CACHE}:/go/.cache \
 	-v ${HOME}/.uws/golang/bot:/home/uws/.config/uws/bot:ro \
