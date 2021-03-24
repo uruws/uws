b6feb2accb8465f178a0ccc4e510b2d5b2974f03
Jeremías <jeremias@talkingpts.org>
2021-03-24 12:09:07 -0300

    mount devel sec files

--- docker/uwsbot/devel.sh
+++ docker/uwsbot/devel.sh
@@ -7,6 +7,9 @@ mkdir -vp ${BUILD_CACHE}
 SRCDIR=${UWS_SRCDIR:-'/opt/src/TalkingPts'}
 mkdir -vp ${SRCDIR}/MonitoringBots
 
+SECDIR=${UWS_SECDIR:-"${HOME}/uws/sec/bot"}
+mkdir -vp ${SECDIR}
+
 mkdir -vp ${HOME}/.uws/golang ${PWD}/build/uwsbot/stats
 
 TMPDIR=${PWD}/tmp
@@ -17,6 +20,7 @@ exec docker run -it --rm --name uwsbot-devel \
 	-v ${PWD}/go:/go/src/uws \
 	-v ${TMPDIR}:/go/tmp \
 	-v ${SRCDIR}:/uws/src \
+	-v ${SECDIR}:/uws/etc/sec/bot \
 	-v ${BUILD_CACHE}:/go/.cache \
 	-v ${HOME}/.uws/golang/bot:/home/uws/.config/uws/bot:ro \
 	-v ${PWD}/build/uwsbot/stats:/uws/var/uwsbot/stats \
