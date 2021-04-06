d218accde6ca45730551621b7986fb28eedf5dfd
Jeremías <jeremias@talkingpts.org>
2021-04-06 15:58:16 -0300

    munin-node prod mongodb stats

--- host/assets/jsbatch/srv/etc/munin-node/prod/app/setup.sh
+++ host/assets/jsbatch/srv/etc/munin-node/prod/app/setup.sh
@@ -4,11 +4,11 @@ set -eu
 pl_ena=/root/bin/plugin-enable.sh
 ${pl_ena} http_loadtime http_loadtime
 
-#~ ${pl_ena} contrib mongodb/mongodb_conn mongodb_conn
-#~ ${pl_ena} contrib mongodb/mongodb_docs mongodb_docs
-#~ ${pl_ena} contrib mongodb/mongo_lag mongo_lag
-#~ ${pl_ena} contrib mongodb/mongo_mem mongo_mem
-#~ ${pl_ena} contrib mongodb/mongo_ops mongo_ops
+${pl_ena} contrib mongodb/mongodb_conn mongodb_conn
+${pl_ena} contrib mongodb/mongodb_docs mongodb_docs
+${pl_ena} contrib mongodb/mongo_lag mongo_lag
+${pl_ena} contrib mongodb/mongo_mem mongo_mem
+${pl_ena} contrib mongodb/mongo_ops mongo_ops
 
 rm -vf /etc/munin/plugins/uwsbot_stats
 ln -svf /uws/bin/uwsbot-stats /etc/munin/plugins/uwsbot_stats
