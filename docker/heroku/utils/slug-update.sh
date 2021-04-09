dfead5516647be17400f6e286cf6cb0ebb436224
Jeremías <jeremias@talkingpts.org>
2021-04-09 20:22:52 -0300

    heroku export app env vars

--- docker/heroku/utils/slug-update.sh
+++ docker/heroku/utils/slug-update.sh
@@ -2,11 +2,13 @@
 set -eu
 echo "SLUG UPDATE: $@"
 APP=${1:?'app name?'}
+
 cd /home/uws/download
-heroku config -s -a ${APP} >${APP}.env
+
 heroku slugs -a ${APP} | head >${APP}-slugs.head
 cat ${APP}-slugs.head | grep -E '^v[[:digit:]]+: ' |
 	head -n1 | cut -d ' ' -f 2 >${APP}.new
+
 if test -s ./${APP}.cur; then
 	slug_cur=$(cat ${APP}.cur)
 	slug_new=$(cat ${APP}.new)
@@ -15,7 +17,13 @@ if test -s ./${APP}.cur; then
 		exit 0
 	fi
 fi
+
+heroku config -s -a ${APP} >${APP}.env.new
+cat ${APP}.env.new | sed 's#^#export #' >${APP}.env
+rm -vf ${APP}.env.new
+
 echo "${APP}: update slug ${slug_new}"
 heroku slugs:download -a "${APP}" "${slug_new}"
 rm -vf ${APP}/slug.tar.gz
+
 exit 0
