337838fbe160662960e90e69dc9a20b69bab215c
Jeremías <jeremias@talkingpts.org>
2021-04-15 21:04:06 -0300

    heroku slug-update fixing first time run

--- docker/heroku/utils/slug-update.sh
+++ docker/heroku/utils/slug-update.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 set -eu
-echo "SLUG UPDATE: $@"
 APP=${1:?'app name?'}
 
-cd /home/uws/download
+cd "${HOME}/download"
 
-heroku slugs -a ${APP} | head >${APP}-slugs.head
-cat ${APP}-slugs.head | grep -E '^v[[:digit:]]+: ' |
-	head -n1 | cut -d ' ' -f 2 >${APP}.new
+heroku slugs -a "${APP}" | head >"${APP}-slugs.head"
+cat "${APP}-slugs.head" | grep -E '^v[[:digit:]]+: ' |
+	head -n1 | cut -d ' ' -f 2 >"${APP}.new"
 
-slug_new=''
-if test -s ./${APP}.cur; then
-	slug_cur=$(cat ${APP}.cur)
-	slug_new=$(cat ${APP}.new)
-	if test "X${slug_new}" = "X${slug_cur}"; then
-		echo "${APP}: no new slug..."
-		exit 0
-	fi
+if ! test -s "./${APP}.cur"; then
+	echo 'NEW' >"./${APP}.cur"
 fi
 
-heroku config -s -a ${APP} >${APP}.env.new
-cat ${APP}.env.new | sed 's#^#export #' >${APP}.env
-rm -vf ${APP}.env.new
+slug_cur=$(cat "${APP}.cur")
+slug_new=$(cat "${APP}.new")
+if test "X${slug_new}" = "X${slug_cur}"; then
+	echo "${APP}: no new slug..."
+	exit 0
+fi
+
+heroku config -s -a "${APP}" >"${APP}.env.new"
+< "${APP}.env.new" sed 's#^#export #' >"${APP}.env"
+rm -vf "${APP}.env.new"
 
 echo "${APP}: update slug ${slug_new}"
 heroku slugs:download -a "${APP}" "${slug_new}"
-rm -vf ${APP}/slug.tar.gz
+rm -vf "${APP}/slug.tar.gz"
+
+echo "${slug_new}" >"./${APP}.cur"
 
 exit 0
