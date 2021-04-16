54b56b41aa4db0e322604798becfeca8d805167f
Jeremías <jeremias@talkingpts.org>
2021-04-16 19:22:06 -0300

    install awscli and eksctl util

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -eu
 #HEROKU_API_KEY=${HEROKU_API_KEY:-''}
-#userdir=${HOME}/uws/heroku
-#mkdir -vp ${userdir}
+awsdir=${HOME}/.uws/eks/aws
+kubedir=${HOME}/.uws/eks/kube
+mkdir -vp ${awsdir} ${kubedir}
 exec docker run -it --rm --name uws-eks-devel \
 	--hostname eks-devel.uws.local -u uws \
+	-v ${awsdir}:/home/uws/.aws:ro \
 	uws/eks $@
