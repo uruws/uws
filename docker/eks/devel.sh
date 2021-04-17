238d7765b75ad2c6231b2bcd0d1b926b858d8dfb
Jeremías <jeremias@talkingpts.org>
2021-04-16 21:55:59 -0300

    docker eks helper utils

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -eu
-#HEROKU_API_KEY=${HEROKU_API_KEY:-''}
 awsdir=${HOME}/.uws/eks/aws
 kubedir=${HOME}/.uws/eks/kube
 mkdir -vp ${awsdir} ${kubedir}
+utils=${PWD}/docker/eks/utils
+test -d ${utils} || {
+	echo "${utils}: directory not found" >&2
+	exit 1
+}
 exec docker run -it --rm --name uws-eks-devel \
 	--hostname eks-devel.uws.local -u uws \
+	-v ${utils}:/home/uws/bin:ro \
 	-v ${awsdir}:/home/uws/.aws:ro \
 	-v ${kubedir}:/home/uws/.kube \
 	uws/eks $@
