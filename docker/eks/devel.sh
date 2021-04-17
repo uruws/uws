5e6d9e827064b04f6ac73b9d6e80a4d973c3f437
Jeremías <jeremias@talkingpts.org>
2021-04-17 04:33:53 -0300

    cluster setup aws auth

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -4,6 +4,7 @@ awsdir=${HOME}/.uws/eks/aws
 kubedir=${HOME}/.uws/eks/kube
 mkdir -vp ${awsdir} ${kubedir}
 utils=${PWD}/docker/eks/utils
+files=${PWD}/secret/eks/files
 test -d ${utils} || {
 	echo "${utils}: directory not found" >&2
 	exit 1
@@ -11,6 +12,7 @@ test -d ${utils} || {
 exec docker run -it --rm --name uws-eks-devel \
 	--hostname eks-devel.uws.local -u uws \
 	-v ${utils}:/home/uws/bin:ro \
+	-v ${files}:/home/uws/files:ro \
 	-v ${awsdir}:/home/uws/.aws:ro \
 	-v ${kubedir}:/home/uws/.kube \
 	uws/eks $@
