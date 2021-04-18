840a5107e50645a0b286fee7dcb62c8962ac5b7c
Jeremías <jeremias@talkingpts.org>
2021-04-18 02:28:08 -0300

    cluster setup k8s dashboard

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -6,6 +6,7 @@ utils=${PWD}/docker/eks/utils
 files=${PWD}/secret/eks/files
 exec docker run -it --rm \
 	--hostname eks-devel.uws.local -u uws \
+	-p 127.0.0.1:8001:8001 \
 	-v ${utils}:/home/uws/bin:ro \
 	-v ${files}:/home/uws/files:ro \
 	-v ${awsdir}:/home/uws/.aws:ro \
