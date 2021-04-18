333faca8191fcc857a5416b6bb342da1278675d0
Jeremías <jeremias@talkingpts.org>
2021-04-18 03:21:37 -0300

    cluster setup prometheus server

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -7,6 +7,9 @@ files=${PWD}/secret/eks/files
 exec docker run -it --rm \
 	--hostname eks-devel.uws.local -u uws \
 	-p 127.0.0.1:8001:8001 \
+	-p 127.0.0.1:9090:9090 \
+	-p 127.0.0.1:9091:9091 \
+	-p 127.0.0.1:9093:9093 \
 	-v ${utils}:/home/uws/bin:ro \
 	-v ${files}:/home/uws/files:ro \
 	-v ${awsdir}:/home/uws/.aws:ro \
