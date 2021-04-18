a9f7d1761c506eb02c5ccf588408f16ca374e77c
Jeremías <jeremias@talkingpts.org>
2021-04-18 05:17:10 -0300

    cluster use default profile

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -2,8 +2,9 @@
 set -eu
 awsdir=${PWD}/secret/eks/aws
 kubedir=${PWD}/secret/eks/kube
-utils=${PWD}/docker/eks/utils
 files=${PWD}/secret/eks/files
+utils=${PWD}/docker/eks/utils
+k8s=${PWD}/k8s
 exec docker run -it --rm \
 	--hostname eks-devel.uws.local -u uws \
 	-p 127.0.0.1:8001:8001 \
@@ -11,6 +12,7 @@ exec docker run -it --rm \
 	-p 127.0.0.1:9091:9091 \
 	-p 127.0.0.1:9093:9093 \
 	-v ${utils}:/home/uws/bin:ro \
+	-v ${k8s}:/home/uws/k8s:ro \
 	-v ${files}:/home/uws/files:ro \
 	-v ${awsdir}:/home/uws/.aws:ro \
 	-v ${kubedir}/clusters:/home/uws/.kube/eksctl/clusters \
