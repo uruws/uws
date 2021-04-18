3437db9a90d7272cb07a55be0290763737d6ad02
Jeremías <jeremias@talkingpts.org>
2021-04-18 01:19:59 -0300

    do not mix managed and fargate nodes

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -4,7 +4,7 @@ awsdir=${PWD}/secret/eks/aws
 kubedir=${PWD}/secret/eks/kube
 utils=${PWD}/docker/eks/utils
 files=${PWD}/secret/eks/files
-exec docker run -it --rm --name uws-eks-devel \
+exec docker run -it --rm \
 	--hostname eks-devel.uws.local -u uws \
 	-v ${utils}:/home/uws/bin:ro \
 	-v ${files}:/home/uws/files:ro \
