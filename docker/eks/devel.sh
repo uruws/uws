2054621d5c110f7e0e3b2f7ac897d30071eb7abf
Jeremías <jeremias@talkingpts.org>
2021-04-19 17:44:56 -0300

    do not read-only eks docker

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -7,15 +7,13 @@ files=${PWD}/docker/eks/files
 utils=${PWD}/docker/eks/utils
 k8s=${PWD}/k8s
 cluster=${PWD}/cluster
-exec docker run -it --rm --read-only \
+exec docker run -it --rm \
 	--hostname eks-devel.uws.local -u uws \
 	-p 127.0.0.1:0:3000 \
 	-p 127.0.0.1:0:8001 \
 	-p 127.0.0.1:0:9090 \
 	-p 127.0.0.1:0:9091 \
 	-p 127.0.0.1:0:9093 \
-	--tmpfs /home/uws/tmp:rw,size=100m,mode=1777 \
-	--tmpfs /home/uws/.kube/cache:rw,size=10m,mode=1777 \
 	-v ${utils}:/home/uws/bin:ro \
 	-v ${k8s}:/home/uws/k8s:ro \
 	-v ${cluster}:/home/uws/cluster:ro \
