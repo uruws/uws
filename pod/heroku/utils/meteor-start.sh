191a6ecddc6920dc52e03d26a37ea69da6cd46c3
Jeremías <jeremias@talkingpts.org>
2021-04-20 18:38:49 -0300

    heroku pod new version

--- pod/heroku/utils/meteor-start.sh
+++ pod/heroku/utils/meteor-start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-echo "--- DEBUG"
+echo "--- DEBUG start"
 echo "--- user"
 id -a
 echo "--- env"
@@ -10,11 +10,14 @@ ls -lh /home/uws/meteor
 ls -lh /home/uws/meteor/app
 echo "--- mount"
 (mount | grep /home/uws/meteor/app) || true
-echo "--- DEBUG"
+echo "--- app.env"
 if ! test -s /run/app.env; then
 	echo "/run/app.env: file not found" >&2
 	exit 1
 fi
+ls -lh /run/app.env
+tail /run/app.env
+echo "--- DEBUG end"
 . /run/app.env
 cd /home/uws/meteor/app
 exec ./.meteor/heroku_build/bin/node ./.meteor/heroku_build/app/main.js
