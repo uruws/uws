36014e41b652e9f15f6135bd6c1ad4c00684ede8
Jeremías <jeremias@talkingpts.org>
2021-04-20 16:18:49 -0300

    meteor-start app.env path

--- pod/heroku/utils/meteor-start.sh
+++ pod/heroku/utils/meteor-start.sh
@@ -11,10 +11,10 @@ ls -lh /home/uws/meteor/app
 echo "--- mount"
 (mount | grep /home/uws/meteor/app) || true
 echo "--- DEBUG"
-if ! test -s /home/uws/meteor/app.env; then
-	echo "api/app.env: file not found" >&2
+if ! test -s /run/app.env; then
+	echo "/run/app.env: file not found" >&2
 	exit 1
 fi
-. /home/uws/meteor/app.env
+. /run/app.env
 cd /home/uws/meteor/app
-exec ./.meteor/heroku_build/bin/node .meteor/heroku_build/app/main.js
+exec ./.meteor/heroku_build/bin/node ./.meteor/heroku_build/app/main.js
