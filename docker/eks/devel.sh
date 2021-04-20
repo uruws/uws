e981ac95a3346bb850480f0243bc111bfcd9370f
Jeremías <jeremias@talkingpts.org>
2021-04-20 12:23:17 -0300

    initial pod heroku deploy

--- docker/eks/devel.sh
+++ docker/eks/devel.sh
@@ -6,6 +6,7 @@ secrets=${PWD}/secret/eks/files
 files=${PWD}/docker/eks/files
 utils=${PWD}/docker/eks/utils
 k8s=${PWD}/k8s
+pod=${PWD}/pod
 cluster=${PWD}/cluster
 exec docker run -it --rm \
 	--hostname eks-devel.uws.local -u uws \
@@ -16,6 +17,7 @@ exec docker run -it --rm \
 	-p 127.0.0.1:0:9093 \
 	-v ${utils}:/home/uws/bin:ro \
 	-v ${k8s}:/home/uws/k8s:ro \
+	-v ${pod}:/home/uws/pod:ro \
 	-v ${cluster}:/home/uws/cluster:ro \
 	-v ${files}:/home/uws/files:ro \
 	-v ${secrets}:/home/uws/secret:ro \
