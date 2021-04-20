0b56f671207af63117fdfa8a611d43f6fdf52904
Jeremías <jeremias@talkingpts.org>
2021-04-19 23:06:21 -0300

    ecr login multiple regions

--- docker/ecr-login.sh
+++ docker/ecr-login.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -eu
+
+region=${1:-'us-east-1'}
 aws=./docker/awscli/run.sh
 
-${aws} ecr get-login-password --region us-east-1 |
-	docker login --username AWS --password-stdin 789470191893.dkr.ecr.us-east-1.amazonaws.com
+${aws} ecr get-login-password --region ${region} |
+	docker login --username AWS --password-stdin 789470191893.dkr.ecr.${region}.amazonaws.com
 
 exit 0
