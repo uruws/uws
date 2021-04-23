ea76ad022b65da17285f2e5e90d3254d353f90a6
Jeremías <jeremias@talkingpts.org>
2021-04-23 18:13:11 -0300

    heroku logs filter meteor service

--- pod/heroku/http-logs.sh
+++ pod/heroku/http-logs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
-./pod/heroku/logs.sh -n ingress-nginx service/ingress-nginx-controller --prefix=false $@
+./pod/heroku/logs.sh -n ingress-nginx service/ingress-nginx-controller --prefix=false $@ |
+	fgrep heroku-meteor-service
 exit 0
