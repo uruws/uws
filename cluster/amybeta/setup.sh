4dd061922fea90748f3126b3a9fc0f7295ab2bfb
Jeremías <jeremias@talkingpts.org>
2021-05-07 19:20:49 -0300

    new amybeta cluster

--- cluster/amybeta/setup.sh
+++ cluster/amybeta/setup.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
 set -eu
 
-kubectl='kubectl --kubeconfig=/home/uws/.kube/eksctl/clusters/amybeta'
-k8s=/home/uws/k8s
-secret=/home/uws/secret
 cluster=/home/uws/cluster/amybeta
 
 set -x
 
-uwseks-cluster-setup amybeta
+uwskube create secret generic basic-auth --from-file=auth=${HOME}/secret/auth
+uwskube get secret basic-auth -o yaml
 
-uwseks-cluster-setup-dashboard amybeta
-uwseks-cluster-setup-metrics-server amybeta
-uwseks-cluster-setup-cert-manager amybeta
-uwseks-cluster-setup-nginx-ingress amybeta
-
-${kubectl} apply -f ${k8s}/acme-prod.yaml
-${kubectl} apply -f ${cluster}/certificates.yaml
-
-${kubectl} create secret generic basic-auth --from-file=auth=${secret}/auth
-${kubectl} get secret basic-auth -o yaml
-${kubectl} apply -f ${cluster}/gateway.yaml
+uwskube apply -f ${cluster}/gateway.yaml
 
 exit 0
