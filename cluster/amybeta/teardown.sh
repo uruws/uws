331cf817a1382ddba5bfb13a79450ba41ced865a
Jeremías <jeremias@talkingpts.org>
2021-05-07 20:05:03 -0300

    amybeta cluster teardown

--- cluster/amybeta/teardown.sh
+++ cluster/amybeta/teardown.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 set -eu
-
-kubectl='kubectl --kubeconfig=/home/uws/.kube/eksctl/clusters/amybeta'
-k8s=/home/uws/k8s
+pod=/home/uws/pod/heroku
 cluster=/home/uws/cluster/amybeta
 
-set -x
+uwskube delete -f ${cluster}/meteor-gateway.yaml
+uwskube delete -f ${cluster}/meteor-scaler.yaml
 
-# FIXME: teardown of cluster gateway removes AWS load balancer
-${kubectl} delete secret basic-auth
-${kubectl} delete -f ${cluster}/gateway.yaml
-
-${kubectl} delete -f ${cluster}/certificates.yaml
-${kubectl} delete -f ${k8s}/acme.yaml
-
-# FIXME: teardown nginx-ingress fails
-uwseks-cluster-teardown-nginx-ingress amybeta
-
-uwseks-cluster-teardown-cert-manager amybeta
-uwseks-cluster-teardown-metrics-server amybeta
-uwseks-cluster-teardown-dashboard amybeta
-
-uwseks-cluster-teardown amybeta
+${pod}/teardown.sh
 exit 0
