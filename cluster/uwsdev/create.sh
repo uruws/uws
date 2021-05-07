09c7255d63860da06055c02a924933423b63513e
Jeremías <jeremias@talkingpts.org>
2021-05-07 14:47:49 -0300

    delete uwsdev cluster

--- cluster/uwsdev/create.sh
+++ cluster/uwsdev/create.sh
@@ -5,11 +5,10 @@ set -eu
 nodes=2
 nodes_min=2
 nodes_max=10
-instance_types='t3a.small'
 
 uwseks-cluster-create --profile ${AWS_PROFILE} --region ${AWS_REGION} \
 	--nodes ${nodes} --nodes-min ${nodes_min} --nodes-max ${nodes_max} \
-	--instance-types "${instance_types}" \
+	--instance-types "${AWS_INSTANCE_TYPES}" \
 	${UWS_CLUSTER}
 
 set -x
