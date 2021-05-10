4c701ee79732ac03055fe41c869af699e8e06ac8
Jeremías <jeremias@talkingpts.org>
2021-05-10 17:51:27 -0300

    fix metrics-server setup/teardown

--- k8s/metrics-server/setup.sh
+++ k8s/metrics-server/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -eu
-. ~/bin/env.export
 uwskube apply -f ~/k8s/metrics-server/deploy.yaml
 exit 0
--- k8s/metrics-server/teardown.sh
+++ k8s/metrics-server/teardown.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 set -eu
-
-. ~/bin/env.export
-
-cluster=${UWS_CLUSTER}
-files=~/files
-kubectl="kubectl --kubeconfig=~/.kube/eksctl/clusters/${cluster}"
-
-${kubectl} delete -f ${files}/metrics-server.yaml
-
+uwskube delete -f ~/k8s/metrics-server/deploy.yaml
 exit 0
