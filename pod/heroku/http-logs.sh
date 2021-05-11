e370ec2a9c2e42d1cc7126de846743d776022ef6
Jeremías <jeremias@talkingpts.org>
2021-05-11 18:57:02 -0300

    remove old heroku meteor deployment

--- pod/heroku/http-logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-./pod/heroku/logs.sh -n ingress-nginx service/ingress-nginx-controller --prefix=false $@ |
-	fgrep heroku-meteor-service
-exit 0
--- pod/heroku/teardown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-pod=/home/uws/pod/heroku
-uwskube delete -f ${pod}/deploy.yaml --wait
-uwskube delete secret heroku-meteor-app-env
-exit 0
