b70760eb6fa7f6fe8f768b9ca8386ae074da98d2
Jeremías <jeremias@talkingpts.org>
2021-05-12 00:08:28 -0300

    fix deploy meteor beta

--- pod/meteor/beta/rollin.sh
+++ pod/meteor/beta/rollin.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -eu
 pod=/home/uws/pod/meteor/beta
-uwskube delete secret -n meteor-beta meteor-beta-env
 uwskube delete -n meteor-beta -f ${pod}/deploy.yaml
 exit 0
--- pod/meteor/beta/teardown.sh
+++ pod/meteor/beta/teardown.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
+uwskube delete secret -n meteor-beta meteor-beta-env || true
 uwskube delete namespace meteor-beta
 exit 0
