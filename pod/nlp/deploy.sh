cb16a4090cd7b5791f75b04af883869c6b103bad
Jeremías <jeremias@talkingpts.org>
2021-05-21 19:02:48 -0300

    nlp deploy split services

--- pod/nlp/deploy.sh
+++ pod/nlp/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -eu
 pod=/home/uws/pod/nlp
-uwskube apply -f ${pod}/deploy.yaml
+uwskube apply -f ${pod}/api/deploy.yaml
+uwskube apply -f ${pod}/ner/deploy.yaml
+uwskube apply -f ${pod}/sentiment/deploy.yaml
 exit 0
