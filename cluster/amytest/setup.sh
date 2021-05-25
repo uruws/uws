b395375a0e66e34894147c298eb56158b3570d73
Jeremías <jeremias@talkingpts.org>
2021-05-25 18:43:59 -0300

    amytest setup

--- cluster/amytest/setup.sh
+++ cluster/amytest/setup.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -eu
-
 cluster=/home/uws/cluster/amytest
-
-#~ uwskube create secret generic basic-auth --from-file=auth=${HOME}/secret/auth
-#~ uwskube get secret basic-auth -o yaml
-
+uwskube create secret generic basic-auth --from-file=auth=${HOME}/secret/auth
 uwskube apply -f ${cluster}/gateway.yaml
-
 exit 0
