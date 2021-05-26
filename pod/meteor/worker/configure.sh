7793241a8ba0f5ed25929a7b7d48dd7d1916d1c4
Jeremías <jeremias@talkingpts.org>
2021-05-26 18:16:28 -0300

    deploy meteor worker 2.1.9 - nlp client

--- pod/meteor/worker/configure.sh
+++ pod/meteor/worker/configure.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -eu
+
 appenv=${1:?'app.env?'}
+
 uwskube delete secret -n meteor-worker meteor-worker-env || true
-uwskube create secret generic -n meteor-worker meteor-worker-env --from-file="app.env=${appenv}"
+uwskube create secret generic -n meteor-worker meteor-worker-env \
+	--from-file="app.env=${appenv}"
+
+crtfn=${HOME}/ca/cert/nlp/meteor-worker.crt
+keyfn=${HOME}/ca/cert/nlp/meteor-worker.key
+
+uwskube delete secret -n meteor-worker nlp-client-cert || true
+uwskube create secret generic -n meteor-worker nlp-client-cert \
+	--from-file="tls.crt=${crtfn}" --from-file="tls.key=${keyfn}"
+
 exit 0
