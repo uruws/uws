be03822997337decc91560170d3aa6f1467dcab6
Jeremías <jeremias@talkingpts.org>
2021-05-31 21:08:13 -0300

    fix k8s dashboard setup/teardown

--- k8s/dashboard/teardown.sh
+++ k8s/dashboard/teardown.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 set -eu
-
-. ~/bin/env.export
-
-cluster=${UWS_CLUSTER}
-files=~/files
-kubectl="kubectl --kubeconfig=~/.kube/eksctl/clusters/${cluster}"
-
-# FIXME: teardown k8s-dashboard fails
-${kubectl} delete -f ${files}/k8s-dashboard.yaml
-
-${kubectl} delete -f ${files}/k8s-dashboard-auth.yaml
-
+uwskube delete -f ~/k8s/dashboard/auth.yaml
+uwskube delete -f ~/k8s/dashboard/deploy.yaml
 exit 0
