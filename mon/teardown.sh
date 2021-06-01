dc08c7a7094c54c994d584721a2ab614a96e76f0
Jeremías <jeremias@talkingpts.org>
2021-05-31 22:31:08 -0300

    mon deploy grafana

--- mon/teardown.sh
+++ mon/teardown.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -eu
+uwskube delete configmap provision-datasources -n mon
+uwskube delete configmap provision-dashboards -n mon
+uwskube delete configmap dashboard-uws -n mon
+uwskube delete configmap grafcfg -n mon
 uwskube delete configmap promcfg -n mon
 uwskube delete namespace mon
 exit 0
