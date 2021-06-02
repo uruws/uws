67af2050017a086b738ae842678f7663d8061fa0
Jeremías <jeremias@talkingpts.org>
2021-06-02 13:14:35 -0300

    amy new mon dashboard

--- cluster/amy/setup.sh
+++ cluster/amy/setup.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 set -eu
-
 cluster=/home/uws/cluster/amy
-
-set -x
-
-uwskube create secret generic basic-auth --from-file=auth=${HOME}/secret/auth
-uwskube get secret basic-auth -o yaml
-
 uwskube apply -f ${cluster}/gateway.yaml
-
 exit 0
