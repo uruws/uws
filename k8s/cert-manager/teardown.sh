2a6436bc7c08cc13ea241463f1a1328a5820230f
Jeremías <jeremias@talkingpts.org>
2021-06-09 14:32:35 -0300

    fix cert-manager teardown

--- k8s/cert-manager/teardown.sh
+++ k8s/cert-manager/teardown.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-set -eu
-
-. ~/bin/env.export
-
-helm="helm --kubeconfig ${HOME}/.kube/eksctl/clusters/${UWS_CLUSTER}"
-
-${helm} uninstall --namespace cert-manager cert-manager
-
-uwskube delete secret -n cert-manager acme-prod-account-key
+set -u
+helm uninstall --namespace cert-manager cert-manager
 uwskube delete namespace cert-manager
-
 exit 0
