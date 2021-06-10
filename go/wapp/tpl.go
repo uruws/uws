5656b74b6d0cdf63833ef6cb803f5397fbe976ef
Jeremías <jeremias@talkingpts.org>
2021-06-10 19:41:12 -0300

    wapp load errors template

--- go/wapp/tpl.go
+++ go/wapp/tpl.go
@@ -11,15 +11,23 @@ import (
 )
 
 var baseTpl *template.Template
+var errorTpl *template.Template
 
 func LoadTemplates(tpldir string) error {
 	log.Debug("load templates")
 	var err error
+
 	baseFn := filepath.Join(tpldir, "base.html")
 	baseTpl, err = template.New("base").ParseFiles(baseFn)
 	if err != nil {
 		return log.DebugError(err)
 	}
-	log.Debug("load templates done!")
+
+	errorFn := filepath.Join(tpldir, "error.html")
+	errorTpl, err = template.New("error").ParseFiles(errorFn)
+	if err != nil {
+		return log.DebugError(err)
+	}
+
 	return nil
 }
