07077bb24f07cc2895904a7fe401ab5f334540b6
Jeremías <jeremias@talkingpts.org>
2021-06-11 18:26:14 -0300

    amy-west new cluster

--- cluster/amy/create.sh
+++ cluster/amy/create.sh
@@ -3,7 +3,7 @@ set -eu
 
 uwseks-cluster-create --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
 	--zones "${AWS_ZONES}" \
-	--nodes 3 --nodes-min 3 --nodes-max 50 \
+	--nodes 10 --nodes-min 5 --nodes-max 90 \
 	--instance-types "${AWS_INSTANCE_TYPES}" \
 	"${UWS_CLUSTER}"
 
--- cluster/amy/setup.sh
+++ cluster/amy/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -eu
 cluster=/home/uws/cluster/amy
-uwskube apply -f ${cluster}/gateway.yaml
+
+uwskube create secret tls tapo-wild-certificate \
+	--cert=${HOME}/ca/godaddyCerts/bundled_all.crt \
+	--key=${HOME}/ca/godaddyCerts/server.key
+
+uwskube apply -f ${cluster}/web-gateway.yaml
 exit 0
