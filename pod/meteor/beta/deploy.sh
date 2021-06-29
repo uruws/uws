904b728cde64dcd9e558666b2f4cb2593d8e6b11
Jeremías <jeremias@talkingpts.org>
2021-06-29 12:49:42 -0300

    meteor beta ops scripts

--- pod/meteor/beta/deploy.sh
+++ pod/meteor/beta/deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 set -eu
+appver=${1:-''}
 pod=/home/uws/pod/meteor/beta
-uwskube apply -n meteor-beta -f ${pod}/deploy.yaml
+if test "X${appver}" != 'X'; then
+	~/pod/meteor/setcfg.sh "${appver}"
+fi
+envf=$(mktemp -p /tmp meteor-beta-deploy.XXXXXXXX)
+~/pod/meteor/getcfg.sh >${envf}
+cat ${envf}
+. ${envf}
+rm -f ${envf}
+envsubst <${pod}/deploy.yaml | uwskube apply -f -
 exit 0
--- pod/meteor/web/deploy.sh
+++ pod/meteor/web/deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 set -eu
+appver=${1:-''}
 pod=/home/uws/pod/meteor/web
-uwskube apply -f ${pod}/deploy.yaml
+if test "X${appver}" != 'X'; then
+	~/pod/meteor/setcfg.sh "${appver}"
+fi
+envf=$(mktemp -p /tmp meteor-web-deploy.XXXXXXXX)
+~/pod/meteor/getcfg.sh >${envf}
+cat ${envf}
+. ${envf}
+rm -f ${envf}
+envsubst <${pod}/deploy.yaml | uwskube apply -f -
 exit 0
