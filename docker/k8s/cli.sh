90e77546275ffc5f42ab12f8d92b4c5933ac2070
Jeremías <jeremias@talkingpts.org>
2021-06-29 16:06:24 -0300

    k8s cli mount kube cache dir

--- docker/k8s/cli.sh
+++ docker/k8s/cli.sh
@@ -15,6 +15,7 @@ hostname="${UWS_CLUSTER}.${AWS_REGION}.k8scli"
 
 kube_cache=${HOME}/.uwscli/kube/cache
 mkdir -p ${kube_cache}
+chmod 1777 ${kube_cache}
 
 exec docker run -it --rm \
 	--hostname ${hostname} -u uws \
@@ -22,5 +23,6 @@ exec docker run -it --rm \
 	-v ${pod}:/home/uws/pod:ro \
 	-v ${awsdir}:/home/uws/.aws:ro \
 	-v ${kubedir}/clusters:/home/uws/.kube/eksctl/clusters:ro \
+	-v ${kube_cache}/:/home/uws/.kube/cache \
 	--env-file ${eksenv} \
 	uws/k8s $@
