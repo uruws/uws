adb11eb11ff0624506cef797b848b3745713e709
Jeremías <jeremias@talkingpts.org>
2021-07-09 17:06:15 -0300

    mount sftp dir

--- srv/proftpd/run.sh
+++ srv/proftpd/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -eu
+mkdir -vp /srv/uws/ftp
 exec docker run -it --rm --name uws-proftpd \
 	--hostname proftpd.uws.local \
 	--read-only \
 	--tmpfs /run:rw,size=100m,mode=1777 \
 	-p 127.0.0.1:2222:22 \
+	-v /srv/uws/sftp:/srv/ftp \
 	uws/proftpd
