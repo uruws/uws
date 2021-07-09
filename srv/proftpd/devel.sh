1e293a4661ea0a2d48472598b934bdb0f3fa030d
Jeremías <jeremias@talkingpts.org>
2021-07-09 16:58:56 -0300

    ftp auth

--- srv/proftpd/devel.sh
+++ srv/proftpd/devel.sh
@@ -4,4 +4,5 @@ exec docker run -it --rm --name uws-proftpd-devel \
 	--hostname proftpd-devel.uws.local \
 	--entrypoint /bin/bash \
 	-v ${PWD}/srv/proftpd/config:/mnt/config \
+	-v ${PWD}/srv/proftpd/secret/auth:/mnt/auth \
 	uws/proftpd
--- srv/proftpd/entrypoint.sh
+++ srv/proftpd/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 set -eu
-#ln -svf /dev/stdout /run/sftp.log
-#ls -lh /run
 exec /usr/sbin/proftpd -n -4 -S 0.0.0.0 -d 3
