566913af5efb79637277cc5dc0d8d30a4a137add
Jeremías <jeremias@talkingpts.org>
2021-07-09 15:05:40 -0300

    sftp port

--- srv/proftpd/run.sh
+++ srv/proftpd/run.sh
@@ -4,5 +4,5 @@ exec docker run -it --rm --name uws-proftpd \
 	--hostname proftpd.uws.local \
 	--read-only \
 	--tmpfs /run:rw,size=100m,mode=1777 \
-	-p 127.0.0.1:2222:222 \
+	-p 127.0.0.1:2222:22 \
 	uws/proftpd
