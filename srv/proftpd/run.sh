61d4c855d2182072488b1a79ec5f4e1130e64b0b
Jeremías <jeremias@talkingpts.org>
2021-07-13 19:49:26 -0300

    sftp set interactive mode

--- srv/proftpd/run.sh
+++ srv/proftpd/run.sh
@@ -3,8 +3,9 @@ set -eu
 sftp_home=${SFTP_HOME:-'/srv/uws/sftp/home'}
 sftp_addr=${SFTP_ADDR:-'127.0.0.1'}
 sftp_port=${SFTP_PORT:-'2222'}
+sftp_mode=${SFTP_MODE:-'-it --rm'}
 mkdir -vp ${sftp_home}
-exec docker run -it --rm --name uws-proftpd \
+exec docker run ${sftp_mode} --name uws-proftpd \
 	--hostname proftpd.uws.local \
 	--read-only -u root \
 	-p "${sftp_addr}:${sftp_port}:22" \
