961751a1a638f8dbc9364de085b1db6042472693
Jeremías <jeremias@talkingpts.org>
2021-07-13 13:38:05 -0300

    proftpd local settings

--- srv/proftpd/entrypoint.sh
+++ srv/proftpd/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eu
-exec /usr/sbin/proftpd -n -4 -S 0.0.0.0 -d 3
+exec /usr/sbin/proftpd -c /usr/local/etc/proftpd/proftpd.conf -n -4 -S 0.0.0.0 -d 7
--- srv/proftpd/run.sh
+++ srv/proftpd/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
-mkdir -vp /srv/uws/ftp
+mkdir -vp /srv/uws/sftp/run /srv/uws/sftp/home
 exec docker run -it --rm --name uws-proftpd \
 	--hostname proftpd.uws.local \
 	--read-only \
-	--tmpfs /run:rw,size=100m,mode=1777 \
 	-p 127.0.0.1:2222:22 \
-	-v /srv/uws/sftp:/srv/ftp \
+	-v /srv/uws/sftp/run:/run \
+	-v /srv/uws/sftp/home:/srv/ftp \
 	uws/proftpd
