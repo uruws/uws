c3d79259e9a14d9206804437e90de075e94e7df5
Jeremías <jeremias@talkingpts.org>
2021-07-13 17:06:15 -0300

    remove unused files and configurable setup

--- srv/proftpd/run.sh
+++ srv/proftpd/run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -eu
-mkdir -vp /srv/uws/sftp/home
+sftp_home=${SFTP_HOME:-'/srv/uws/sftp/home'}
+sftp_addr=${SFTP_ADDR:-'127.0.0.1'}
+sftp_port=${SFTP_PORT:-'2222'}
+mkdir -vp ${sftp_home}
 exec docker run -it --rm --name uws-proftpd \
 	--hostname proftpd.uws.local \
 	--read-only -u root \
-	-p 127.0.0.1:2222:22 \
-	-v /srv/uws/sftp/home:/srv/ftp \
+	-p "${sftp_addr}:${sftp_port}:22" \
+	-v "${sftp_home}:/srv/ftp" \
 	--tmpfs /run:rw,mode=1777,size=10m \
 	uws/proftpd
