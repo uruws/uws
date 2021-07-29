de9c039b94048869e6ea3fe3fc339f534404c268
Jeremías <jeremias@talkingpts.org>
2021-07-29 16:55:43 -0300

    remove old cli deploy script

--- cli/app-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-cluster=${1:?'cluster?'}
-kind=${2:?'kind?'}
-release=${3:?'release?'}
-cd /srv/uws/deploy
-exec ./docker/k8s/cli.sh ${cluster} ./pod/${kind}/deploy.sh "${release}"
