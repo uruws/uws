4d48bbb0847a69c8b489ba4ef0953bb636138a39
Jeremías <jeremias@talkingpts.org>
2021-08-05 16:20:07 -0300

    remove k8s dashboard files

--- k8s/dashboard/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-uwskube apply -f ~/k8s/dashboard/deploy.yaml
-uwskube apply -f ~/k8s/dashboard/auth.yaml
-exit 0
--- k8s/dashboard/teardown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-uwskube delete -f ~/k8s/dashboard/auth.yaml
-uwskube delete -f ~/k8s/dashboard/deploy.yaml
-exit 0
