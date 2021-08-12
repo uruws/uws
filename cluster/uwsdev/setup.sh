e586de207aac87ccd96c9866092a0dcd08190438
Jeremías <jeremias@talkingpts.org>
2021-08-12 19:42:21 -0300

    uwsdev gateway cert protect

--- cluster/uwsdev/setup.sh
+++ cluster/uwsdev/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -eu
 cluster=~/cluster/${UWS_CLUSTER}
+
+~/k8s/ca/setup.sh
+
 uwskube apply -f ${cluster}/gateway.yaml
 exit 0
--- cluster/uwsdev/teardown.sh
+++ cluster/uwsdev/teardown.sh
@@ -2,4 +2,6 @@
 set -eu
 cluster=~/cluster/${UWS_CLUSTER}
 uwskube delete -f ${cluster}/gateway.yaml
+
+~/k8s/ca/teardown.sh
 exit 0
