db9197ec37b28e45fc63d21d6358bfdded0ad5b5
Jeremías <jeremias@talkingpts.org>
2021-08-13 14:41:39 -0300

    k8smon ping and health handlers

--- go/cmd/k8smon/main.go
+++ go/cmd/k8smon/main.go
@@ -11,14 +11,24 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/_/healthz", healthzHandler)
+	http.HandleFunc("/_/ping", pingHandler)
 	http.HandleFunc("/", mainHandler)
 	http.ListenAndServe(":2800", nil)
 }
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "ok")
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
 	if hostname, err := os.Hostname(); err != nil {
 		fmt.Fprintf(w, "ERROR: %s\n", err)
 	} else {
 		fmt.Fprintf(w, "uwsctl@%s\n", hostname)
 	}
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "index")
 }
