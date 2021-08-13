cd99dc6d5d4770d1b4b01fdc6646d3dcdaea88e0
Jeremías <jeremias@talkingpts.org>
2021-08-13 17:38:28 -0300

    log response size

--- go/cmd/k8smon/main.go
+++ go/cmd/k8smon/main.go
@@ -20,29 +20,33 @@ func main() {
 	http.HandleFunc("/_/ping", pingHandler)
 
 	http.HandleFunc("/", mainHandler)
-	http.ListenAndServe(":2800", nil)
 
-	log.Debug("main end")
+	log.Debug("serve...")
+	log.Fatal("%s", http.ListenAndServe(":2800", nil))
 }
 
-func logRequest(r *http.Request, status int) {
-	log.Print("%s %s %s %s %d", r.RemoteAddr, r.Method, r.URL, r.Proto, status)
+func logRequest(r *http.Request, status, size int) {
+	log.Print("%s %s %s %d %d", r.RemoteAddr, r.Method, r.URL, status, size)
 }
 
 func writeError(w http.ResponseWriter, r *http.Request, err error) {
 	log.Error("%s", err)
 	http.Error(w, fmt.Sprintf("error: %s\n", err), http.StatusInternalServerError)
-	logRequest(r, http.StatusInternalServerError)
+	logRequest(r, http.StatusInternalServerError, len("error: \n") + len(err.Error()))
 }
 
 func writeNotFound(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, fmt.Sprintf("not found: %s\n", r.URL.Path), http.StatusNotFound)
-	logRequest(r, http.StatusNotFound)
+	logRequest(r, http.StatusNotFound, len("not found: \n") + len(r.URL.Path))
 }
 
 func write(w http.ResponseWriter, r *http.Request, message string, args ...interface{}) {
-	fmt.Fprintf(w, message, args...)
-	logRequest(r, http.StatusOK)
+	n, err := fmt.Fprintf(w, message, args...)
+	if err != nil {
+		log.Error("%s %s: %s", r.Method, r.URL, err)
+	} else {
+		logRequest(r, http.StatusOK, n)
+	}
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
