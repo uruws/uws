118cf98e2e3e27f1241815a729575c920098c023
Jeremías <jeremias@talkingpts.org>
2021-08-13 23:59:13 -0300

    k8smon run kubectl

--- go/k8s/mon/mon.go
+++ go/k8s/mon/mon.go
@@ -3,3 +3,70 @@
 
 // Package mon implements k8s mon api.
 package mon
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"time"
+
+	"uws/log"
+)
+
+var (
+	kubecmd string
+)
+
+func init() {
+	kubecmd = os.Getenv("UWSKUBE")
+	if kubecmd == "" {
+		kubecmd = "/usr/local/bin/uwskube"
+	}
+}
+
+func Kube(args ...string) (*os.File, error) {
+	var (
+		outfh *os.File
+		errfh *os.File
+		err   error
+	)
+	outfh, err = ioutil.TempFile("", "kube-out.*")
+	if err != nil {
+		return nil, log.DebugError(err)
+	}
+	defer func(fn string) {
+		os.Remove(fn)
+	}(outfh.Name())
+	errfh, err = ioutil.TempFile("", "kube-err.*")
+	if err != nil {
+		return nil, log.DebugError(err)
+	}
+	defer func(fn string) {
+		os.Remove(fn)
+	}(errfh.Name())
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer cancel()
+	args = append(args, "-o", "json")
+	cmd := exec.CommandContext(ctx, kubecmd, args...)
+	cmd.Stdout = outfh
+	cmd.Stderr = errfh
+	log.Debug("run: %s", cmd)
+	xerr := cmd.Run()
+	if xerr != nil {
+		if _, err := errfh.Seek(0, 0); err != nil {
+			log.Error("seek error fh: %s", err)
+		} else {
+			if blob, err := ioutil.ReadAll(errfh); err != nil {
+				log.Error("read error fh: %s", err)
+			} else {
+				log.Error("%s", blob)
+			}
+		}
+		return nil, log.DebugError(xerr)
+	}
+	if _, err := outfh.Seek(0, 0); err != nil {
+		return nil, log.DebugError(err)
+	}
+	return outfh, nil
+}
