0ca56173976679d4efa4b2346abb67e7ac85e4bb
Jeremías <jeremias@talkingpts.org>
2021-08-13 21:22:29 -0300

    wapp lib

--- go/cmd/k8smon/main.go
+++ go/cmd/k8smon/main.go
@@ -5,11 +5,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
 	"uws/log"
+	"uws/wapp"
 )
 
 func main() {
@@ -25,50 +25,26 @@ func main() {
 	log.Fatal("%s", http.ListenAndServe(":2800", nil))
 }
 
-func logRequest(r *http.Request, status, size int) {
-	log.Print("%s %s %s %d %d", r.RemoteAddr, r.Method, r.URL, status, size)
-}
-
-func writeError(w http.ResponseWriter, r *http.Request, err error) {
-	log.Error("%s", err)
-	http.Error(w, fmt.Sprintf("error: %s\n", err), http.StatusInternalServerError)
-	logRequest(r, http.StatusInternalServerError, len("error: \n") + len(err.Error()))
-}
-
-func writeNotFound(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, fmt.Sprintf("not found: %s\n", r.URL.Path), http.StatusNotFound)
-	logRequest(r, http.StatusNotFound, len("not found: \n") + len(r.URL.Path))
-}
-
-func write(w http.ResponseWriter, r *http.Request, message string, args ...interface{}) {
-	n, err := fmt.Fprintf(w, message, args...)
-	if err != nil {
-		log.Error("%s %s: %s", r.Method, r.URL, err)
-	} else {
-		logRequest(r, http.StatusOK, n)
-	}
-}
-
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	if _, err := os.Hostname(); err != nil {
-		writeError(w, r, err)
+		wapp.Error(w, r, err)
 	} else {
-		write(w, r, "ok\n")
+		wapp.Write(w, r, "ok\n")
 	}
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	if hostname, err := os.Hostname(); err != nil {
-		writeError(w, r, err)
+		wapp.Error(w, r, err)
 	} else {
-		write(w, r, "uwsctl@%s\n", hostname)
+		wapp.Write(w, r, "uwsctl@%s\n", hostname)
 	}
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		writeNotFound(w, r)
+		wapp.NotFound(w, r)
 	} else {
-		write(w, r, "index\n")
+		wapp.Write(w, r, "index\n")
 	}
 }
--- go/wapp/wapp.go
+++ go/wapp/wapp.go
@@ -5,16 +5,37 @@
 package wapp
 
 import (
+	"fmt"
 	"net/http"
+
+	"uws/log"
 )
 
-func Error(w http.ResponseWriter, message string, code int) {
-}
-
-func Render(w http.ResponseWriter, p *Page) {
+func logRequest(r *http.Request, status, size int) {
+	log.Print("%s %s %s %d %d", r.RemoteAddr, r.Method, r.URL, status, size)
 }
 
 func ServeStatic(dir string) {
 	http.Handle("/static/",
 		http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
+}
+
+func Error(w http.ResponseWriter, r *http.Request, err error) {
+	log.Error("%s", err)
+	http.Error(w, fmt.Sprintf("error: %s\n", err), http.StatusInternalServerError)
+	logRequest(r, http.StatusInternalServerError, len("error: \n") + len(err.Error()))
+}
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, fmt.Sprintf("not found: %s\n", r.URL.Path), http.StatusNotFound)
+	logRequest(r, http.StatusNotFound, len("not found: \n") + len(r.URL.Path))
+}
+
+func Write(w http.ResponseWriter, r *http.Request, message string, args ...interface{}) {
+	n, err := fmt.Fprintf(w, message, args...)
+	if err != nil {
+		log.Error("%s %s: %s", r.Method, r.URL, err)
+	} else {
+		logRequest(r, http.StatusOK, n)
+	}
 }
