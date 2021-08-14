6a7e1e65f179dcbe690d6288820556e36e21bf17
Jeremías <jeremias@talkingpts.org>
2021-08-14 19:04:52 -0300

    munin-node k8smon plugin

---
+++ srv/munin-node/utils/k8smon.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+plname=$(basename "${0}" | tr -s '_' '/')
+base_uri="http://${K8S_SERVICE_HOST}:${K8S_SERVICE_PORT}/"
+action='report'
+if test 'Xconfig' = "X${1:-''}"; then
+	action='config'
+fi
+uri="${base_uri}/${action}/${plname}"
+exec wget -O - "${uri}"
