21d5f45a2cb675c51b707e57950f59f7262ca059
Jeremías <jeremias@talkingpts.org>
2021-08-15 16:31:45 -0300

    split kube code

--- go/k8s/mon/mon.go
+++ go/k8s/mon/mon.go
@@ -5,14 +5,10 @@
 package mon
 
 import (
-	"context"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"os/exec"
 	"regexp"
 	"strings"
-	"time"
 
 	"uws/log"
 )
@@ -59,88 +55,4 @@ func (b *Buffer) Write(format string, args ...interface{}) error {
 		log.Error("%s", err)
 	}
 	return err
-}
-
-func Kube(args ...string) ([]byte, error) {
-	var (
-		outfh *os.File
-		errfh *os.File
-		err   error
-	)
-	outfh, err = ioutil.TempFile("", "kube-out.*")
-	if err != nil {
-		return nil, log.DebugError(err)
-	}
-	defer func() {
-		os.Remove(outfh.Name())
-		outfh.Close()
-	}()
-	errfh, err = ioutil.TempFile("", "kube-err.*")
-	if err != nil {
-		return nil, log.DebugError(err)
-	}
-	defer func() {
-		os.Remove(errfh.Name())
-		errfh.Close()
-	}()
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
-	defer cancel()
-	cmd := exec.CommandContext(ctx, kubecmd, args...)
-	cmd.Stdout = outfh
-	cmd.Stderr = errfh
-	log.Debug("run: %s", cmd)
-	xerr := cmd.Run()
-	if xerr != nil {
-		if _, err := errfh.Seek(0, 0); err != nil {
-			log.Error("errfh seek: %s", err)
-		} else {
-			if blob, err := ioutil.ReadAll(errfh); err != nil {
-				log.Error("errfh read: %s", err)
-			} else {
-				if len(blob) > 0 {
-					log.Error("%s", blob)
-				}
-			}
-		}
-		return nil, log.DebugError(xerr)
-	}
-	if _, err := outfh.Seek(0, 0); err != nil {
-		return nil, log.DebugError(err)
-	}
-	return ioutil.ReadAll(outfh)
-}
-
-type cacheEntry struct {
-	expire time.Time
-	data []byte
-}
-
-var cache map[string]*cacheEntry
-
-func init() {
-	cache = make(map[string]*cacheEntry)
-}
-
-func cacheKey(args ...string) string {
-	return CleanFN(strings.Join(args, "_"))
-}
-
-func KubeCache(args ...string) ([]byte, error) {
-	now := time.Now()
-	k := cacheKey(args...)
-	e, ok := cache[k]
-	if ok {
-		if e.expire.Before(now) {
-			return e.data, nil
-		}
-	}
-	e = nil
-	var err error
-	delete(cache, k)
-	cache[k] = new(cacheEntry)
-	cache[k].data, err = Kube(args...)
-	if err != nil {
-		return nil, err
-	}
-	return cache[k].data, nil
 }
