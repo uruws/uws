377e69e38c686e11db9d1936341523f233fbbfa3
Jeremías <jeremias@talkingpts.org>
2021-08-15 00:57:17 -0300

    deploy munin node cluster conf

--- k8s/mon/munin-node/deploy.sh
+++ k8s/mon/munin-node/deploy.sh
@@ -2,9 +2,12 @@
 set -eu
 
 cluster_setup=${HOME}/k8s/mon/munin-node/utils/cluster-setup.sh
+node_conf=${HOME}/k8s/mon/munin-node/node.conf
+
 uwskube delete configmap cluster-setup -n mon || true
 uwskube create configmap cluster-setup -n mon \
-	--from-file="setup.sh=${cluster_setup}"
+	--from-file="setup.sh=${cluster_setup}" \
+	--from-file="munin-node.conf=${node_conf}"
 
 export VERSION="$(cat ~/k8s/mon/VERSION)"
 envsubst <~/k8s/mon/munin-node/deploy.yaml | uwskube apply -f -
