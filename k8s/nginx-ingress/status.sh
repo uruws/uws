ba3394907219c44a9435cc574751e9729b489b15
Jeremías <jrmsdev@gmail.com>
2021-08-17 17:09:23 -0300

    nginx proxy upgrade

---
+++ k8s/nginx-ingress/status.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -eu
+uwskube get all -n ingress-nginx
+exit 0
--- k8s/nginx-ingress/upgrade.sh
+++ k8s/nginx-ingress/upgrade.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
+uwskube delete -f ~/k8s/nginx-ingress/setup-jobs.yaml
 uwskube apply -f ~/k8s/nginx-ingress/setup.yaml
 uwskube apply -f ~/k8s/nginx-ingress/setup-jobs.yaml
 ~/k8s/nginx-ingress/deploy.sh
