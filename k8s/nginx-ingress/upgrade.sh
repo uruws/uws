1fdf16d936041588f113b8365474426ad3418411
Jeremías <jrmsdev@gmail.com>
2021-08-17 17:14:26 -0300

    nginx proxy upgrade wait

--- k8s/nginx-ingress/upgrade.sh
+++ k8s/nginx-ingress/upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
-uwskube delete -f ~/k8s/nginx-ingress/setup-jobs.yaml
-uwskube apply -f ~/k8s/nginx-ingress/setup.yaml
-uwskube apply -f ~/k8s/nginx-ingress/setup-jobs.yaml
+uwskube delete -f ~/k8s/nginx-ingress/setup-jobs.yaml --wait=true
+uwskube apply -f ~/k8s/nginx-ingress/setup.yaml --wait=true
+uwskube apply -f ~/k8s/nginx-ingress/setup-jobs.yaml --wait=true
 ~/k8s/nginx-ingress/deploy.sh
 exit 0
