cbda85ea2070a88e924f8bbaccbe9fbcafbfd9fa
Jeremías <jeremias@talkingpts.org>
2021-08-19 11:33:17 -0300

    uws ca cert chain file

--- docker/mkcert/utils/ca-cert.sh
+++ docker/mkcert/utils/ca-cert.sh
@@ -2,5 +2,12 @@
 set -eu
 DOMAIN=${1:-'cert domain?'}
 umask 0027
-exec mkcert -ecdsa -cert-file "${HOME}/ca/cert/${DOMAIN}.pem" \
+
+mkcert -ecdsa -cert-file "${HOME}/ca/cert/${DOMAIN}.pem" \
 	-key-file "${HOME}/ca/cert/${DOMAIN}-key.pem" "$@"
+
+chain_file="${HOME}/ca/cert/${DOMAIN}-chain.pem"
+cat "${HOME}/ca/cert/${DOMAIN}.pem" >${chain_file}
+cat "${HOME}/ca/rootCA.pem" >>${chain_file}
+
+exit 0
