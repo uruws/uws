4e0e2f71dbfddc2fb110d30aa9148be38a096541
Jeremías <jeremias@talkingpts.org>
2021-08-19 11:15:43 -0300

    ca certs dir

--- docker/mkcert/utils/ca-cert.sh
+++ docker/mkcert/utils/ca-cert.sh
@@ -2,5 +2,5 @@
 set -eu
 DOMAIN=${1:-'cert domain?'}
 umask 0027
-exec mkcert -ecdsa -cert-file "${HOME}/ca/crt/${DOMAIN}.pem" \
-	-key-file "${HOME}/ca/key/${DOMAIN}.pem" "$@"
+exec mkcert -ecdsa -cert-file "${HOME}/ca/cert/${DOMAIN}.pem" \
+	-key-file "${HOME}/ca/cert/${DOMAIN}-key.pem" "$@"
