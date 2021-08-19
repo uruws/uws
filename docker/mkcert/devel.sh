4e9b85e172bdf03cd322c2cef9595a9576bf3a3d
Jeremías <jeremias@talkingpts.org>
2021-08-19 14:02:17 -0300

    uws ca change client cert default password

--- docker/mkcert/devel.sh
+++ docker/mkcert/devel.sh
@@ -2,5 +2,6 @@
 exec docker run -it --rm --name uws-mkcert-devel \
 	--hostname mkcert-devel.uws.local \
 	-v ${PWD}/docker/mkcert/utils:/home/uws/utils \
+	-v ${PWD}/secret/ca/uws:/home/uws/ca \
 	--workdir /home/uws/utils \
 	-u uws uws/mkcert /bin/bash -l
---
+++ docker/mkcert/utils/p12-chpass.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+CAFN=${1:?'CA file?'}
+INFN=${2:?'input file?'}
+OUTFN=${3:?'output file?'}
+
+umask 0027
+
+tmp_crt=$(mktemp -t p12-crt.XXXXXXXX)
+tmp_key=$(mktemp -t p12-key.XXXXXXXX)
+
+# stolen from: https://gist.github.com/slabko/5c3210207b2c2d62a61148389fc94800
+
+openssl pkcs12 -in ${INFN} -passin pass:changeit -nocerts \
+	-out ${tmp_key} -passout pass:changeit
+
+openssl pkcs12 -in ${INFN} -passin pass:changeit -clcerts -nokeys \
+	-out ${tmp_crt}
+
+openssl pkcs12 -export -certfile ${CAFN} -out ${OUTFN} \
+	-in ${tmp_crt} -inkey ${tmp_key} \
+	-passin pass:changeit \
+	-passout pass:${P12PW}
+
+rm -f ${tmp_crt} ${tmp_key}
+echo "${OUTFN} created!"
+
+exit 0
