7dcca261286818c5489d60b064480b1eb0207df3
Jeremías <jeremias@talkingpts.org>
2021-08-19 16:41:40 -0300

    improving mounting CADIR

--- docker/mkcert/cmd.sh
+++ docker/mkcert/cmd.sh
@@ -6,7 +6,11 @@ CAROOT="${CAROOT:-${HOME}/.uws/ca}"
 if test 'X' = "X${CANAME}"; then
 	CANAME='default'
 fi
+mkdir -v -m 0700 ${CAROOT}/${CANAME}/${CAVERSION} \
+	${CAROOT}/${CANAME}/${CAVERSION}/cert \
+	${CAROOT}/${CANAME}/${CAVERSION}/client
 exec docker run --rm --network none --name uws-mkcert \
 	--hostname "${CAVERSION}.${CANAME}.ca.uws.talkingpts.org" \
-	-v "${CAROOT}:/home/uws/ca" \
+	-v "${PWD}/docker/mkcert/etc:/usr/local/etc/ssl:ro" \
+	-v "${CAROOT}/${CANAME}/${CAVERSION}:/home/uws/ca" \
 	-u uws uws/mkcert "$@"
--- docker/mkcert/devel.sh
+++ docker/mkcert/devel.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 exec docker run -it --rm --name uws-mkcert-devel \
 	--hostname mkcert-devel.uws.local \
-	-v ${PWD}/docker/mkcert/utils:/home/uws/utils \
-	-v ${PWD}/secret/ca/uws:/home/uws/ca \
+	-v ${PWD}/docker/mkcert/utils:/home/uws/utils:ro \
+	-v ${PWD}/docker/mkcert/etc:/usr/local/etc/ssl:ro \
+	-v ${PWD}/secret/ca/uws/ops/210819:/home/uws/ca:ro \
 	--workdir /home/uws/utils \
 	-u uws uws/mkcert /bin/bash -l
