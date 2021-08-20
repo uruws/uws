7bf856a826cbdfc43d773b7fcbc51d4ee021a065
Jeremías <jeremias@talkingpts.org>
2021-08-20 17:44:23 -0300

    uws ops ca 210820 new version

--- docker/mkcert/utils/ca-revoke.sh
+++ docker/mkcert/utils/ca-revoke.sh
@@ -5,5 +5,6 @@ export OPENSSL_CONF=/usr/local/etc/ssl/openssl.cnf
 for fn in $(ls ${HOME}/ca/revoke/new/*.pem 2>/dev/null); do
 	echo "ca revoke: ${fn}"
 	openssl ca -revoke "${fn}"
+	mv -f "${fn}" "${HOME}/ca/revoke/$(basename "${fn}")"
 done
 exec ca-crl.sh
