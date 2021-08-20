aaab9c91aed1814ff27fe4cb317a5672e3f94430
Jeremías <jeremias@talkingpts.org>
2021-08-20 12:32:37 -0300

    fix ca revoke

--- docker/mkcert/utils/ca-revoke.sh
+++ docker/mkcert/utils/ca-revoke.sh
@@ -2,7 +2,7 @@
 set -eu
 umask 0027
 export OPENSSL_CONF=/usr/local/etc/ssl/openssl.cnf
-for fn in $(ls "${HOME}/ca/revoke/*.pem"); do
+for fn in $(ls ${HOME}/ca/revoke/*.pem); do
 	echo "ca revoke: ${fn}"
 	openssl ca -revoke "${fn}"
 done
