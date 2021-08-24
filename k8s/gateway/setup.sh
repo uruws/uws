77e6192efa8bba691f0d8d581a563f873dae4f6b
Jeremías <jeremias@talkingpts.org>
2021-08-24 15:57:37 -0300

    cluster gateway use ops ca

--- k8s/gateway/setup.sh
+++ k8s/gateway/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -eu
+uwskube create secret generic uwsca-ops \
+	--from-file=ca.crt=${HOME}/ca/uws/ops/210823/rootCA.pem \
+	--from-file=ca.crl=${HOME}/ca/uws/ops/210823/rootCA-crl.pem
 envsubst <~/k8s/gateway/gateway.yaml | uwskube apply -f -
 exit 0
--- k8s/gateway/teardown.sh
+++ k8s/gateway/teardown.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-set -eu
+set -u
 uwskube delete svc munin-web
 uwskube delete svc k8smon
-uwskube delete ingress cluster-gateway
+uwskube delete ingress cluster-gateway --wait=true
+uwskube delete secret uwsca-ops
 exit 0
