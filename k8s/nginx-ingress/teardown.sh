acf9f5c91c3a5e5a0812d555276f9e5014d606b8
Jeremías <jeremias@talkingpts.org>
2021-08-26 16:57:10 -0300

    set cluster http expire headers

--- k8s/nginx-ingress/teardown.sh
+++ k8s/nginx-ingress/teardown.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -eu
+if test -s ~/cluster/nginx/config.yaml; then
+	uwskube delete -f ~/cluster/nginx/config.yaml
+else
+	uwskube delete -f ~/k8s/nginx-ingress/config.yaml
+fi
 uwskube delete -f ~/k8s/nginx-ingress/deploy.yaml
 uwskube delete -f ~/k8s/nginx-ingress/setup-jobs.yaml
 uwskube delete -f ~/k8s/nginx-ingress/setup.yaml
