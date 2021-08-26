c5a11ccda577206088906a0a31fd9b1e2fb77468
Jeremías <jeremias@talkingpts.org>
2021-08-26 17:18:57 -0300

    gateway deploy server snippet

--- k8s/gateway/setup.sh
+++ k8s/gateway/setup.sh
@@ -3,5 +3,5 @@ set -eu
 uwskube create secret generic uwsca-ops \
 	--from-file=ca.crt=${HOME}/ca/uws/ops/210823/rootCA.pem \
 	--from-file=ca.crl=${HOME}/ca/uws/ops/210823/rootCA-crl.pem
-envsubst <~/k8s/gateway/gateway.yaml | uwskube apply -f -
+~/k8s/gateway/deploy.sh
 exit 0
