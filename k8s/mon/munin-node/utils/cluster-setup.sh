905419fdb3218c99de1416608d6cb0ad34ab02fb
Jeremías <jeremias@talkingpts.org>
2021-09-02 16:17:15 -0300

    fix plugin compile

--- k8s/mon/munin-node/utils/cluster-setup.sh
+++ k8s/mon/munin-node/utils/cluster-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-python3 -m compileall /srv/munin/plugins
+python3 -m compileall /srv/munin/plugins/*.py
 
 install -v -m 0755 /srv/munin/plugins/k8smon.sh /usr/local/bin/k8smon
 install -v -m 0755 /srv/munin/plugins/nginx.py /usr/local/bin/nginx
