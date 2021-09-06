daa4064d831f637fef4789e6c870e875ecf3b296
Jeremías <jeremias@talkingpts.org>
2021-09-06 12:32:50 -0300

    graph web bytes sent

--- k8s/mon/munin-node/plugins/nginx.py
+++ k8s/mon/munin-node/plugins/nginx.py
@@ -7,24 +7,29 @@ import os
 import sys
 
 # load lib
+
 MONLIB = os.getenv('MONLIB', '/srv/munin/plugins')
 sys.path.insert(0, MONLIB)
 
 import mon
 
 # load env
+
 __nginx_metrics = 'http://metrics.ingress-nginx.svc.cluster.local:10254/metrics'
 METRICS_URL = os.getenv('NGINX_METRICS_URL', __nginx_metrics)
 
 # load modules
+
 import nginx_conn
 import nginx_proc
 import nginx_cfg
+import web_bytes_sent
 
 if __name__ == '__main__':
 	__mod = dict(
 		nginx_conn = nginx_conn,
 		nginx_proc = nginx_proc,
 		nginx_cfg = nginx_cfg,
+		web_bytes_sent = web_bytes_sent,
 	)
 	sys.exit(mon.main(METRICS_URL, __mod))
---
+++ k8s/mon/munin-node/plugins/web_bytes_sent.py
@@ -0,0 +1,68 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+import mon
+
+from time import time
+
+sts = dict(
+	host = dict(),
+)
+
+def __parse(name, meta, value):
+	global sts
+	host = meta['host']
+	mon.dbg('parse web_bytes_sent:', name, host)
+	if not sts['host'].get(host, None):
+		sts['host'][host] = dict(sum = 0, count = 0, meta = meta)
+	sts['host'][host][name] += value
+	return True
+
+def parse(name, meta, value):
+	if name == 'nginx_ingress_controller_bytes_sent_sum':
+		return __parse('sum', meta, value)
+	elif name == 'nginx_ingress_controller_bytes_sent_count':
+		return __parse('count', meta, value)
+	return False
+
+def config(sts):
+	mon.dbg('config web_bytes_sent')
+	# host
+	hn = 0
+	for host in sorted(sts['host'].keys()):
+		meta = sts['host'][host]['meta']
+		hostid = mon.cleanfn(host)
+		ingress = mon.cleanfn(meta['ingress'])
+		# count
+		print(f"multigraph web_bytes_sent_{hostid}_count")
+		print(f"graph_title {host} bytes sent count")
+		print('graph_args --base 1000 -l 0')
+		print(f"graph_category {ingress}")
+		print('graph_vlabel number')
+		print('graph_scale yes')
+		print('sent.label sent')
+		print(f"sent.colour COLOUR{hn}")
+		print('sent.min 0')
+		# sum
+		print(f"multigraph web_bytes_sent_{hostid}_sum")
+		print(f"graph_title {host} bytes sent")
+		print('graph_args --base 1024 -l 0')
+		print(f"graph_category {ingress}")
+		print('graph_vlabel bytes')
+		print('graph_scale yes')
+		print('sent.label sent')
+		print(f"sent.colour COLOUR{hn}")
+		print('sent.min 0')
+		hn += 1
+
+def report(sts):
+	mon.dbg('report web_bytes_sent')
+	for host in sorted(sts['host'].keys()):
+		meta = sts['host'][host]['meta']
+		hostid = mon.cleanfn(host)
+		# count
+		print(f"multigraph web_bytes_sent_{hostid}_count")
+		print('sent.value', sts['host'][host]['count'])
+		# sum
+		print(f"multigraph web_bytes_sent_{hostid}_sum")
+		print('sent.value', sts['host'][host]['sum'])
