9c0c14131f48815feb98425c8d83e51fd407a290
Jeremías <jeremias@talkingpts.org>
2021-09-08 12:17:30 -0300

    quiet uwscli logs manager

--- host/assets/jsbatch/uws/bin/uwscli-logs.sh
+++ host/assets/jsbatch/uws/bin/uwscli-logs.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -eu
 
-find ~/logs/ -type f -name '*-build-*.log' -mtime +2 -exec gzip -v9 {} \;
+find ~/logs/ -type f -name '*-build-*.log' -mtime +2 -exec gzip -9 {} \;
 
 if test -s ~/logs/app-ctl.log; then
 	dst=app-ctl-$(date '+%y%m%d').log
-	mv -vf ~/logs/app-ctl.log ~/logs/${dst}
-	gzip -v9 ~/logs/${dst}
+	mv -f ~/logs/app-ctl.log ~/logs/${dst}
+	gzip -9 ~/logs/${dst}
 fi
 
-find ~/logs/ -type f -name '*.log.gz' -mtime +30 -exec rm -vf {} \;
+find ~/logs/ -type f -name '*.log.gz' -mtime +30 -exec rm -f {} \;
 
 exit 0
