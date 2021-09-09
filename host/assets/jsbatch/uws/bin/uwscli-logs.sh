be15d511cf264536469aa90c0ac2f8b70eb67c5b
Jeremías <jeremias@talkingpts.org>
2021-09-09 11:42:18 -0300

    rotate app-ctl.log on sundays only

--- host/assets/jsbatch/uws/bin/uwscli-logs.sh
+++ host/assets/jsbatch/uws/bin/uwscli-logs.sh
@@ -4,9 +4,12 @@ set -eu
 find ~/logs/ -type f -name '*-build-*.log' -mtime +2 -exec gzip -9 {} \;
 
 if test -s ~/logs/app-ctl.log; then
-	dst=app-ctl-$(date '+%y%m%d').log
-	mv -f ~/logs/app-ctl.log ~/logs/${dst}
-	gzip -9 ~/logs/${dst}
+	# run on sundays only
+	if test 'X0' = "X$(date '+%w')"; then
+		dst=app-ctl-$(date '+%y%m%d').log
+		mv -f ~/logs/app-ctl.log ~/logs/${dst}
+		gzip -9 ~/logs/${dst}
+	fi
 fi
 
 find ~/logs/ -type f -name '*.log.gz' -mtime +30 -exec rm -f {} \;
