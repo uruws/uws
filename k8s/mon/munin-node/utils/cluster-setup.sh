4e3cc75924f3200aadea9327f6144b1fcb51b970
Jeremías <jeremias@talkingpts.org>
2021-09-14 19:47:46 -0300

    munin-node setup new nodes plugin

--- k8s/mon/munin-node/utils/cluster-setup.sh
+++ k8s/mon/munin-node/utils/cluster-setup.sh
@@ -3,12 +3,12 @@ set -eu
 
 python3 -m compileall /srv/munin/plugins/*.py
 
-install -v -m 0755 /srv/munin/plugins/k8smon.sh /usr/local/bin/k8smon
+install -v -m 0755 /srv/munin/plugins/nodes.py /usr/local/bin/nodes
 install -v -m 0755 /srv/munin/plugins/nginx.py /usr/local/bin/nginx
 
 enpl=/root/bin/plugin-enable.sh
 
-${enpl} local k8smon nodes
+${enpl} local nodes
 ${enpl} local nginx
 
 exit 0
