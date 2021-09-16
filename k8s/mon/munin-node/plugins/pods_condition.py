7cc5988d7cd099aeebd8b0819f45f19e861d4cc6
Jeremías <jeremias@talkingpts.org>
2021-09-16 16:50:05 -0300

    pods condition index

--- k8s/mon/munin-node/plugins/pods_condition.py
+++ k8s/mon/munin-node/plugins/pods_condition.py
@@ -5,7 +5,9 @@ import mon
 
 def parse(pods):
 	mon.dbg('pods_condition parse')
-	sts = dict()
+	sts = dict(
+		index = dict(),
+	)
 	for i in pods['items']:
 		kind = i['kind']
 		if kind != 'Pod':
@@ -13,15 +15,16 @@ def parse(pods):
 		for cond in i['status'].get('conditions', {}):
 			st = cond['status']
 			typ = cond['type']
-			if not sts.get(typ, None):
-				sts[typ] = 0
+			if not sts['index'].get(typ, None):
+				sts['index'][typ] = 0
 			if st == 'True':
-				sts[typ] += 1
+				sts['index'][typ] += 1
 	return sts
 
 def config(sts):
 	mon.dbg('pods_condition config')
 	cluster = mon.cluster()
+	# index
 	print('multigraph pod_condition')
 	print(f"graph_title {cluster} pods condition")
 	print('graph_args --base 1000 -l 0')
@@ -30,7 +33,7 @@ def config(sts):
 	print('graph_printf %3.0lf')
 	print('graph_scale yes')
 	cc = 0
-	for ctype in sorted(sts.keys()):
+	for ctype in sorted(sts['index'].keys()):
 		cid = mon.cleanfn(ctype.lower())
 		print(f"c_{cid}.label", ctype)
 		print(f"c_{cid}.colour COLOUR{cc}")
@@ -41,7 +44,7 @@ def config(sts):
 def report(sts):
 	mon.dbg('pods_condition report')
 	print('multigraph pod_condition')
-	for ctype in sorted(sts.keys()):
+	for ctype in sorted(sts['index'].keys()):
 		cid = mon.cleanfn(ctype.lower())
-		print(f"c_{cid}.value", sts[ctype])
+		print(f"c_{cid}.value", sts['index'][ctype])
 	if mon.debug(): print()
