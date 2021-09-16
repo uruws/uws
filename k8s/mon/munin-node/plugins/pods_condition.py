3f0550b085425c42778b3b01252ecbf543d7d14c
Jeremías <jeremias@talkingpts.org>
2021-09-16 19:19:58 -0300

    pods condition show real name

--- k8s/mon/munin-node/plugins/pods_condition.py
+++ k8s/mon/munin-node/plugins/pods_condition.py
@@ -16,22 +16,25 @@ def parse(pods):
 		# status
 		m = i['metadata']
 		ns = m.get('namespace', None)
-		name = mon.generateName(i)
+		name = m.get('name', None)
 		if not sts['cond'].get(ns, None):
 			sts['cond'][ns] = dict()
 		if not sts['cond'][ns].get(name, None):
-			sts['cond'][ns][name] = dict()
+			sts['cond'][ns][name] = dict(
+				count = dict(),
+				gname = mon.generateName(i),
+			)
 		# index
 		for cond in i['status'].get('conditions', {}):
 			st = cond['status']
 			typ = cond['type']
 			if not sts['index'].get(typ, None):
 				sts['index'][typ] = 0
-			if not sts['cond'][ns][name].get(typ, None):
-				sts['cond'][ns][name][typ] = 0
+			if not sts['cond'][ns][name]['count'].get(typ, None):
+				sts['cond'][ns][name]['count'][typ] = 0
 			if st == 'True':
 				sts['index'][typ] += 1
-				sts['cond'][ns][name][typ] += 1
+				sts['cond'][ns][name]['count'][typ] += 1
 	return sts
 
 def config(sts):
@@ -56,7 +59,8 @@ def config(sts):
 	# status
 	for ns in sorted(sts['cond'].keys()):
 		for name in sorted(sts['cond'][ns].keys()):
-			cid = mon.cleanfn(f"{ns}_{name}")
+			gname = sts['cond'][ns][name]['gname']
+			cid = mon.cleanfn(f"{ns}_{gname}")
 			print(f"multigraph pod_condition.{cid}")
 			print(f"graph_title {cluster} {ns}/{name} condition")
 			print('graph_args --base 1000 -l 0')
@@ -65,7 +69,7 @@ def config(sts):
 			print('graph_printf %3.0lf')
 			print('graph_scale yes')
 			cc = 0
-			for ctype in sorted(sts['cond'][ns][name].keys()):
+			for ctype in sorted(sts['cond'][ns][name]['count'].keys()):
 				fid = mon.cleanfn(ctype.lower())
 				print(f"c_{fid}.label", ctype)
 				print(f"c_{fid}.colour COLOUR{cc}")
@@ -83,10 +87,11 @@ def report(sts):
 	# status
 	for ns in sorted(sts['cond'].keys()):
 		for name in sorted(sts['cond'][ns].keys()):
-			cid = mon.cleanfn(f"{ns}_{name}")
+			gname = sts['cond'][ns][name]['gname']
+			cid = mon.cleanfn(f"{ns}_{gname}")
 			print(f"multigraph pod_condition.{cid}")
-			for ctype in sorted(sts['cond'][ns][name].keys()):
+			for ctype in sorted(sts['cond'][ns][name]['count'].keys()):
 				fid = mon.cleanfn(ctype.lower())
-				val = sts['cond'][ns][name][ctype]
+				val = sts['cond'][ns][name]['count'][ctype]
 				print(f"c_{fid}.value", val)
 			if mon.debug(): print()
