115699f961e956f7a8e91d52326ad8c311e8fb75
Jeremías <jeremias@talkingpts.org>
2021-09-17 12:54:35 -0300

    amy-wrkr cluster k8smon

--- cluster/amy/setup.sh
+++ cluster/amy/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -eu
-
-uwskube create secret tls tapo-wild-certificate \
-	--cert=${HOME}/ca/godaddyCerts/bundled_all.crt \
-	--key=${HOME}/ca/godaddyCerts/server.key
-
-uwskube apply -f ~/cluster/web-gateway.yaml
+~/k8s/gateway/setup.sh
+~/k8s/mon/setup.sh
 exit 0
---
+++ cluster/amy/worker-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+~/cluster/setup.sh
+
+uwskube create secret tls tapo-wild-certificate \
+	--cert=${HOME}/ca/godaddyCerts/bundled_all.crt \
+	--key=${HOME}/ca/godaddyCerts/server.key
+
+uwskube apply -f ~/cluster/worker-gateway.yaml
+
+exit 0
