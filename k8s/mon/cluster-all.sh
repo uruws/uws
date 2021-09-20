4eda248bd13e5e22722c91ec752ed6aceaacc37a
Jeremías <jeremias@talkingpts.org>
2021-09-20 17:33:25 -0300

    k8smon all cluster helper

---
+++ k8s/mon/cluster-all.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+action=${1:?'action?'}
+
+deploy() {
+	local cluster="${1}"
+	./docker/k8s/cli.sh "${cluster}" ./k8s/mon/deploy.sh
+}
+
+status() {
+	local cluster="${1}"
+	./docker/k8s/cli.sh "${cluster}" ./k8s/mon/status.sh
+}
+
+for ef in $(ls ./eks/env/*.env); do
+	cluster=$(basename ${ef} .env)
+	echo "*** ${cluster}"
+	if test "X${action}" = 'Xdeploy'; then
+		deploy ${cluster}
+	elif test "X${action}" = 'Xstatus'; then
+		status ${cluster}
+	else
+		echo "invalid action: ${action}" >&2
+		exit 3
+	fi
+done
+
+exit 0
