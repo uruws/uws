36dcaea7eae7a394ca266765154619a1fc20fd82
Jeremías <jeremias@talkingpts.org>
2021-09-21 19:37:29 -0300

    fix uwsnq.sh cmd args

--- cli/uwsnq.sh
+++ cli/uwsnq.sh
@@ -4,10 +4,9 @@ export NQDIR=/run/uwscli/nq
 mkdir -p -m 0750 ${NQDIR}
 CMD=${1:?'cmd?'}
 shift
-ARGS="$*"
 if test "X${CMD}" != 'X/srv/uws/deploy/cli/app-build.sh'\
 	&& test "X${CMD}" != 'X/srv/deploy/Buildpack/build.py'; then
 	echo "${CMD}: invalid command" >&2
 	exit 9
 fi
-exec nq -c -- "${CMD}" "${ARGS}"
+exec nq -c -- "${CMD}" "$@"
