419d668912b275d37606a95aefc4c89bd54d254a
Jeremías <jeremias@talkingpts.org>
2021-09-21 11:09:41 -0300

    mon all cluster munin-node restart

--- k8s/mon/cluster-all.sh
+++ k8s/mon/cluster-all.sh
@@ -13,6 +13,11 @@ status() {
 	./docker/k8s/cli.sh "${cluster}" ./k8s/mon/status.sh
 }
 
+munin_node_restart() {
+	local cluster="${1}"
+	./docker/k8s/cli.sh "${cluster}" ./k8s/mon/munin-node/restart.sh
+}
+
 for ef in $(ls ./eks/env/*.env); do
 	cluster=$(basename ${ef} .env)
 	echo "*** ${cluster}"
@@ -20,6 +25,8 @@ for ef in $(ls ./eks/env/*.env); do
 		deploy ${cluster}
 	elif test "X${action}" = 'Xstatus'; then
 		status ${cluster}
+	elif test "X${action}" = 'Xmunin-node/restart'; then
+		munin_node_restart ${cluster}
 	else
 		echo "invalid action: ${action}" >&2
 		exit 3
