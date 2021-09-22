005e7ccdc9b7ce882697765dd542ebfd0a8fe377
Jeremías <jeremias@talkingpts.org>
2021-09-22 12:02:22 -0300

    nginx logs ignore errors

--- k8s/nginx-ingress/logs.sh
+++ k8s/nginx-ingress/logs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 #exec uwskube logs --tail=10 -n ingress-nginx service/ingress-nginx-controller $@
-exec uwskube -n ingress-nginx logs -l 'app.kubernetes.io/instance=ingress-nginx' $@
+exec uwskube -n ingress-nginx logs -l 'app.kubernetes.io/instance=ingress-nginx' --ignore-errors $@
