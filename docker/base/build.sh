303270a23380271b18dac93af40c2f1d81e55b54
Jeremías <jeremias@talkingpts.org>
2021-09-22 14:14:16 -0300

    base debian upgrade 10 -> 11

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -4,8 +4,17 @@ if test 'X0' = "X$(id -u)"; then
 	echo "do not run as root!" >&2
 	exit 1
 fi
-exec docker build $@ --rm -t uws/base \
+# base
+docker build $@ --rm -t uws/base \
 	--build-arg UWS_UID=$(id -u) \
 	--build-arg UWS_GID=$(id -g) \
 	--build-arg UWS_UMASK=$(umask) \
 	./docker/base
+# base.2109
+docker build $@ --rm -t uws/base-2109 \
+	--build-arg UWS_UID=$(id -u) \
+	--build-arg UWS_GID=$(id -g) \
+	--build-arg UWS_UMASK=$(umask) \
+	-f docker/base/Dockerfile.2109 \
+	./docker/base
+exit 0
--- docs/infra/upgrades.md
+++ docs/infra/upgrades.md
@@ -4,9 +4,16 @@
     * jsbatch
         * `2109`: 10 (buster) -> 11 (bullseye)
     * containers
+        * docker/base
+            * `2109`: 10 (buster) -> 11 (bullseye)
+        * docker/base-testing
+            * `2109`: bullseye -> bookworm
 
 * cluster
     * aws AMI
         * `2108`
     * nginx ingress
         * `2108`: 0.45.0 -> 0.48.1
+
+* external utils
+    * docker/awscli
