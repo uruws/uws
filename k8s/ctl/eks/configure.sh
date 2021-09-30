a13839ac0503e13eb0fa21d87590c0ee620018e8
Jeremías <jeremias@talkingpts.org>
2021-09-30 13:32:03 -0300

    fix eks configure namespace

--- k8s/ctl/eks/configure.sh
+++ k8s/ctl/eks/configure.sh
@@ -6,8 +6,8 @@ set -eu
 #~ aws_conf=${HOME}/.aws/config
 #~ aws_auth=${HOME}/.aws/credentials
 
-#~ uwskube delete secret aws-auth -n mon || true
-#~ uwskube create secret generic aws-auth -n mon \
+#~ uwskube delete secret aws-auth -n ctl || true
+#~ uwskube create secret generic aws-auth -n ctl \
 	#~ --from-file="config=${aws_conf}" \
 	#~ --from-file="credentials=${aws_auth}"
 
@@ -15,8 +15,8 @@ set -eu
 
 cluster_auth=${HOME}/.kube/eksctl/clusters/${UWS_CLUSTER}
 
-uwskube delete secret cluster-auth -n mon || true
-uwskube create secret generic cluster-auth -n mon \
+uwskube delete secret cluster-auth -n ctl || true
+uwskube create secret generic cluster-auth -n ctl \
 	--from-file="${UWS_CLUSTER}=${cluster_auth}"
 
 # cluster env
@@ -27,8 +27,8 @@ echo "UWS_CLUSTER=${UWS_CLUSTER}" >${cluster_env}
 echo "AWS_PROFILE=${AWS_PROFILE}" >>${cluster_env}
 echo "K8S_VERSION=${K8S_VERSION}" >>${cluster_env}
 
-uwskube delete configmap cluster-env -n mon || true
-uwskube create configmap cluster-env -n mon \
+uwskube delete configmap cluster-env -n ctl || true
+uwskube create configmap cluster-env -n ctl \
 	--from-env-file=${cluster_env}
 
 rm -f ${cluster_env}
