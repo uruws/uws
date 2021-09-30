06534df4d75efe6b39fb2d916c887aabf4879e5e
Jeremías <jeremias@talkingpts.org>
2021-09-30 14:39:35 -0300

    delete all jobs

--- k8s/ctl/eks/rollin.sh
+++ k8s/ctl/eks/rollin.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -u
 uwskube delete cronjob eks-nodegroup-upgrade -n ctl
+uwskube delete jobs -n ctl --all
 #~ uwskube delete secret aws-auth -n ctl
 uwskube delete secret cluster-auth -n ctl
 uwskube delete configmap cluster-env -n ctl
