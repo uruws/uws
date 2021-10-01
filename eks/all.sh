36e44208c450c9d475fe61aad5ee466ce45b2fc7
Jeremías <jeremias@talkingpts.org>
2021-10-01 12:41:11 -0300

    run all clusters cmd helper

---
+++ eks/all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+action=${1:?'action?'}
+
+if ! test -x ${action}; then
+	echo "invalid action: ${action}" >&2
+	exit 3
+fi
+
+for ef in $(ls ./eks/env/*.env); do
+	cluster=$(basename ${ef} .env)
+	echo "*** ${cluster}"
+	./eks/cmd.sh ${cluster} ${action}
+done
+
+exit 0
