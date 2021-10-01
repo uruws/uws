38eb3556667f75d5a669683cb1afb1dffc33e473
Jeremías <jeremias@talkingpts.org>
2021-10-01 15:12:09 -0300

    nginx ingress upgrade 0.49.2

--- k8s/nginx-ingress/upstream-get.sh
+++ k8s/nginx-ingress/upstream-get.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
-VERSION='0.48.1'
-wget -O upstream-deploy.yaml https://github.com/kubernetes/ingress-nginx/raw/controller-v${VERSION}/deploy/static/provider/aws/deploy.yaml
+VERSION='0.49.2'
+wget -O upstream-deploy.yaml \
+	https://github.com/kubernetes/ingress-nginx/raw/controller-v${VERSION}/deploy/static/provider/aws/deploy.yaml
 exit 0
