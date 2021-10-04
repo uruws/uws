0c039e05787569808cbbad48e8e3214988e07a5d
Jeremías <jeremias@talkingpts.org>
2021-10-04 17:53:36 -0300

    munin alert python script

--- srv/munin/devel.sh
+++ srv/munin/devel.sh
@@ -2,8 +2,8 @@
 set -eu
 exec docker run -it --rm --name uws-munin-devel \
 	--hostname munin-devel.uws.local \
-	-v ${PWD}/srv/munin:/home/uws/srv/munin \
-	-u uws --entrypoint /bin/bash \
+	-v ${PWD}/srv/munin:/home/uws/srv/munin:ro \
+	-u uws --entrypoint /usr/local/bin/uws-login.sh \
 	-e USER=uws -e HOME=/home/uws \
 	--workdir /home/uws \
 	$@ uws/munin
---
+++ srv/munin/utils/munin-alert.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main(args):
+	return 128
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
