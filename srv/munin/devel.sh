e7c7c1f20273face9dde57b44927db1834bdc048
Jeremías <jeremias@talkingpts.org>
2021-10-05 16:25:24 -0300

    munin devel tmp dir

--- srv/munin/devel.sh
+++ srv/munin/devel.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -eu
+mkdir -vp ${PWD}/tmp
 exec docker run -it --rm --name uws-munin-devel \
 	--hostname munin-devel.uws.local \
 	--read-only \
 	-v ${PWD}/srv/munin:/home/uws/srv/munin:ro \
+	-v ${PWD}/tmp:/home/uws/tmp \
 	-u uws --entrypoint /usr/local/bin/uws-login.sh \
 	-e USER=uws -e HOME=/home/uws \
 	--workdir /home/uws \
