8d557be5fbe1a0615cbe7a53ba9ee5e86cfe7bc8
Jeremías <jeremias@talkingpts.org>
2021-10-05 23:15:27 -0300

    parse alerts info

--- srv/munin/utils/munin-alert.py
+++ srv/munin/utils/munin-alert.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
 import fileinput
+import json
 import sys
 
 def main(args):
+	# ~ fh = open('/home/uws/tmp/munin-run/alerts.out', 'w')
 	try:
 		for line in fileinput.input('-'):
-			print(line)
+			line = line.strip()
+			# ~ print(line, file = fh)
+			print('LINE:', line)
+			try:
+				stats = json.loads(line)
+			except Exception as err:
+				# TODO: send [ERROR] by email
+				print('ERROR:', err, file = sys.stderr)
+				continue
+			print('STATS:', stats)
 	except KeyboardInterrupt:
+		# ~ fh.close()
 		return 1
+	# ~ fh.close()
 	return 128
 
 if __name__ == '__main__':
