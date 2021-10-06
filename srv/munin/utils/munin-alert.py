aed6a0782475607620a3c2eb2e2832a56b8c9df9
Jeremías <jeremias@talkingpts.org>
2021-10-06 00:31:59 -0300

    generate alert email

--- srv/munin/utils/munin-alert.py
+++ srv/munin/utils/munin-alert.py
@@ -1,11 +1,70 @@
 #!/usr/bin/env python3
 
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
 import fileinput
 import json
 import sys
 
+from email.message import EmailMessage
+from email.headerregistry import Address
+from io import StringIO
+
 def parse(stats):
 	print('STATS:', stats)
+	msg = EmailMessage()
+	msg['From'] = Address('munin alert', 'munin-alert', 'uws.talkingpts.org')
+	msg['To'] = Address('jrms', 'jeremias', 'talkingpts.org')
+	msg['Subject'] = _msgSubject(stats)
+	with StringIO() as c:
+		_msgContent(c, stats)
+		msg.set_content(c.getvalue())
+		c.close()
+	return msg
+
+def _msgSubject(s):
+	worst = s.get('worst', 'ERROR')
+	host = s.get('host', 'NO_HOST')
+	plugin = s.get('plugin', 'NO_PLUGIN')
+	return f"[{worst}] {host} {plugin}"
+
+def _msgContent(c, s):
+	worst = s.get('worst', 'ERROR')
+	group = s.get('group', 'NO_GROUP')
+	host = s.get('host', 'NO_HOST')
+	plugin = s.get('plugin', 'NO_PLUGIN')
+	category = s.get('category', 'NO_CATEGORY')
+	title = s.get('title', 'NO_TITLE')
+	c.write(f"{group} :: {host} :: {plugin}\n")
+	c.write('\n')
+	c.write(f"{category} :: {title} :: {worst}\n")
+	c.write('\n')
+	ok = s.get('ok', [])
+	if len(ok) > 0:
+		c.write('OK\n')
+		for f in ok:
+			c.write(f"  {f['label']}: {f['value']}\n")
+	foks = s.get('foks', [])
+	if len(foks) > 0:
+		c.write('RECOVER\n')
+		for f in foks:
+			c.write(f"  {f['label']}: {f['value']}\n")
+	warn = s.get('warning', [])
+	if len(warn) > 0:
+		c.write('WARNING\n')
+		for f in warn:
+			c.write(f"  {f['label']}: {f['value']}\n")
+	crit = s.get('critical', [])
+	if len(crit) > 0:
+		c.write('CRITICAL\n')
+		for f in crit:
+			c.write(f"  {f['label']}: {f['value']}\n")
+	unk = s.get('unknown', [])
+	if len(unk) > 0:
+		c.write('UNKNOWN\n')
+		for f in unk:
+			c.write(f"  {f['label']}\n")
 
 def main():
 	# ~ fh = open('/home/uws/tmp/munin-run/alerts.out', 'w')
@@ -20,7 +79,9 @@ def main():
 				# TODO: send [ERROR] by email
 				print('ERROR:', err, file = sys.stderr)
 				continue
-			parse(stats)
+			msg = parse(stats)
+			print('MSG:', msg)
+			#send(msg)
 	except KeyboardInterrupt:
 		# ~ fh.close()
 		return 1
