ea8387f4818f0a639327e5e9d8c9976e84865315
Jeremías <jeremias@talkingpts.org>
2021-10-07 12:42:29 -0300

    delete jobs too

--- k8s/ctl/eks/jobs-clean.sh
+++ k8s/ctl/eks/jobs-clean.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -u
-for j in $(uwskube get pods -n ctl | grep -F 'Error' | cut -d ' ' -f 1); do
+for j in $(uwskube get pods -n ctl -o name); do
 	uwskube delete pod ${j} -n ctl
+done
+for j in $(uwskube get jobs -n ctl -o name); do
+	uwskube delete job ${j} -n ctl
 done
 exit 0
