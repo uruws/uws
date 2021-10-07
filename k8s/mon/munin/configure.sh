e231a8ce8c48508eb61c9ec055b31e503737ce12
Jeremías <jeremias@talkingpts.org>
2021-10-07 20:22:14 -0300

    k8smon munin smtps setup

--- k8s/mon/munin/configure.sh
+++ k8s/mon/munin/configure.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -eu
+# smtps CA
+CA=${HOME}/ca/uws/smtps/211006/client
+uwskube delete secret smtps-ca -n mon || true
+uwskube create secret generic smtps-ca -n mon \
+	--from-file="${CA}"
+
+# cluster.conf
 conf=/tmp/mon.munin.cluster.conf
 envsubst <~/k8s/mon/munin/conf.d/cluster.conf >${conf}
 uwskube delete configmap munin-confd -n mon || true
