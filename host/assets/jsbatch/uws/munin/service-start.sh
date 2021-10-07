891e388c60c9dce4659ebd9d0aa598516243102e
Jeremías <jeremias@talkingpts.org>
2021-10-07 17:57:40 -0300

    fix certs access

--- host/assets/jsbatch/uws/munin/service-start.sh
+++ host/assets/jsbatch/uws/munin/service-start.sh
@@ -6,13 +6,13 @@ exec /usr/bin/docker run --rm -u root \
 	--name uws-munin-service \
 	--hostname munin.uws.local \
 	--add-host docker.uws.local:${HOSTIP} \
-	-v ${CA}/client:/etc/opt/uws/ca:ro \
 	-v /srv/munin/var/lib:/var/lib/munin \
 	-v /srv/munin/var/alert:/var/opt/munin-alert \
 	-v /srv/munin/var/log:/var/log/munin \
 	-v /srv/munin/cache/www:/var/cache/munin/www \
 	-v /srv/etc/munin:/srv/etc/munin \
+	-v ${CA}/client:/srv/etc/ca:ro \
 	-e UWS_SMTPS='ops.uws.talkingpts.org' \
-	-e UWS_SMTPS_CERT='/etc/opt/uws/ca/08082dca-8d77-5c81-9a44-94642089b3b1.pem' \
-	-e UWS_SMTPS_KEY='/etc/opt/uws/ca/08082dca-8d77-5c81-9a44-94642089b3b1.key' \
+	-e UWS_SMTPS_CERT='/etc/opt/uws/ca/smtps.pem' \
+	-e UWS_SMTPS_KEY='/etc/opt/uws/ca/smtps.key' \
 	uws/munin
--- srv/munin/utils/bin/munin-start.sh
+++ srv/munin/utils/bin/munin-start.sh
@@ -15,6 +15,16 @@ chmod -v 0755 /var/log/munin
 mkdir -vp /var/opt/munin-alert
 chmod -v 1777 /var/opt/munin-alert
 
+if test -d /srv/etc/ca; then
+	install -v -d -m 0750 -o root -g munin /etc/opt/uws/ca
+	install -v -m 0640 -o root -g munin \
+		/srv/etc/ca/08082dca-8d77-5c81-9a44-94642089b3b1.pem \
+		/etc/opt/uws/ca/smtps.pem
+	install -v -m 0640 -o root -g munin \
+		/srv/etc/ca/08082dca-8d77-5c81-9a44-94642089b3b1.key \
+		/etc/opt/uws/ca/smtps.key
+fi
+
 if test -d /srv/etc/munin; then
 	cp -vrf /srv/etc/munin /etc
 fi
