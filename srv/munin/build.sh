e1d0aae3c84bbc5213ca4584c4e6348497907503
Jeremías <jeremias@talkingpts.org>
2021-10-07 14:40:04 -0300

    munin install uws sendmail lib

--- srv/munin/build.sh
+++ srv/munin/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 set -eu
+buildd=${PWD}/srv/munin/build
+mkdir -vp ${buildd}
+install -v -C ${PWD}/python/lib/sendmail.py ${buildd}/sendmail.py
 exec docker build $@ --rm -t uws/munin ./srv/munin
