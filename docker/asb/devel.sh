396d0563d46e2d59e20170f0d1dd799dbac190f2
Jeremías <jeremias@talkingpts.org>
2021-10-12 17:27:50 -0300

    asb create ec2 security group

--- docker/asb/devel.sh
+++ docker/asb/devel.sh
@@ -5,11 +5,12 @@ mkdir -vp ${TMPDIR}
 exec docker run -it --rm --name uws-ansible-devel \
 	--hostname ansible-devel.uws.local -u uws \
 	--read-only \
-	-e AWS_PROFILE=default \
+	-e AWS_PROFILE=uwsasb \
 	-e AWS_REGION=sa-east-1 \
 	-e ANSIBLE_CONFIG=/home/uws/asb/ansible.cfg \
 	-v ${TMPDIR}:/home/uws/tmp \
 	-v ${PWD}/asb:/home/uws/asb:ro \
+	-v ${PWD}/secret/asb/aws:/home/uws/.aws:ro \
 	--tmpfs /tmp:rw,mode=1777 \
 	--tmpfs /home/uws/.ansible/tmp:rw,mode=1777 \
 	--tmpfs /home/uws/.ansible/cp:rw,mode=1777 \
