6561abf393ad0cdafa28fc8ed5c6f9e80f2864b0
Jeremías <jeremias@talkingpts.org>
2021-10-12 15:17:39 -0300

    asb workdir

--- docker/asb/devel.sh
+++ docker/asb/devel.sh
@@ -5,8 +5,10 @@ mkdir -vp ${TMPDIR}
 exec docker run -it --rm --name uws-ansible-devel \
 	--hostname ansible-devel.uws.local -u uws \
 	--read-only \
-	-e ANSIBLE_CONFIG=/home/uws/asb/ansible.cfg \
+	-e AWS_PROFILE=default \
+	-e AWS_REGION=sa-east-1 \
 	-v ${TMPDIR}:/home/uws/tmp \
 	-v ${PWD}/asb:/home/uws/asb:ro \
+	--tmpfs /tmp:rw,mode=1777 \
 	--tmpfs /home/uws/.ansible/tmp:rw,mode=1777 \
 	uws/ansible
