46a0e7c3c9d8ee1a203a0e630f8656ef3107e846
Jeremías <jeremias@talkingpts.org>
2021-10-12 14:03:08 -0300

    base ansible config

--- docker/asb/devel.sh
+++ docker/asb/devel.sh
@@ -5,6 +5,7 @@ mkdir -vp ${TMPDIR}
 exec docker run -it --rm --name uws-ansible-devel \
 	--hostname ansible-devel.uws.local -u uws \
 	--read-only \
+	-e ANSIBLE_CONFIG=/home/uws/asb/ansible.cfg \
 	-v ${TMPDIR}:/home/uws/tmp \
 	-v ${PWD}/asb:/home/uws/asb:ro \
 	--tmpfs /home/uws/.ansible/tmp:rw,mode=1777 \
