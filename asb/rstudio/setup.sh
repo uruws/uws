c8a6f0c44fbefbe828aea04b3745f2967d4e0cdf
Jeremías <jeremias@talkingpts.org>
2021-10-12 22:22:33 -0300

    rstudio setup cmd args

--- asb/rstudio/setup.sh
+++ asb/rstudio/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -eu
-alias asbpl=ansible-playbook
-asbpl ./rstudio/ec2-key.yaml
-asbpl ./rstudio/ec2-vm.yaml
-exit 0
+exec ansible-playbook $@ \
+	./rstudio/ec2-key.yaml \
+	./rstudio/ec2-vm.yaml
