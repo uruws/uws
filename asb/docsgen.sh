2c21a7864016a383ca1dc8d1ef19b8d2586a80dc
Jeremías <jeremias@talkingpts.org>
2021-10-19 21:01:33 -0300

    improve asb docs gen

--- asb/docsgen.sh
+++ asb/docsgen.sh
@@ -3,21 +3,31 @@ set -eu
 mkdir -vp ~/tmp/ansible-doc
 cd ~/tmp/ansible-doc
 
-echo '*** l.txt'
-ansible-doc -l >l.txt
+echo '*** asbdoc.l'
+ansible-doc -l >asbdoc.l
 
-for m in $(grep -vE '^\w+\.' l.txt | cut -d ' ' -f 1); do
+for m in $(grep -vE '^\w+\.' asbdoc.l | cut -d ' ' -f 1); do
 	touch ${m}.txt
 done
 
-for m in $(grep -E '^community\.docker\.' l.txt | cut -d ' ' -f 1 | cut -d '.' -f 3); do
+for m in $(grep -E '^community\.general\.docker\.' asbdoc.l | cut -d ' ' -f 1); do
+	touch ${m}.txt
+done
+
+for m in $(grep -E '^community\.docker\.' asbdoc.l | cut -d ' ' -f 1); do
+	touch ${m}.txt
+done
+
+for m in $(grep -E '^community\.kubernetes\.' asbdoc.l | cut -d ' ' -f 1); do
 	touch ${m}.txt
 done
 
 for fn in $(ls *.txt); do
 	asbdoc=$(basename ${fn} .txt)
-	echo "*** ${asbdoc}"
-	ansible-doc ${asbdoc} >${fn}
+	echo "*** ${fn}"
+	if ! test -s ${fn}; then
+		ansible-doc ${asbdoc} >${fn}
+	fi
 done
 
 exit 0
