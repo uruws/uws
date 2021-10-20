54990f326ce39b2bc1de9c76dd142bccbe4f0f42
Jeremías <jeremias@talkingpts.org>
2021-10-20 15:06:47 -0300

    nlp logs max stream update

--- pod/nlp/sentiment/logs.sh
+++ pod/nlp/sentiment/logs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eu
-exec ~/pod/lib/logs.py -n nlp -l 'app.kubernetes.io/name=nlp-sentiment' --max 10 $@
+exec ~/pod/lib/logs.py -n nlp -l 'app.kubernetes.io/name=nlp-sentiment' --max 20 $@
