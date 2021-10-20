082547d765e8bfc13e61d98d2912ac42fa95c84b
Jeremías <jeremias@talkingpts.org>
2021-10-20 16:03:57 -0300

    asb access to srv definitions

--- docker/asb/run.sh
+++ docker/asb/run.sh
@@ -15,10 +15,11 @@ exec docker run -it --rm --name uws-ansible-${asbenv} \
 	-e ASBENV_FILENAME=/home/uws/asb/env/${asbenv}.env \
 	-v ${TMPDIR}:/home/uws/tmp \
 	-v ${PWD}/asb:/home/uws/asb:ro \
+	-v ${PWD}/docker/asb/files/ssh:/home/uws/.ssh:ro \
 	-v ${PWD}/secret/asb:/home/uws/secret:ro \
 	-v ${PWD}/secret/asb/aws:/home/uws/.aws:ro \
 	-v ${PWD}/secret/ca/uws:/home/uws/ca:ro \
-	-v ${PWD}/docker/asb/files/ssh:/home/uws/.ssh:ro \
+	-v ${PWD}/srv:/home/uws/srv:ro \
 	--tmpfs /tmp:rw,mode=1777 \
 	--tmpfs /home/uws/.ansible/tmp:rw,mode=1777 \
 	--tmpfs /home/uws/.ansible/cp:rw,mode=1777 \
