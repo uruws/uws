29ebf69a418d4421353a0dc2b2583beed6979382
Jeremías <jeremias@talkingpts.org>
2021-10-20 17:36:06 -0300

    acme pull docker image

--- srv/acme/bin/acme-cmd.sh
+++ srv/acme/bin/acme-cmd.sh
@@ -2,10 +2,9 @@
 set -eu
 ACME_HOME=/srv/acme
 ACME_RUN=/srv/run/acme
-ACME_IMG=789470191893.dkr.ecr.sa-east-1.amazonaws.com/uwsops:acme
 mkdir -vp ${ACME_HOME} ${ACME_RUN}
 exec docker run --rm --name uws-acme-cmd \
 	--hostname acme-cmd.uws.local \
 	-v ${ACME_HOME}:/srv/acme \
 	-v ${ACME_RUN}:/srv/run/acme \
-	-u uws ${ACME_IMG} $@
+	-u uws uwsops/acme $@
