1291fa577a41d7fb90455c2a74d7f8e7a6d32009
Jeremías <jeremias@talkingpts.org>
2021-10-21 02:16:50 -0300

    kali passwd

--- srv/kali/run.sh
+++ srv/kali/run.sh
@@ -3,6 +3,7 @@ set -eu
 exec docker run --rm \
 	--name uws-kali \
 	--hostname kali.uws.local \
+	-v ${PWD}/secret/kali:/root/etc \
 	--tmpfs /tmp:rw,mode=1777 \
 	-p 127.0.0.1:3327:3327 \
 	uws/kali /usr/local/bin/kali-start.sh
--- srv/kali/utils/kali-start.sh
+++ srv/kali/utils/kali-start.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -eu
+
+chpasswd </root/etc/passwd
+
 rm -vf /var/log/xrdp-sesman.log
 ln -sv /dev/stdout /var/log/xrdp-sesman.log
 xrdp-sesman -n &
+
 sleep 2
+
 rm -vf /var/log/xrdp.log
 ln -sv /dev/stdout /var/log/xrdp.log
 exec xrdp -n -p 3327
