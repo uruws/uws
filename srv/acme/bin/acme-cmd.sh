9bb4d60c95e6faf2f1500c51d463d208ef14cba3
Jeremías <jeremias@talkingpts.org>
2021-10-21 22:32:05 -0300

    docker base uwsops user

--- srv/acme/bin/acme-cmd.sh
+++ srv/acme/bin/acme-cmd.sh
@@ -7,4 +7,7 @@ exec docker run --rm --name uws-acme-cmd \
 	--hostname acme-cmd.uws.local \
 	-v ${ACME_HOME}:/srv/acme \
 	-v ${ACME_RUN}:/srv/run/acme \
-	-u uws uwsops/acme $@
+	--workdir /home/uwsops \
+	-e USER=uwsops \
+	-e HOME=/home/uwsops \
+	-u uwsops uwsops/acme $@
