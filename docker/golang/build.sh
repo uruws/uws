f4f8199e0939dc0b489a37ebdff4bb1f31fe070d
Jeremías <jeremias@talkingpts.org>
2021-10-27 17:48:56 -0300

    build munin using stable golang

--- docker/golang/build.sh
+++ docker/golang/build.sh
@@ -2,4 +2,9 @@
 set -eu
 mkdir -vp ./docker/golang/tmp
 install -C -v -m 640 ./go/go.mod ./go/go.sum ./docker/golang/tmp/
-exec docker build $@ --rm -t uws/golang ./docker/golang
+# golang
+docker build $@ --rm -t uws/golang ./docker/golang
+# golang-2109
+docker build $@ --rm -t uws/golang-2109 \
+	-f ./docker/golang/Dockerfile.2109 ./docker/golang
+exit 0
