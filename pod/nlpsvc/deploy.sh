017e34476cae2015410aa1747ff171444e3cb528
Jeremías <jeremias@talkingpts.org>
2021-11-05 16:00:55 -0300

    remove nlpsvc auto configure

--- pod/nlpsvc/deploy.sh
+++ pod/nlpsvc/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -eu
 version=${1:-''}
-~/pod/nlpsvc/configure.sh ${APP_ENV}
 exec ~/pod/lib/deploy.sh nlpsvc nlpsvc ${version}
--- pod/nlpsvc/restart.sh
+++ pod/nlpsvc/restart.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 set -eu
-~/pod/nlpsvc/configure.sh ${APP_ENV}
 exec uwskube rollout -n nlpsvc restart deploy/nlpsvc
