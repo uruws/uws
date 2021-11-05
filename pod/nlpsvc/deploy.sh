0d127b1da1cefc99bc2aa98d3dc2e5f7dadd2f31
Jeremías <jeremias@talkingpts.org>
2021-11-05 14:43:09 -0300

    nlpsvc APP_ENV config

--- pod/nlpsvc/deploy.sh
+++ pod/nlpsvc/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
 version=${1:-''}
+~/pod/nlpsvc/configure.sh ${APP_ENV}
 exec ~/pod/lib/deploy.sh nlpsvc nlpsvc ${version}
--- pod/nlpsvc/restart.sh
+++ pod/nlpsvc/restart.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -eu
+~/pod/nlpsvc/configure.sh ${APP_ENV}
 exec uwskube rollout -n nlpsvc restart deploy/nlpsvc
