affdcc91d0595a114d4bbebc2f18e5f6628991ca
Jeremías <jeremias@talkingpts.org>
2021-11-08 17:10:34 -0300

    jupyter notebook allow remote access

--- asb/roles/jupyter-notebook/files/jnb-start.sh
+++ asb/roles/jupyter-notebook/files/jnb-start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
 exec jupyter notebook --ip=127.0.0.1 --port=8888 --port-retries=1 \
-	--no-browser --autoreload --notebook-dir=/srv/home/jupyter
+	--no-browser --autoreload --notebook-dir=/srv/home/jupyter \
+	--NotebookApp.allow_remote_access=True
