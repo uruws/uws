e07ed80f0f9ec525d289078533d7ba207c550c45
Jeremías <jeremias@talkingpts.org>
2021-11-08 11:57:05 -0300

    try to reduce default nginx tls site complexity

--- asb/roles/jupyter-notebook/files/jnb-start.sh
+++ asb/roles/jupyter-notebook/files/jnb-start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -eu
 exec jupyter notebook --ip=127.0.0.1 --port=8888 --port-retries=1 \
-	--no-browser --autoreload --notebook-dir=/srv/home/jupyter \
-	--NotebookApp.base_url=/jnb/
+	--no-browser --autoreload --notebook-dir=/srv/home/jupyter
