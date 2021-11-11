d556dfca4ecc5906fe750be6b02892d34f7bb152
Jeremías <jeremias@talkingpts.org>
2021-11-11 15:40:53 -0300

    uwscli lib uwshelp

---
+++ host/assets/jsbatch/srv/home/uwscli/lib/uwshelp.py
@@ -0,0 +1,47 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from argparse import ArgumentParser
+from subprocess import check_output
+
+import uwscli
+
+def __out_split(out):
+	lmax = 0
+	lines = {}
+	for l in out.splitlines():
+		i = l.split(':', 1)
+		lines[i[0]] = ':'.join(i[1:])
+		llen = len(i[0])
+		if llen > lmax:
+			lmax = llen
+	return (lmax, lines)
+
+def all_help():
+	cmd = "grep -sE '^__doc__' %s/*" % uwscli.bindir
+	cmd += """ | sed "s/'//g" | sed "s#"""
+	cmd += "%s\/" % uwscli.bindir
+	cmd += '##" | sed "s/__doc__ = //"'
+	out = check_output(cmd, shell = True, text = True)
+	lmax, lines = __out_split(out)
+	print('uws command line interface')
+	print('')
+	print('available commands:')
+	print('')
+	for c in lines.keys():
+		print('  ', c, ' '*(lmax - len(c)), ' - ', lines[c], sep = '')
+	print('')
+	print('run `uwshelp cmd` to get more information about cmd usage')
+	return 0
+
+def main(argv = []):
+	flags = ArgumentParser(description = 'show uwscli commands help')
+	flags.add_argument('cmd', metavar = 'cmd', default = '', nargs = '?',
+		help = 'command')
+
+	args = flags.parse_args(argv)
+
+	if args.cmd == '':
+		return all_help()
+
+	return uwscli.system("%s/%s --help" % (uwscli.bindir, args.cmd))
