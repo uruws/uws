af699667f4723744da079b3cd8630f7018c42c7f
Jeremías <jeremias@talkingpts.org>
2021-11-11 13:34:04 -0300

    uwscli lib app-events

---
+++ cli/test/app-events_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+import unittest
+import uwscli_t
+
+import app_events
+
+class Test(unittest.TestCase):
+
+	def test_main_no_args(t):
+		with t.assertRaises(SystemExit) as e:
+			app_events.main()
+		err = e.exception
+		t.assertEqual(err.args[0], 2)
+
+	def test_main(t):
+		t.assertEqual(app_events.main(['testing']), 127)
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ host/assets/jsbatch/srv/home/uwscli/lib/app_events.py
@@ -0,0 +1,24 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+import uwscli
+
+def main(argv = []):
+	flags = ArgumentParser(formatter_class = RawDescriptionHelpFormatter,
+		description = __doc__, epilog = uwscli.app_description())
+	flags.add_argument('-w', '--watch', action = 'store_true', default = False,
+		help = 'watch for new messages')
+	flags.add_argument('app', metavar = 'app', choices = uwscli.app_list(),
+		default = 'app', help = 'app name')
+
+	args = flags.parse_args(argv)
+
+	cmd_args = "%s %s events" % (uwscli.app[args.app].cluster,
+		uwscli.app[args.app].pod)
+
+	if args.watch:
+		cmd_args += ' -w'
+
+	return uwscli.run('app-cli.sh', cmd_args)
