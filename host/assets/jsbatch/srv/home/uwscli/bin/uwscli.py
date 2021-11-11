8f292909d9bd7d86a3d5afaf6f2d0f96ce269904
Jeremías <jeremias@talkingpts.org>
2021-11-11 00:24:56 -0300

    split uwscli_conf

--- host/assets/jsbatch/srv/home/uwscli/bin/uwscli.py
+++ host/assets/jsbatch/srv/home/uwscli/bin/uwscli.py
@@ -1,110 +1,12 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
-from os import path, system, getenv
+from os import path, getenv, system
 
 bindir = path.abspath(path.dirname(__file__))
-cmddir = getenv('UWSCLI_CMDDIR', '/srv/uws/deploy/cli')
-docker_storage = '/srv/docker/lib'
-docker_storage_min = 10*1024*1024 # 10G
+_user = getenv('USER', 'unknown')
 
-class App(object):
-	def __init__(self, app, cluster = None, desc = None, pod = None, build = None, deploy = None):
-		self.app = app
-		self.cluster = cluster
-		self.desc = desc
-		self.pod = pod
-		self.build = build
-		self.deploy = deploy
-
-class AppBuild(object):
-	def __init__(self, dir, script, type = 'cli', src = '.', target = None, ref = 'origin/HEAD'):
-		self.dir = dir
-		self.script = script
-		self.type = type
-		self.src = src
-		self.target = target
-		self.ref = ref
-
-class AppDeploy(object):
-	def __init__(self, image, filter = None):
-		self.image = image
-		self.filter = filter
-		if self.filter is None:
-			self.filter = "%s-" % image
-
-def _buildpack(src, target):
-	return AppBuild(
-		'/srv/deploy/Buildpack',
-		'build.py',
-		type = 'pack',
-		src = src,
-		target = target,
-	)
-
-app = {
-	'app': App(False,
-		desc = 'App web and workers',
-		build = _buildpack('app/src', 'app'),
-	),
-	'app-east': App(True,
-		cluster = 'amy-east',
-		desc = 'App web, east cluster',
-		pod = 'meteor/web',
-		deploy = AppDeploy('meteor-app'),
-	),
-	'app-west': App(True,
-		cluster = 'amy-west',
-		desc = 'App web, west cluster',
-		pod = 'meteor/web',
-		deploy = AppDeploy('meteor-app'),
-	),
-	'worker': App(True,
-		cluster = 'amy-wrkr',
-		desc = 'App worker',
-		pod = 'meteor/worker',
-		deploy = AppDeploy('meteor-app'),
-	),
-	'beta': App(True,
-		cluster = 'amybeta',
-		desc = 'App beta',
-		pod = 'meteor/beta',
-		build = _buildpack('beta/src', 'beta'),
-		deploy = AppDeploy('meteor-beta'),
-	),
-	'cs': App(True,
-		cluster = 'amybeta',
-		desc = 'Crowdsourcing',
-		pod = 'meteor/cs',
-		build = _buildpack('cs/src', 'crowdsourcing'),
-		deploy = AppDeploy('meteor-crowdsourcing'),
-	),
-	'nlpsvc': App(True,
-		cluster = 'panoramix',
-		desc = 'NLPService',
-		pod = 'nlpsvc',
-		build = AppBuild('/srv/deploy/NLPService', 'build.sh'),
-		deploy = AppDeploy('nlpsvc'),
-	),
-}
-
-cluster = {
-	'amy-east': {
-		'region': 'us-east-1',
-	},
-	'amy-west': {
-		'region': 'us-west-1',
-	},
-	'amy-wrkr': {
-		'region': 'us-east-1',
-	},
-	'amybeta': {
-		'region': 'us-east-2',
-	},
-	'panoramix': {
-		'region': 'us-east-1',
-	},
-}
+from uwscli_conf import app, cluster, cmddir, docker_storage, docker_storage_min
 
 def __descmax(k):
 	m = 0
@@ -144,8 +46,6 @@ def deploy_list():
 
 def deploy_description():
 	return __desc(deploy_list())
-
-_user = getenv('USER', 'unknown')
 
 def ctl(args):
 	return system("/usr/bin/sudo -H -n -u uws -- %s/app-ctl.sh %s %s" % (cmddir, _user, args))
---
+++ host/assets/jsbatch/srv/home/uwscli/bin/uwscli_conf.py
@@ -0,0 +1,106 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from os import path, getenv
+
+cmddir = getenv('UWSCLI_CMDDIR', '/srv/uws/deploy/cli')
+docker_storage = '/srv/docker/lib'
+docker_storage_min = 10*1024*1024 # 10G
+
+class App(object):
+	def __init__(self, app, cluster = None, desc = None, pod = None, build = None, deploy = None):
+		self.app = app
+		self.cluster = cluster
+		self.desc = desc
+		self.pod = pod
+		self.build = build
+		self.deploy = deploy
+
+class AppBuild(object):
+	def __init__(self, dir, script, type = 'cli', src = '.', target = None, ref = 'origin/HEAD'):
+		self.dir = dir
+		self.script = script
+		self.type = type
+		self.src = src
+		self.target = target
+		self.ref = ref
+
+class AppDeploy(object):
+	def __init__(self, image, filter = None):
+		self.image = image
+		self.filter = filter
+		if self.filter is None:
+			self.filter = "%s-" % image
+
+def _buildpack(src, target):
+	return AppBuild(
+		'/srv/deploy/Buildpack',
+		'build.py',
+		type = 'pack',
+		src = src,
+		target = target,
+	)
+
+app = {
+	'app': App(False,
+		desc = 'App web and workers',
+		build = _buildpack('app/src', 'app'),
+	),
+	'app-east': App(True,
+		cluster = 'amy-east',
+		desc = 'App web, east cluster',
+		pod = 'meteor/web',
+		deploy = AppDeploy('meteor-app'),
+	),
+	'app-west': App(True,
+		cluster = 'amy-west',
+		desc = 'App web, west cluster',
+		pod = 'meteor/web',
+		deploy = AppDeploy('meteor-app'),
+	),
+	'worker': App(True,
+		cluster = 'amy-wrkr',
+		desc = 'App worker',
+		pod = 'meteor/worker',
+		deploy = AppDeploy('meteor-app'),
+	),
+	'beta': App(True,
+		cluster = 'amybeta',
+		desc = 'App beta',
+		pod = 'meteor/beta',
+		build = _buildpack('beta/src', 'beta'),
+		deploy = AppDeploy('meteor-beta'),
+	),
+	'cs': App(True,
+		cluster = 'amybeta',
+		desc = 'Crowdsourcing',
+		pod = 'meteor/cs',
+		build = _buildpack('cs/src', 'crowdsourcing'),
+		deploy = AppDeploy('meteor-crowdsourcing'),
+	),
+	'nlpsvc': App(True,
+		cluster = 'panoramix',
+		desc = 'NLPService',
+		pod = 'nlpsvc',
+		build = AppBuild('/srv/deploy/NLPService', 'build.sh'),
+		deploy = AppDeploy('nlpsvc'),
+	),
+}
+
+cluster = {
+	'amy-east': {
+		'region': 'us-east-1',
+	},
+	'amy-west': {
+		'region': 'us-west-1',
+	},
+	'amy-wrkr': {
+		'region': 'us-east-1',
+	},
+	'amybeta': {
+		'region': 'us-east-2',
+	},
+	'panoramix': {
+		'region': 'us-east-1',
+	},
+}
