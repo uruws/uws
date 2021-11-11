528a28448ccadd0f3d8631a4edd4ddc850f19b53
Jeremías <jeremias@talkingpts.org>
2021-11-11 18:25:57 -0300

    html coverage report

--- cli/test/coverage.sh
+++ cli/test/coverage.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 set -eu
+
 export PYTHONPATH=${HOME}/lib:${HOME}/test
+
+covrc=/tmp/coveragerc
+
 cd ${HOME}
+rm -vf ${covrc}
+touch ${covrc}
 for t in $(ls test/*_test.py); do
 	echo "*** ${t}"
-	python3-coverage run ${t} $@
+	python3-coverage run --append --rcfile=${covrc} ${t} $@
 done
-python3-coverage html
+covd=~/tmp/htmlcov
+rm -rf ${covd}
+python3-coverage html -d ${covd} --rcfile=${covrc}
 exit 0
--- docker/uwscli/devel.sh
+++ docker/uwscli/devel.sh
@@ -3,6 +3,8 @@ set -eu
 
 CLI_HOME=${PWD}/host/assets/jsbatch/srv/home/uwscli
 
+install -v -d -m 1777 ${PWD}/tmp/uwscli
+
 exec docker run -it --rm --name uwscli-devel \
 	--hostname devel.uwscli.local \
 	-e PYTHONPATH=/srv/home/uwscli/lib \
@@ -10,4 +12,5 @@ exec docker run -it --rm --name uwscli-devel \
 	-v ${CLI_HOME}/etc:/srv/home/uwscli/etc:ro \
 	-v ${CLI_HOME}/lib:/srv/home/uwscli/lib:ro \
 	-v ${PWD}/cli/test:/srv/home/uwscli/test:ro \
+	-v ${PWD}/tmp/uwscli:/srv/home/uwscli/tmp:rw \
 	-u uwscli uws/cli
