8d8be801d4e57ae3f27514593596810cc2ba4228
Jeremías <jeremias@talkingpts.org>
2021-11-12 14:38:40 -0300

    test app-autobuild lastTag

--- cli/test/app-autobuild_test.py
+++ cli/test/app-autobuild_test.py
@@ -6,12 +6,20 @@
 import unittest
 import uwscli_t
 
+import uwscli
 import app_autobuild
 
 class Test(unittest.TestCase):
 
 	def setUp(t):
 		uwscli_t.mock()
+
+	def test_lastTag(t):
+		with uwscli_t.mock_gso():
+			rc, out = app_autobuild.lastTag('testing')
+			t.assertEqual(rc, 0)
+			t.assertEqual(out, 'mock_output')
+			uwscli.gso.assert_called_once_with('git -C . describe --abbrev=0 --tags origin/HEAD')
 
 	def test_main_no_args(t):
 		with t.assertRaises(SystemExit) as e:
