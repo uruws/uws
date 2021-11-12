014507f1cc61688c109d9d45594906f7be72f100
Jeremías <jeremias@talkingpts.org>
2021-11-11 22:21:33 -0300

    fix docker -it

---
+++ docker/uwscli/cmd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+
+CLI_HOME=${PWD}/host/assets/jsbatch/srv/home/uwscli
+
+mkdir -vp ${PWD}/tmp/uwscli
+
+exec docker run --rm --name uwscli \
+	--hostname cli.uws.local \
+	-e PYTHONPATH=/srv/home/uwscli/lib \
+	-v ${CLI_HOME}/bin:/srv/home/uwscli/bin:ro \
+	-v ${CLI_HOME}/etc:/srv/home/uwscli/etc:ro \
+	-v ${CLI_HOME}/lib:/srv/home/uwscli/lib:ro \
+	-v ${PWD}/cli/test:/home/uws/test:ro \
+	-v ${PWD}/tmp/uwscli:/home/uws/tmp:rw \
+	uws/cli $@
