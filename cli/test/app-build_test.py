fe92b3731e02c4833ff3435adc6825034ac19ef1
Jeremías <jeremias@talkingpts.org>
2021-11-12 01:49:26 -0300

    test app-build check storage errors

--- cli/test/app-build_test.py
+++ cli/test/app-build_test.py
@@ -27,6 +27,16 @@ class Test(unittest.TestCase):
 		with uwscli_t.mock_gso(output = '100'):
 			t.assertEqual(app_build.check_storage(), 0)
 			uwscli.gso.assert_called_once_with("df -kl /srv/docker | tail -n1 | awk '{ print $4 }'")
+		with uwscli_t.mock_gso(output = '3'):
+			t.assertEqual(app_build.check_storage(), 8)
+
+	def test_check_storage_error(t):
+		with uwscli_t.mock_gso(output = 'testing'):
+			t.assertEqual(app_build.check_storage(), 9)
+		t.assertEqual(uwscli_t.err().strip(), 'value error: testing')
+		with uwscli_t.mock_gso(status = 2, output = 'mock_error'):
+			t.assertEqual(app_build.check_storage(), 2)
+		t.assertEqual(uwscli_t.err().strip(), 'mock_error')
 
 if __name__ == '__main__':
 	unittest.main()
