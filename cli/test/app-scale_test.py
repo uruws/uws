08f1408d98fde12ecfc86c258c70129abccc6141
Jeremías <jeremias@talkingpts.org>
2021-11-12 13:58:03 -0300

    test app-scale

--- cli/test/app-scale_test.py
+++ cli/test/app-scale_test.py
@@ -6,6 +6,7 @@
 import unittest
 import uwscli_t
 
+import uwscli
 import app_scale
 
 class Test(unittest.TestCase):
@@ -19,8 +20,17 @@ class Test(unittest.TestCase):
 		err = e.exception
 		t.assertEqual(err.args[0], 2)
 
+	def test_main_errors(t):
+		with uwscli_t.mock_system(99):
+			t.assertEqual(app_scale.main(['testing', '999']), 99)
+		with uwscli_t.mock_system():
+			t.assertEqual(app_scale.main(['testing', '0']), 9)
+			t.assertEqual(uwscli_t.err().strip(), 'invalid number of replicas: 0')
+
 	def test_main(t):
-		t.assertEqual(app_scale.main(['testing', '999']), 127)
+		with uwscli_t.mock_system():
+			t.assertEqual(app_scale.main(['testing', '99']), 0)
+			uwscli.system.assert_called_once_with('/usr/bin/sudo -H -n -u uws -- /srv/uws/deploy/cli/app-ctl.sh uws ktest test scale 99')
 
 if __name__ == '__main__':
 	unittest.main()
