c21b265517e307078a58895ef886b2f51a94ab5c
Jeremías <jeremias@talkingpts.org>
2021-11-11 23:05:02 -0300

    per test coverage report

--- cli/test/coverage.sh
+++ cli/test/coverage.sh
@@ -3,21 +3,24 @@ set -eu
 
 export PYTHONPATH=/srv/home/uwscli/lib:${HOME}/test
 
-covrc=/tmp/coveragerc
-
 cd ${HOME}
 
-rm -vf ${covrc}
-touch ${covrc}
+rm -vf .coverage
 
-for t in $(ls test/*_test.py); do
-	echo "*** ${t}"
-	python3-coverage run --append --rcfile=${covrc} ${t} $@
-done
+testfn=${1:-''}
+if test "X${testfn}" != 'X'; then
+	shift
+	python3-coverage run ${testfn} $@
+else
+	for t in $(ls test/*_test.py); do
+		echo "*** ${t}"
+		python3-coverage run --append ${t} $@
+	done
+fi
 
 covd=${HOME}/tmp/htmlcov
 rm -rf ${covd}
 
-python3-coverage html -d ${covd} --rcfile=${covrc}
+python3-coverage html -d ${covd}
 
 exit 0
