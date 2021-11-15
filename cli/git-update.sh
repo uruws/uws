32962db798fe582a529eadad68dd5d63a8af3681
Jeremías <jeremias@talkingpts.org>
2021-11-15 15:37:42 -0300

    try to make shellcheck happy enough

--- cli/git-update.sh
+++ cli/git-update.sh
@@ -5,24 +5,24 @@ refname="$1"
 oldrev="$2"
 newrev="$3"
 
-if [ -z "$refname" -o -z "$oldrev" -o -z "$newrev" ]; then
+if [ -z "$refname" ] || [ -z "$oldrev" ] || [ -z "$newrev" ]; then
 	echo "usage: $0 <ref> <oldrev> <newrev>" >&2
 	exit 1
 fi
 
 # --- Check types
 # if $newrev is 0000...0000, it's a commit to delete a ref.
-zero=$(git hash-object --stdin </dev/null | tr '[0-9a-f]' '0')
+zero=$(git hash-object --stdin </dev/null | tr '0-9a-f' '0')
 if [ "$newrev" = "$zero" ]; then
 	newrev_type=delete
 else
-	newrev_type=$(git cat-file -t $newrev)
+	newrev_type=$(git cat-file -t "${newrev}")
 fi
 
 case "$refname","$newrev_type" in
 	refs/tags/*,commit)
 		# un-annotated tag
-		short_refname=${refname##refs/tags/}
+		short_refname="${refname##refs/tags/}"
 		echo "*** The un-annotated tag, $short_refname, is not allowed in this repository" >&2
 		echo "*** Use 'git tag [ -a | -s ]' for tags you want to propagate." >&2
 		exit 1
@@ -34,15 +34,15 @@ case "$refname","$newrev_type" in
 	;;
 	refs/tags/*,tag)
 		# annotated tag
-		if git rev-parse $refname > /dev/null 2>&1
+		if git rev-parse "${refname}" > /dev/null 2>&1
 		then
 			echo "*** Tag '$refname' already exists." >&2
 			echo "*** Modifying a tag is not allowed in this repository." >&2
 			exit 1
 		fi
 		export NQDIR=${HOME}/nq
-		mkdir -p -m 0750 ${NQDIR}
-		nq -c -- /srv/uws/deploy/cli/git-update-run.sh ${PWD} ${refname}
+		mkdir -p -m 0750 "${NQDIR}"
+		nq -c -- /srv/uws/deploy/cli/git-update-run.sh "${PWD}" "${refname}"
 	;;
 esac
 
