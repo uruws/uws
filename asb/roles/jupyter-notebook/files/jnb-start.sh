9c223ec22513fffa17811ad0234d9fe6ebda2c57
Jeremías <jeremias@talkingpts.org>
2021-11-22 17:52:44 -0300

    jnb-start install ipython3 kernel

--- asb/roles/jupyter-notebook/files/jnb-start.sh
+++ asb/roles/jupyter-notebook/files/jnb-start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
+ipython3 kernel install --user
 exec jupyter notebook --ip=127.0.0.1 --port=8888 --port-retries=1 \
 	--no-browser --autoreload --notebook-dir=/srv/home/jupyter \
 	--NotebookApp.allow_remote_access=True
