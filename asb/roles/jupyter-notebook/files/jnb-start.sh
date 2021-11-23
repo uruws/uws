6d6d315f13681e0394f154dc6e1f72fcd87e4bb2
Jeremías <jeremias@talkingpts.org>
2021-11-23 19:34:40 -0300

    fix home, trust xheaders and install kernel only if not there

--- asb/roles/jupyter-notebook/files/jnb-start.sh
+++ asb/roles/jupyter-notebook/files/jnb-start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
-ipython3 kernel install --user
-exec jupyter notebook --ip=127.0.0.1 --port=8888 --port-retries=1 \
-	--no-browser --autoreload --notebook-dir=/srv/home/jupyter \
+if ! test -d ~/.local/share/jupyter/kernels/python3; then
+	ipython3 kernel install --user
+fi
+exec jupyter-notebook --ip=127.0.0.1 --port=8888 --port-retries=1 \
+	--no-browser --autoreload --notebook-dir=/srv/home \
+	--NotebookApp.trust_xheaders=True \
 	--NotebookApp.allow_remote_access=True
