d8d7cf44e97b187067bbc6aeb9e8baf1cd98af75
Jeremías <jeremias@talkingpts.org>
2021-11-29 11:55:14 -0300

    test semver vendor package

--- docker/uwscli/devel.sh
+++ docker/uwscli/devel.sh
@@ -3,7 +3,8 @@ set -eu
 
 CLI_HOME=${PWD}/host/assets/jsbatch/srv/home/uwscli
 
-mkdir -vp ${PWD}/tmp/uwscli
+mkdir -vp ${PWD}/tmp/uwscli ${PWD}/tmp/uwscli/pytest_cache
+mkdir -vp ${CLI_HOME}/vendor/semver-2.13.0/.pytest_cache
 
 exec docker run -it --rm --name uwscli-devel \
 	--hostname clidev.uws.local \
@@ -11,8 +12,10 @@ exec docker run -it --rm --name uwscli-devel \
 	-v ${CLI_HOME}/bin:/srv/home/uwscli/bin:ro \
 	-v ${CLI_HOME}/etc:/srv/home/uwscli/etc:ro \
 	-v ${CLI_HOME}/lib:/srv/home/uwscli/lib:ro \
+	-v ${CLI_HOME}/vendor:/srv/home/uwscli/vendor:ro \
 	-v ${PWD}/cli:/srv/uws/deploy/cli:ro \
 	-v ${PWD}/cli/test:/home/uws/test:ro \
 	-v ${PWD}/cli/testdata:/home/uws/testdata:ro \
 	-v ${PWD}/tmp/uwscli:/home/uws/tmp:rw \
+	-v ${PWD}/tmp/uwscli/pytest_cache:/srv/home/uwscli/vendor/semver-2.13.0/.pytest_cache:rw \
 	uws/cli $@
