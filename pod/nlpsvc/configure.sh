68ef92098760428bac1b4cfa90855e79e0ee2bee
Jeremías <jeremias@talkingpts.org>
2021-12-01 16:07:48 -0300

    nlpsvc 1.2 deploy

--- pod/nlpsvc/configure.sh
+++ pod/nlpsvc/configure.sh
@@ -2,6 +2,12 @@
 set -eu
 appenv=${1:?'app env?'}
 envf=${HOME}/secret/nlpsvc/${appenv}.env
+
 uwskube delete secret -n nlpsvc nlpsvc-env || true
 uwskube create secret generic -n nlpsvc nlpsvc-env --from-env-file="${envf}"
+
+uwskube delete secret -n nlpsvc etcdir || true
+uwskube create secret generic -n nlpsvc etcdir \
+	--from-file=${HOME}/secret/nlpsvc/etc/
+
 exit 0
