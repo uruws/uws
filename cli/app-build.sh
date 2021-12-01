182195a9f6389de0d54b44d8a2fcbbfcf155b4bf
Jeremías <jeremias@talkingpts.org>
2021-12-01 12:02:45 -0300

    app build status dir

--- cli/app-build.sh
+++ cli/app-build.sh
@@ -14,6 +14,9 @@ logf=${logs_dir}/${app}-build-${version_tag}.log
 mkdir -vp "${logs_dir}"
 date -R >"${logf}"
 
+status_dir=/run/uwscli/build
+statusf=${status_dir}/${app}.status
+
 build_cmd=${build_dir}/${build_script}
 if test "$(dirname ${build_dir})" != '/srv/deploy'; then
 	echo "ERROR: invalid cmd ${build_cmd}" | tee -a "${logf}"
@@ -29,4 +32,16 @@ if ! test -x "./${build_script}"; then
 	exit 8
 fi
 
-exec ./"${build_script}" | tee -a "${logf}"
+set +e
+
+echo 'BUILD' >${statusf}
+./"${build_script}" | tee -a "${logf}"
+rc=$?
+
+if test "X${rc}" != 'X0'; then
+	echo 'FAIL' >${statusf}
+else
+	echo 'OK' >${statusf}
+fi
+
+exit ${rc}
