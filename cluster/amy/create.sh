3be34b8acd298f67e93c4323ffffcbd0dc156171
Jeremías <jeremias@talkingpts.org>
2021-12-06 13:55:29 -0300

    eks create k8s version arg

--- cluster/amy/create.sh
+++ cluster/amy/create.sh
@@ -2,7 +2,7 @@
 set -eu
 
 uwseks-cluster-create --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
-	--zones "${AWS_ZONES}" \
+	--zones "${AWS_ZONES}" --k8s-version "${K8S_VERSION}" \
 	--nodes 10 --nodes-min 5 --nodes-max 100 \
 	--instance-types "${AWS_INSTANCE_TYPES}" \
 	"${UWS_CLUSTER}"
