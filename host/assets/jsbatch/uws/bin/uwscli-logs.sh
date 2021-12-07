7049de10127269aa2ab83ac1396a4c3a19b376ff
Jeremías <jeremias@talkingpts.org>
2021-12-07 13:41:28 -0300

    update uwscli logs compresser

--- host/assets/jsbatch/uws/bin/uwscli-logs.sh
+++ host/assets/jsbatch/uws/bin/uwscli-logs.sh
@@ -2,13 +2,23 @@
 set -eu
 
 find ~/logs/ -type f -name '*-build-*.log' -mtime +2 -exec gzip -9 {} \;
+find ~/logs/ -type f -name 'deploy-*.log' -mtime +2 -exec gzip -9 {} \;
 
 if test -s ~/logs/app-ctl.log; then
 	# run on sundays only
 	if test 'X0' = "X$(date '+%w')"; then
 		dst=app-ctl-$(date '+%y%m%d').log
-		mv -f ~/logs/app-ctl.log ~/logs/${dst}
-		gzip -9 ~/logs/${dst}
+		mv -f ~/logs/app-ctl.log "${HOME}/logs/${dst}"
+		gzip -9 "${HOME}/logs/${dst}"
+	fi
+fi
+
+if test -s ~/logs/uwsq.log; then
+	# run on sundays only
+	if test 'X0' = "X$(date '+%w')"; then
+		dst=uwsq-$(date '+%y%m%d').log
+		mv -f ~/logs/uwsq.log "${HOME}/logs/${dst}"
+		gzip -9 "${HOME}/logs/${dst}"
 	fi
 fi
 
