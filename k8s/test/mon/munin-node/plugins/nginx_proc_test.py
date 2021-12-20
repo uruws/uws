9fa0417a1b92c24649858b1957aecd565c32d735
Jeremías <jeremias@talkingpts.org>
2021-12-20 19:57:17 -0300

    test nginx_proc

---
+++ k8s/test/mon/munin-node/plugins/nginx_proc_test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+import unittest
+from unittest.mock import MagicMock, call
+
+import mon_t
+
+import nginx_proc
+
+_bup_print = nginx_proc._print
+_bup_sts = nginx_proc.sts
+
+class Test(unittest.TestCase):
+
+	def setUp(t):
+		mon_t.setUp()
+		nginx_proc._print = MagicMock()
+		nginx_proc.sts = _bup_sts.copy()
+
+	def tearDown(t):
+		mon_t.tearDown()
+		nginx_proc._print = _bup_print
+
+	def test_globals(t):
+		t.assertDictEqual(nginx_proc.sts, {
+			'byte': {
+				'read': 'U',
+				'write': 'U',
+			},
+			'cpu': {
+				'controller': 'U',
+				'total': 'U',
+			},
+			'mem': {
+				'resident': 'U',
+				'virtual': 'U',
+			},
+			'requests': 'U',
+			'uptime': 'U',
+		})
+
+	def test_print(t):
+		_bup_print('test', 'ing')
+
+if __name__ == '__main__':
+	unittest.main()
