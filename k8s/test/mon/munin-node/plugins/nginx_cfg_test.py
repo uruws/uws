b75d28bdf79303e5c6fa3cdbf97eafd669aec9ee
Jeremías <jeremias@talkingpts.org>
2021-12-20 19:18:55 -0300

    test nginx_cfg config

--- k8s/test/mon/munin-node/plugins/nginx_cfg_test.py
+++ k8s/test/mon/munin-node/plugins/nginx_cfg_test.py
@@ -4,7 +4,7 @@
 # See LICENSE file.
 
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
 import mon_t
 
@@ -43,6 +43,43 @@ class Test(unittest.TestCase):
 			'config_last_reload_successful': 'U',
 			'config_last_reload_successful_timestamp_seconds': 'U',
 		})
+
+	def test_config(t):
+		nginx_cfg.config({})
+		config = [
+			call('multigraph nginx_cfg_hash'),
+			call('graph_title Running config hash'),
+			call('graph_args --base 1000 -l 0'),
+			call('graph_category nginx_cfg'),
+			call('graph_vlabel number'),
+			call('graph_scale yes'),
+			call('hash.label hash'),
+			call('hash.colour COLOUR0'),
+			call('hash.min 0'),
+			call('multigraph nginx_cfg_reload'),
+			call('graph_title Config reload status'),
+			call('graph_args --base 1000 -l 0 -u 1'),
+			call('graph_category nginx_cfg'),
+			call('graph_vlabel successful reload'),
+			call('graph_scale yes'),
+			call('reload.label reload'),
+			call('reload.colour COLOUR0'),
+			call('reload.draw AREA'),
+			call('reload.min 0'),
+			call('reload.max 1'),
+			call('multigraph nginx_cfg_uptime'),
+			call('graph_title Config uptime since last reload'),
+			call('graph_args --base 1000 -l 0'),
+			call('graph_category nginx_cfg'),
+			call('graph_vlabel hours'),
+			call('graph_scale no'),
+			call('uptime.label uptime'),
+			call('uptime.colour COLOUR0'),
+			call('uptime.draw AREA'),
+			call('uptime.min 0'),
+		]
+		nginx_cfg._print.assert_has_calls(config)
+		t.assertEqual(nginx_cfg._print.call_count, len(config))
 
 if __name__ == '__main__':
 	unittest.main()
