576b92fa6665c82e161574f3ef484f05405cca2c
Jeremías <jeremias@talkingpts.org>
2021-12-20 17:52:45 -0300

    test nginx_cfg print

--- k8s/test/mon/munin-node/plugins/nginx_cfg_test.py
+++ k8s/test/mon/munin-node/plugins/nginx_cfg_test.py
@@ -4,10 +4,23 @@
 # See LICENSE file.
 
 import unittest
+from unittest.mock import MagicMock
+
+import mon_t
 
 import nginx_cfg
 
+_bup_print = nginx_cfg._print
+
 class Test(unittest.TestCase):
+
+	def setUp(t):
+		mon_t.setUp()
+		nginx_cfg._print = MagicMock()
+
+	def tearDown(t):
+		mon_t.tearDown()
+		nginx_cfg._print = _bup_print
 
 	def test_globals(t):
 		t.assertDictEqual(nginx_cfg.sts, {
@@ -15,6 +28,9 @@ class Test(unittest.TestCase):
 			'config_last_reload_successful': 'U',
 			'config_last_reload_successful_timestamp_seconds': 'U',
 		})
+
+	def test_print(t):
+		_bup_print('test', 'ing')
 
 if __name__ == '__main__':
 	unittest.main()
