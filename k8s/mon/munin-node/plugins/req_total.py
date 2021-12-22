6deb4849b68a726840bc7532d5585f1f19226649
Jeremías <jeremias@talkingpts.org>
2021-12-22 14:52:04 -0300

    test mon req_total

--- k8s/mon/munin-node/plugins/req_total.py
+++ k8s/mon/munin-node/plugins/req_total.py
@@ -3,104 +3,107 @@
 
 import mon
 
+def _print(*args):
+	print(*args)
+
 def config(cluster, host, hostid, sts):
 	# total index
-	print(f"multigraph web_request_{hostid}")
-	print(f"graph_title {host} request")
-	print('graph_args --base 1000 -l 0')
-	print('graph_category web')
-	print('graph_vlabel total number')
-	print('graph_scale yes')
-	print('graph_total', cluster, 'total')
+	_print(f"multigraph web_request_{hostid}")
+	_print(f"graph_title {host} request")
+	_print('graph_args --base 1000 -l 0')
+	_print('graph_category web')
+	_print('graph_vlabel total number')
+	_print('graph_scale yes')
+	_print('graph_total', cluster, 'total')
 	fid = 0
 	for path in sorted(sts.keys()):
 		pathid = mon.cleanfn(path)
-		for method in sorted(sts[path].keys()):
+		for method in sorted(sts.get(path, {}).keys()):
 			methid = mon.cleanfn(method)
-			for status in sorted(sts[path][method].keys()):
+			for status in sorted(sts[path].get(method, {}).keys()):
 				stid = mon.cleanfn(status)
-				print(f"req_{pathid}_{methid}_{stid}.label {status} {method} {path}")
-				print(f"req_{pathid}_{methid}_{stid}.colour COLOUR{fid}")
-				print(f"req_{pathid}_{methid}_{stid}.draw AREASTACK")
-				print(f"req_{pathid}_{methid}_{stid}.min 0")
+				_print(f"req_{pathid}_{methid}_{stid}.label {status} {method} {path}")
+				_print(f"req_{pathid}_{methid}_{stid}.colour COLOUR{fid}")
+				_print(f"req_{pathid}_{methid}_{stid}.draw AREASTACK")
+				_print(f"req_{pathid}_{methid}_{stid}.min 0")
 				fid = mon.color(fid)
-	if mon.debug(): print()
+	if mon.debug(): _print()
 	# total
-	print(f"multigraph web_request_{hostid}.total")
-	print(f"graph_title {host} request")
-	print('graph_args --base 1000 -l 0')
-	print('graph_category web')
-	print('graph_vlabel total number')
-	print('graph_scale yes')
-	print('graph_total', cluster, 'total')
+	_print(f"multigraph web_request_{hostid}.total")
+	_print(f"graph_title {host} request")
+	_print('graph_args --base 1000 -l 0')
+	_print('graph_category web')
+	_print('graph_vlabel total number')
+	_print('graph_scale yes')
+	_print('graph_total', cluster, 'total')
 	fid = 0
 	for path in sorted(sts.keys()):
 		pathid = mon.cleanfn(path)
-		for method in sorted(sts[path].keys()):
+		for method in sorted(sts.get(path, {}).keys()):
 			methid = mon.cleanfn(method)
-			for status in sorted(sts[path][method].keys()):
+			for status in sorted(sts[path].get(method, {}).keys()):
 				stid = mon.cleanfn(status)
-				print(f"req_{pathid}_{methid}_{stid}.label {status} {method} {path}")
-				print(f"req_{pathid}_{methid}_{stid}.colour COLOUR{fid}")
-				print(f"req_{pathid}_{methid}_{stid}.draw AREASTACK")
-				print(f"req_{pathid}_{methid}_{stid}.min 0")
+				_print(f"req_{pathid}_{methid}_{stid}.label {status} {method} {path}")
+				_print(f"req_{pathid}_{methid}_{stid}.colour COLOUR{fid}")
+				_print(f"req_{pathid}_{methid}_{stid}.draw AREASTACK")
+				_print(f"req_{pathid}_{methid}_{stid}.min 0")
 				fid = mon.color(fid)
-	if mon.debug(): print()
+	if mon.debug(): _print()
 	# total count
-	print(f"multigraph web_request_{hostid}.count")
-	print(f"graph_title {host} request count")
-	print('graph_args --base 1000 -l 0')
-	print('graph_category web')
-	print('graph_vlabel number per second')
-	print('graph_scale no')
-	print('graph_total', cluster, 'total')
+	_print(f"multigraph web_request_{hostid}.count")
+	_print(f"graph_title {host} request count")
+	_print('graph_args --base 1000 -l 0')
+	_print('graph_category web')
+	_print('graph_vlabel number per second')
+	_print('graph_scale no')
+	_print('graph_total', cluster, 'total')
 	fid = 0
 	for path in sorted(sts.keys()):
 		pathid = mon.cleanfn(path)
-		for method in sorted(sts[path].keys()):
+		for method in sorted(sts.get(path, {}).keys()):
 			methid = mon.cleanfn(method)
-			for status in sorted(sts[path][method].keys()):
+			for status in sorted(sts[path].get(method, {}).keys()):
 				stid = mon.cleanfn(status)
-				print(f"req_{pathid}_{methid}_{stid}.label {status} {method} {path}")
-				print(f"req_{pathid}_{methid}_{stid}.colour COLOUR{fid}")
-				print(f"req_{pathid}_{methid}_{stid}.draw AREASTACK")
-				print(f"req_{pathid}_{methid}_{stid}.type DERIVE")
-				print(f"req_{pathid}_{methid}_{stid}.min 0")
-				print(f"req_{pathid}_{methid}_{stid}.cdef req_{pathid}_{methid}_{stid},1000,/")
+				_print(f"req_{pathid}_{methid}_{stid}.label {status} {method} {path}")
+				_print(f"req_{pathid}_{methid}_{stid}.colour COLOUR{fid}")
+				_print(f"req_{pathid}_{methid}_{stid}.draw AREASTACK")
+				_print(f"req_{pathid}_{methid}_{stid}.type DERIVE")
+				_print(f"req_{pathid}_{methid}_{stid}.min 0")
+				_print(f"req_{pathid}_{methid}_{stid}.cdef req_{pathid}_{methid}_{stid},1000,/")
 				fid = mon.color(fid)
-	if mon.debug(): print()
+	if mon.debug(): _print()
 
 def report(hostid, sts):
 	# total index
-	print(f"multigraph web_request_{hostid}")
+	_print(f"multigraph web_request_{hostid}")
 	for path in sorted(sts.keys()):
 		pathid = mon.cleanfn(path)
-		for method in sorted(sts[path].keys()):
+		for method in sorted(sts.get(path, {}).keys()):
 			methid = mon.cleanfn(method)
-			for status in sorted(sts[path][method].keys()):
+			for status in sorted(sts[path].get(method, {}).keys()):
 				stid = mon.cleanfn(status)
-				value = sts[path][method][status]['count']
-				print(f"req_{pathid}_{methid}_{stid}.value {value}")
-	if mon.debug(): print()
+				value = sts[path][method][status].get('count', 'U')
+				_print(f"req_{pathid}_{methid}_{stid}.value {value}")
+	if mon.debug(): _print()
 	# total
-	print(f"multigraph web_request_{hostid}.total")
+	_print(f"multigraph web_request_{hostid}.total")
 	for path in sorted(sts.keys()):
 		pathid = mon.cleanfn(path)
-		for method in sorted(sts[path].keys()):
+		for method in sorted(sts.get(path, {}).keys()):
 			methid = mon.cleanfn(method)
-			for status in sorted(sts[path][method].keys()):
+			for status in sorted(sts[path].get(method, {}).keys()):
 				stid = mon.cleanfn(status)
-				value = sts[path][method][status]['count']
-				print(f"req_{pathid}_{methid}_{stid}.value {value}")
-	if mon.debug(): print()
+				value = sts[path][method][status].get('count', 'U')
+				_print(f"req_{pathid}_{methid}_{stid}.value {value}")
+	if mon.debug(): _print()
 	# total count
-	print(f"multigraph web_request_{hostid}.count")
+	_print(f"multigraph web_request_{hostid}.count")
 	for path in sorted(sts.keys()):
 		pathid = mon.cleanfn(path)
-		for method in sorted(sts[path].keys()):
+		for method in sorted(sts.get(path, {}).keys()):
 			methid = mon.cleanfn(method)
-			for status in sorted(sts[path][method].keys()):
+			for status in sorted(sts[path].get(method, {}).keys()):
 				stid = mon.cleanfn(status)
-				value = mon.derive(sts[path][method][status]['count'])
-				print(f"req_{pathid}_{methid}_{stid}.value {value}")
-	if mon.debug(): print()
+				value = mon.derive(sts[path][method][status].get('count', 'U'))
+				_print(f"req_{pathid}_{methid}_{stid}.value {value}")
+	if mon.debug(): _print()
---
+++ k8s/test/mon/munin-node/plugins/req_total_test.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+import unittest
+from unittest.mock import MagicMock, call
+
+import mon_t
+import mon
+
+import req_total
+
+_bup_print = req_total._print
+
+class Test(unittest.TestCase):
+
+	def setUp(t):
+		mon_t.setUp()
+		req_total._print = MagicMock()
+
+	def tearDown(t):
+		mon_t.tearDown()
+		req_total._print = _bup_print
+
+	def test_print(t):
+		_bup_print('testing')
+
+	def test_config(t):
+		sts = {
+			'/testing': {
+				'GET': {
+					'200': {}
+				},
+			},
+		}
+		req_total.config(mon.cluster(), 'thost.uws', 'thost_uws', sts)
+		config = [
+			# total index
+			call('multigraph web_request_thost_uws'),
+			call('graph_title thost.uws request'),
+			call('graph_args --base 1000 -l 0'),
+			call('graph_category web'),
+			call('graph_vlabel total number'),
+			call('graph_scale yes'),
+			call('graph_total', 'k8stest', 'total'),
+			call('req__testing_GET_200.label 200 GET /testing'),
+			call('req__testing_GET_200.colour COLOUR0'),
+			call('req__testing_GET_200.draw AREASTACK'),
+			call('req__testing_GET_200.min 0'),
+			# total
+			call('multigraph web_request_thost_uws.total'),
+			call('graph_title thost.uws request'),
+			call('graph_args --base 1000 -l 0'),
+			call('graph_category web'),
+			call('graph_vlabel total number'),
+			call('graph_scale yes'),
+			call('graph_total', 'k8stest', 'total'),
+			call('req__testing_GET_200.label 200 GET /testing'),
+			call('req__testing_GET_200.colour COLOUR0'),
+			call('req__testing_GET_200.draw AREASTACK'),
+			call('req__testing_GET_200.min 0'),
+			# total count
+			call('multigraph web_request_thost_uws.count'),
+			call('graph_title thost.uws request count'),
+			call('graph_args --base 1000 -l 0'),
+			call('graph_category web'),
+			call('graph_vlabel number per second'),
+			call('graph_scale no'),
+			call('graph_total', 'k8stest', 'total'),
+			call('req__testing_GET_200.label 200 GET /testing'),
+			call('req__testing_GET_200.colour COLOUR0'),
+			call('req__testing_GET_200.draw AREASTACK'),
+			call('req__testing_GET_200.type DERIVE'),
+			call('req__testing_GET_200.min 0'),
+			call('req__testing_GET_200.cdef req__testing_GET_200,1000,/'),
+		]
+		req_total._print.assert_has_calls(config)
+		t.assertEqual(req_total._print.call_count, len(config))
+
+	def test_report(t):
+		sts = {
+			'/testing': {
+				'GET': {
+					'200': {'count': 99.0},
+				},
+			},
+		}
+		req_total.report('thost_uws', sts)
+		report = [
+			# total index
+			call('multigraph web_request_thost_uws'),
+			call('req__testing_GET_200.value 99.0'),
+			# total
+			call('multigraph web_request_thost_uws.total'),
+			call('req__testing_GET_200.value 99.0'),
+			# total count
+			call('multigraph web_request_thost_uws.count'),
+			call('req__testing_GET_200.value 99000'),
+		]
+		req_total._print.assert_has_calls(report)
+		t.assertEqual(req_total._print.call_count, len(report))
+
+if __name__ == '__main__':
+	unittest.main()
