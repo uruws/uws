3f7cf50fd710bef0d96b8cc7ae15be7930fe432c
Jeremías <jeremias@talkingpts.org>
2021-12-23 19:07:24 -0300

    go test mon lib

--- go/k8s/mon/mon_test.go
+++ go/k8s/mon/mon_test.go
@@ -2,3 +2,18 @@
 // See LICENSE file.
 
 package mon
+
+import (
+	"testing"
+
+	. "uws/testing/check"
+)
+
+func TestGlobals(t *testing.T) {
+	IsEqual(t, kubecmd, "/usr/local/bin/uwskube", "kubecmd")
+	IsEqual(t, cluster, "", "cluster")
+}
+
+func TestCluster(t *testing.T) {
+	IsEqual(t, Cluster(), "", "Cluster()")
+}
