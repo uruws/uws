5cd5cb55ef6de72fc624f99991022207754329aa
Jeremías <jeremias@talkingpts.org>
2021-12-27 13:10:32 -0300

    go mock http request and response

--- go/testing/mock/mock_http.go
+++ go/testing/mock/mock_http.go
@@ -5,8 +5,17 @@ package mock
 
 import (
 	"net/http"
+	"net/http/httptest"
 )
 
 func HTTPListenAndServe(addr string, h http.Handler) error {
 	return nil
+}
+
+func HTTPRequest(method, target string) *http.Request {
+	return httptest.NewRequest(method, target, nil)
+}
+
+func HTTPResponse() *httptest.ResponseRecorder {
+	return httptest.NewRecorder()
 }
