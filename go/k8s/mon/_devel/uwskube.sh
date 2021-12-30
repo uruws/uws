8a623b1ba8896eea2d02713cded3d5a63f50c2b8
Jeremías <jeremias@talkingpts.org>
2021-12-30 16:57:02 -0300

    remove old devel code

--- go/k8s/mon/_devel/uwskube.sh
+++ go/k8s/mon/_devel/uwskube.sh
@@ -34,15 +34,4 @@ elif test "X${1}" = 'Xtest_top_nodes'; then
 	exit 0
 fi
 
-datadir=/go/tmp/k8smon/${UWS_CLUSTER}
-fn=${datadir}/NOTSET_IN_DEVEL_UWSKUBE_SH
-if test 'Xget nodes -o json' = "X$*"; then
-	fn=nodes.json
-elif test 'Xget deployments,statefulset,daemonset -A -o json' = "X$*"; then
-	fn=deployments.json
-elif test 'Xget pods -A -o json' = "X$*"; then
-	fn=pods.json
-fi
-datfn=${datadir}/${fn}
-
-exec cat ${datfn}
+exit 128
