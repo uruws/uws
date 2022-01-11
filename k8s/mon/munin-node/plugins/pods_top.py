3211121b169ae98aecbdbe9b14797e339d85798e
Jeremías <jeremias@talkingpts.org>
2022-01-11 13:35:11 -0300

    pods top namespace totals

--- k8s/mon/munin-node/plugins/pods_top.py
+++ k8s/mon/munin-node/plugins/pods_top.py
@@ -31,6 +31,43 @@ def _print(*args):
 
 def config(sts):
 	mon.dbg('pods_top config')
+	cluster = mon.cluster()
+	# cpu total
+	_print('multigraph pods_top_cpu')
+	_print(f"graph_title {cluster} pods CPU")
+	_print('graph_args --base 1000 -l 0')
+	_print('graph_category top')
+	_print('graph_vlabel millicores')
+	_print('graph_printf %3.0lf')
+	_print('graph_scale yes')
+	_print('graph_total total')
+	total = sts.get('total', {})
+	color = 0
+	for ns in sorted(total.keys()):
+		nsid = mon.cleanfn(ns)
+		_print(f"{nsid}.label {ns}")
+		_print(f"{nsid}.colour COLOUR{color}")
+		_print(f"{nsid}.draw AREASTACK")
+		_print(f"{nsid}.min 0")
+		color = mon.color(color)
+	# mem total
+	_print('multigraph pods_top_mem')
+	_print(f"graph_title {cluster} pods memory")
+	_print('graph_args --base 1000 -l 0')
+	_print('graph_category top')
+	_print('graph_vlabel MiB')
+	_print('graph_printf %3.0lf')
+	_print('graph_scale yes')
+	_print('graph_total total')
+	total = sts.get('total', {})
+	color = 0
+	for ns in sorted(total.keys()):
+		nsid = mon.cleanfn(ns)
+		_print(f"{nsid}.label {ns}")
+		_print(f"{nsid}.colour COLOUR{color}")
+		_print(f"{nsid}.draw AREASTACK")
+		_print(f"{nsid}.min 0")
+		color = mon.color(color)
 
 def report(sts):
 	mon.dbg('pods_top report')
--- k8s/test/mon/munin-node/plugins/pods_top_test.py
+++ k8s/test/mon/munin-node/plugins/pods_top_test.py
@@ -83,6 +83,36 @@ _pods = {
 	]
 }
 
+_sts = {
+	'info': {
+		'cert-manager': {
+			'cert-manager-66b6d6bf59-xsx76': {'cpu': 1, 'mem': 24},
+			'cert-manager-cainjector-856d4df858-csjg7': {'cpu': 3, 'mem': 52},
+		},
+		'ingress-nginx': {
+			'ingress-nginx-controller-59c8576d75-qbms9': {'cpu': 3, 'mem': 76},
+		},
+		'kube-system': {
+			'aws-node-5lmld': {'cpu': 4, 'mem': 41},
+			'cluster-autoscaler-848d4b88dc-rlvx6': {'cpu': 2, 'mem': 41},
+			'coredns-7d74b564bd-9mc2d': {'cpu': 5, 'mem': 8},
+			'kube-proxy-6zggk': {'cpu': 1, 'mem': 14},
+			'metrics-server-588cd8ddb5-r6hrq': {'cpu': 4, 'mem': 20},
+		},
+		'mon': {
+			'k8s-6894c9b96c-kg7fq': {'cpu': 1, 'mem': 7},
+			'munin-0': {'cpu': 4, 'mem': 66},
+			'munin-node-7cdd78b4d-qtpgc': {'cpu': 1, 'mem': 12},
+		},
+	},
+	'total': {
+		'cert-manager': {'cpu': 4, 'mem': 76},
+		'ingress-nginx': {'cpu': 3, 'mem': 76},
+		'kube-system': {'cpu': 16, 'mem': 124},
+		'mon': {'cpu': 6, 'mem': 85},
+	},
+}
+
 class Test(unittest.TestCase):
 
 	def setUp(t):
@@ -101,35 +131,64 @@ class Test(unittest.TestCase):
 
 	def test_parse_data(t):
 		t.maxDiff = None
-		t.assertDictEqual(pods_top.parse(_pods), {
-			'info': {
-				'cert-manager': {
-					'cert-manager-66b6d6bf59-xsx76': {'cpu': 1, 'mem': 24},
-					'cert-manager-cainjector-856d4df858-csjg7': {'cpu': 3, 'mem': 52},
-				},
-				'ingress-nginx': {
-					'ingress-nginx-controller-59c8576d75-qbms9': {'cpu': 3, 'mem': 76},
-				},
-				'kube-system': {
-					'aws-node-5lmld': {'cpu': 4, 'mem': 41},
-					'cluster-autoscaler-848d4b88dc-rlvx6': {'cpu': 2, 'mem': 41},
-					'coredns-7d74b564bd-9mc2d': {'cpu': 5, 'mem': 8},
-					'kube-proxy-6zggk': {'cpu': 1, 'mem': 14},
-					'metrics-server-588cd8ddb5-r6hrq': {'cpu': 4, 'mem': 20},
-				},
-				'mon': {
-					'k8s-6894c9b96c-kg7fq': {'cpu': 1, 'mem': 7},
-					'munin-0': {'cpu': 4, 'mem': 66},
-					'munin-node-7cdd78b4d-qtpgc': {'cpu': 1, 'mem': 12},
-				},
-			},
-			'total': {
-				'cert-manager': {'cpu': 4, 'mem': 76},
-				'ingress-nginx': {'cpu': 3, 'mem': 76},
-				'kube-system': {'cpu': 16, 'mem': 124},
-				'mon': {'cpu': 6, 'mem': 85},
-			},
-		})
+		t.assertDictEqual(pods_top.parse(_pods), _sts)
+
+	def test_config(t):
+		pods_top.config(_sts)
+		config = [
+			# cpu total
+			call('multigraph pods_top_cpu'),
+			call('graph_title k8stest pods CPU'),
+			call('graph_args --base 1000 -l 0'),
+			call('graph_category top'),
+			call('graph_vlabel millicores'),
+			call('graph_printf %3.0lf'),
+			call('graph_scale yes'),
+			call('graph_total total'),
+			call('cert_manager.label cert-manager'),
+			call('cert_manager.colour COLOUR0'),
+			call('cert_manager.draw AREASTACK'),
+			call('cert_manager.min 0'),
+			call('ingress_nginx.label ingress-nginx'),
+			call('ingress_nginx.colour COLOUR1'),
+			call('ingress_nginx.draw AREASTACK'),
+			call('ingress_nginx.min 0'),
+			call('kube_system.label kube-system'),
+			call('kube_system.colour COLOUR2'),
+			call('kube_system.draw AREASTACK'),
+			call('kube_system.min 0'),
+			call('mon.label mon'),
+			call('mon.colour COLOUR3'),
+			call('mon.draw AREASTACK'),
+			call('mon.min 0'),
+			# mem total
+			call('multigraph pods_top_mem'),
+			call('graph_title k8stest pods memory'),
+			call('graph_args --base 1000 -l 0'),
+			call('graph_category top'),
+			call('graph_vlabel MiB'),
+			call('graph_printf %3.0lf'),
+			call('graph_scale yes'),
+			call('graph_total total'),
+			call('cert_manager.label cert-manager'),
+			call('cert_manager.colour COLOUR0'),
+			call('cert_manager.draw AREASTACK'),
+			call('cert_manager.min 0'),
+			call('ingress_nginx.label ingress-nginx'),
+			call('ingress_nginx.colour COLOUR1'),
+			call('ingress_nginx.draw AREASTACK'),
+			call('ingress_nginx.min 0'),
+			call('kube_system.label kube-system'),
+			call('kube_system.colour COLOUR2'),
+			call('kube_system.draw AREASTACK'),
+			call('kube_system.min 0'),
+			call('mon.label mon'),
+			call('mon.colour COLOUR3'),
+			call('mon.draw AREASTACK'),
+			call('mon.min 0'),
+		]
+		pods_top._print.assert_has_calls(config)
+		t.assertEqual(pods_top._print.call_count, len(config))
 
 if __name__ == '__main__':
 	unittest.main()
