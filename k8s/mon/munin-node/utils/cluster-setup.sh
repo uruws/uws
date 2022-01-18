a4bf3cee426e8a9968d49cd67d5760270052d614
Jeremías <jeremias@talkingpts.org>
2022-01-18 15:28:48 -0300

    k8smon: cluster setup k8s plugin

--- k8s/mon/munin-node/utils/cluster-setup.sh
+++ k8s/mon/munin-node/utils/cluster-setup.sh
@@ -7,6 +7,7 @@ install -v -m 0755 /srv/munin/plugins/nodes.py /usr/local/bin/nodes
 install -v -m 0755 /srv/munin/plugins/nginx.py /usr/local/bin/nginx
 install -v -m 0755 /srv/munin/plugins/deployments.py /usr/local/bin/deployments
 install -v -m 0755 /srv/munin/plugins/pods.py /usr/local/bin/pods
+install -v -m 0755 /srv/munin/plugins/k8s.py /usr/local/bin/k8s
 
 enpl=/root/bin/plugin-enable.sh
 
@@ -14,5 +15,6 @@ ${enpl} local nodes
 ${enpl} local nginx
 ${enpl} local deployments
 ${enpl} local pods
+${enpl} local k8s
 
 exit 0
