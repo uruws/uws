769da1b4c7d7182b69a60b627d28dccbad047f14
Jeremías <jeremias@talkingpts.org>
2022-01-19 11:54:14 -0300

    k8s devel mount go sources (read-only)

--- docker/k8s/devel.sh
+++ docker/k8s/devel.sh
@@ -3,12 +3,20 @@ set -eu
 
 k8s=${PWD}/k8s
 pod=${PWD}/pod
+go=${PWD}/go
 
 install -v -d -m 0750 ${PWD}/tmp/k8s
 
-exec docker run --rm --name k8s-devel \
+devel_login=${K8S_DEVEL_LOGIN:-'false'}
+docker_args='--rm --name k8s-devel'
+if test "X${devel_login}" = 'Xtrue'; then
+	docker_args='-it --rm --name k8s-devel-login'
+fi
+
+exec docker run ${docker_args} \
 	--hostname k8s-devel.uws.local -u uws \
 	-e PYTHONPATH=/home/uws/pod/lib:/home/uws/k8s/mon/munin-node/plugins \
+	-v ${go}:/go/src:ro \
 	-v ${k8s}:/home/uws/k8s:ro \
 	-v ${pod}:/home/uws/pod:ro \
 	-v ${PWD}/tmp/k8s:/home/uws/tmp \
