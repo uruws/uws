481088f55a93f037200272e2b28ab12d65ff2fe4
Jeremías <jeremias@talkingpts.org>
2022-01-20 15:33:59 -0300

    app-autobuild: deploy helper script

---
+++ cli/app-autobuild-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+logfn=$(mktemp app-autobuild-deploy.XXXXXXXXXX)
+
+exec >${logfn}
+exec 2>&1
+
+app=${1:?'app?'}
+version=${2:?'version?'}
+
+bindir=${UWSCLI_BINDIR:-'/srv/home/uwscli/bin'}
+
+set +e
+
+${bindir}/app-autobuild "${app}" --deploy "${version}"
+rc=$?
+
+set -e
+
+subject="app-autobuild ${app} ${version}"
+
+if test "X${rc}" = '0'; then
+	cat ${logfn} | mailx -s "[OK] ${subject}" root
+else
+	cat ${logfn} | mailx -s "[ERROR] ${subject}" munin-alert
+fi
+
+exit ${rc}
