197ad46df80a68bb821cd9a75978b897b59026fa
Jeremías <jeremias@talkingpts.org>
2022-01-24 15:26:42 -0300

    mnpl: setup plugins

--- srv/munin-node/plugins/bin/mnpl.sh
+++ srv/munin-node/plugins/bin/mnpl.sh
@@ -7,6 +7,25 @@ export PYTHONPATH=${pldir}
 mod=$(basename ${0})
 #~ echo "MOD: ${mod}"
 
+action=${1:-'report'}
+
+pl_setup() {
+	for fn in ${pldir}/*.py; do
+		p=$(basename ${fn} .py)
+		if test "X${p}" != 'Xmnpl'; then
+			ln -sv ${fn} /etc/munin/plugins/${p}
+		fi
+	done
+}
+
+if test "X${mod}" = 'Xmnpl.sh'; then
+	if test "X${action}" = 'Xsetup'; then
+		pl_setup
+		exit 0
+	fi
+	exit 9
+fi
+
 if ! test -f ${pldir}/${mod}.py; then
 	echo "${mod}: invalid mnpl command" >&2
 	exit 1
