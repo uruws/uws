45dcb2d031b3bea9c611320b5328950ce28d1899
Jeremías <jeremias@talkingpts.org>
2022-01-24 16:39:58 -0300

    munin-node: fix check script

--- srv/munin-node/check.sh
+++ srv/munin-node/check.sh
@@ -8,8 +8,11 @@ docker run --rm --name uws-munin-node-check \
 	--hostname munin-node.uws.local \
 	--read-only \
 	--workdir /home/uws \
+	-e PYTHONPATH=/uws/lib/plugins \
 	-v ${PWD}/srv/munin-node/utils:/home/uws/utils:ro \
 	-v ${PWD}/srv/munin-node/test:/home/uws/test:ro \
+	-v ${PWD}/srv/munin-node/plugins/bin:/uws/bin:ro \
+	-v ${PWD}/srv/munin-node/plugins/lib:/uws/lib/plugins \
 	-v ${TMPDIR}:/home/uws/tmp \
 	uws/python-2109 $@
 
--- srv/munin-node/utils-devel.sh
+++ srv/munin-node/utils-devel.sh
@@ -11,7 +11,7 @@ docker run -it --rm --name uws-munin-node-utils-devel \
 	-e PYTHONPATH=/uws/lib/plugins \
 	-v ${PWD}/srv/munin-node/utils:/home/uws/utils:ro \
 	-v ${PWD}/srv/munin-node/test:/home/uws/test:ro \
-	-v ${PWD}/srv/munin-node/plugins/bin:/uws/bin \
+	-v ${PWD}/srv/munin-node/plugins/bin:/uws/bin:ro \
 	-v ${PWD}/srv/munin-node/plugins/lib:/uws/lib/plugins \
 	-v ${TMPDIR}:/home/uws/tmp \
 	uws/python-2109
