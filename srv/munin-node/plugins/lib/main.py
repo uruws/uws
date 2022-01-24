d8a87bc00c530199f7b862a392ff579a3ad09048
Jeremías <jeremias@talkingpts.org>
2022-01-24 11:54:38 -0300

    munin-node: check valid cmd

--- srv/munin-node/plugins/lib/main.py
+++ srv/munin-node/plugins/lib/main.py
@@ -5,10 +5,19 @@ from pathlib import Path
 
 import mnpl
 
+ENOCMD    = 1
+EWRONGCMD = 2
+
+_cmd = {}
+
 def run(argv: list[str]):
 	cmd = Path(argv[0]).stem.strip()
 	if cmd == '':
 		mnpl.error('empty command name')
-		return 1
+		return ENOCMD
 	mnpl.log('run', cmd)
+	x = _cmd.get(cmd, None)
+	if x is None:
+		mnpl.error(f"{cmd}: invalid command")
+		return EWRONGCMD
 	return 0
--- srv/munin-node/test/plugins/main_test.py
+++ srv/munin-node/test/plugins/main_test.py
@@ -3,10 +3,25 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
+from contextlib    import contextmanager
+from unittest.mock import MagicMock
+from unittest.mock import call
+
 import unittest
 
 import mnpl_t
 import main
+
+@contextmanager
+def mock_cmd(cmd = {}):
+	_bup_cmd = main._cmd.copy()
+	try:
+		main._cmd = {'testing': True}
+		main._cmd.update(cmd)
+		yield
+	finally:
+		main._cmd = None
+		main._cmd = _bup_cmd.copy()
 
 class Test(unittest.TestCase):
 
@@ -16,11 +31,13 @@ class Test(unittest.TestCase):
 	def tearDown(t):
 		mnpl_t.teardown()
 
-	def test_main_error(t):
-		t.assertEqual(main.run(['']), 1)
+	def test_main_errors(t):
+		t.assertEqual(main.run(['']), main.ENOCMD)
+		t.assertEqual(main.run(['invalid']), main.EWRONGCMD)
 
 	def test_main(t):
-		t.assertEqual(main.run(['testing']), 0)
+		with mock_cmd():
+			t.assertEqual(main.run(['testing']), 0)
 
 if __name__ == '__main__':
 	unittest.main()
