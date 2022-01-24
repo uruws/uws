c6409fb0ac991238259544066b491e0f4ddeda9d
Jeremías <jeremias@talkingpts.org>
2022-01-24 17:19:29 -0300

    ca-revoke: check revoke/new dir

--- docker/mkcert/utils/ca-revoke.sh
+++ docker/mkcert/utils/ca-revoke.sh
@@ -2,9 +2,11 @@
 set -eu
 umask 0027
 export OPENSSL_CONF=/usr/local/etc/ssl/openssl.cnf
-for fn in ${HOME}/ca/revoke/new/*.pem; do
-	echo "ca revoke: ${fn}"
-	openssl ca -revoke "${fn}"
-	mv -f "${fn}" "${HOME}/ca/revoke/$(basename "${fn}")"
-done
+if test -d ${HOME}/ca/revoke/new; then
+	for fn in ${HOME}/ca/revoke/new/*.pem; do
+		echo "ca revoke: ${fn}"
+		openssl ca -revoke "${fn}"
+		mv -f "${fn}" "${HOME}/ca/revoke/$(basename "${fn}")"
+	done
+fi
 exec ca-crl.sh
