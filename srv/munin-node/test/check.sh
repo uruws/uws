983c74584fd91012d5d4ea3e641f012859407861
Jeremías <jeremias@talkingpts.org>
2022-01-25 12:10:23 -0300

    munin-node: run typecheck

--- srv/munin-node/test/check.sh
+++ srv/munin-node/test/check.sh
@@ -3,6 +3,9 @@ set -eu
 echo '***** munin-node/test/run/shellcheck.sh'
 ./test/run/shellcheck.sh
 
+echo '***** munin-node/test/run/typecheck.sh'
+./test/run/typecheck.sh
+
 echo '***** munin-node/test/run/coverage.sh'
 ./test/run/coverage.sh
 
