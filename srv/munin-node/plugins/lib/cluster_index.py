dbf1eae2f17c60b9d4a4d44c6a33bcdc692d45f2
Jeremías <jeremias@talkingpts.org>
2022-01-26 17:33:01 -0300

    mnpl: index auth 404 check

---
+++ srv/munin-node/plugins/lib/cluster_index.py
@@ -0,0 +1,17 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+import sys
+
+import mnpl
+
+def main(argv: list[str]):
+	cfg = mnpl.Config(
+		path     = '/',
+		status   = 404,
+		category = 'index',
+	)
+	return mnpl.main(argv, cfg)
+
+if __name__ == '__main__': # pragma no cover
+	sys.exit(main(sys.argv[1:]))
