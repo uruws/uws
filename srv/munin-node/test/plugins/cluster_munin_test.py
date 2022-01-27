298024b7a7acbdd6f673d022370962e8a8a7ebe9
Jeremías <jeremias@talkingpts.org>
2022-01-27 16:04:59 -0300

    mnpl: fix tests

--- srv/munin-node/test/plugins/cluster_munin_test.py
+++ srv/munin-node/test/plugins/cluster_munin_test.py
@@ -18,7 +18,7 @@ class Test(unittest.TestCase):
 		mnpl_t.teardown()
 
 	def test_main(t):
-		t.assertEqual(cluster_munin.main([]), 0)
+		t.assertEqual(cluster_munin.main(['config']), 0)
 
 if __name__ == '__main__':
 	unittest.main()
