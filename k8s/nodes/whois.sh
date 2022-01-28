acf99a1c1f52c481092aebeb28a19add89cc0b4d
Jeremías <jeremias@talkingpts.org>
2022-01-28 15:46:21 -0300

    k8s: fix shellcheck

--- k8s/nodes/whois.sh
+++ k8s/nodes/whois.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 miss_address=${1:?'miss_address.txt?'}
 cat ${miss_address} | while read addr; do
 	echo -n "${addr}: "
-	echo $(whois ${addr} | grep -iE 'CDIR:|NetName:|Organization:|City:|Country:')
+	echo "$(whois ${addr} | grep -iE 'CDIR:|NetName:|Organization:|City:|Country:')"
 done
 exit 0
