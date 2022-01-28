bb983e07f750eabf5c1afa2f87479e4e4283b41d
Jeremías <jeremias@talkingpts.org>
2022-01-28 14:27:20 -0300

    k8s: case insensitive nodes whois info

--- k8s/nodes/whois.sh
+++ k8s/nodes/whois.sh
@@ -3,6 +3,6 @@ set -eu
 miss_address=${1:?'miss_address.txt?'}
 cat ${miss_address} | while read addr; do
 	echo -n "${addr}: "
-	echo $(whois ${addr} | grep -E 'CDIR:|NetName:|Organization:|City:|Country:')
+	echo $(whois ${addr} | grep -iE 'CDIR:|NetName:|Organization:|City:|Country:')
 done
 exit 0
