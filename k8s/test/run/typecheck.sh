d192bead3b6db209c7962c14d71a66920fce6710
Jeremías <jeremias@talkingpts.org>
2022-02-03 15:43:51 -0300

    k8s: typecheck pod/lib

--- k8s/test/run/typecheck.sh
+++ k8s/test/run/typecheck.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -eu
 exec python3 -m mypy ${HOME}/k8s/mon/munin-node/plugins \
-	${HOME}/k8s/test/mon/munin-node/plugins
+	${HOME}/k8s/test/mon/munin-node/plugins \
+	${HOME}/pod/lib \
+	${HOME}/k8s/test/pod/lib
