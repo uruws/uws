c430428975b4046bc65dc19738b3d6ac6a4a314e
Jeremías <jeremias@talkingpts.org>
2022-02-03 16:09:24 -0300

    k8s: typecheck MYPYPATH

--- k8s/test/run/typecheck.sh
+++ k8s/test/run/typecheck.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -eu
+MYPYPATH=/uws/lib/plugins
+export MYPYPATH
 exec python3 -m mypy ${HOME}/k8s/mon/munin-node/plugins \
 	${HOME}/k8s/test/mon/munin-node/plugins \
 	${HOME}/pod/lib \
