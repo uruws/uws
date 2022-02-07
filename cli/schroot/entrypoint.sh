02f15016c61ae0b7bc8476b3f279ff017a8bdb49
Jeremías <jeremias@talkingpts.org>
2022-02-07 17:59:29 -0300

    cli: schroot entrypoint sudoers setup

--- cli/schroot/entrypoint.sh
+++ cli/schroot/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -eu
+
+doas install -v -m 0440 -o root -g root \
+	./host/assets/jsbatch/etc/sudoers.d/99-uwscli \
+	/etc/sudoers.d/99-uwscli
+
 PATH=/srv/home/uwscli/bin:${PATH}
 exec /bin/bash -i
