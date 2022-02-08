67d1af8f161f3b2b16fc2399f9db6e890ec073e9
Jeremías <jeremias@talkingpts.org>
2022-02-08 00:57:41 -0300

    cli: schroot entrypoint fix uws home perms

--- cli/schroot/entrypoint.sh
+++ cli/schroot/entrypoint.sh
@@ -5,7 +5,7 @@ doas install -v -m 0440 -o root -g root \
 	./host/assets/jsbatch/etc/sudoers.d/99-uwscli \
 	/etc/sudoers.d/99-uwscli
 
-doas install -v -d -m 0770 -o root -g uws /home/uws
+doas install -v -d -m 0750 -o uws -g uws /home/uws
 
 PATH=/srv/home/uwscli/bin:${PATH}
 exec /bin/bash -i
