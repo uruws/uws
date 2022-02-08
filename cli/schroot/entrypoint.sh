410cb03266eb9541a847a88357870f734facb5a7
Jeremías <jeremias@talkingpts.org>
2022-02-08 12:06:20 -0300

    cli: schroot entrypoint use SCHROOT_GROUP env var

--- cli/schroot/entrypoint.sh
+++ cli/schroot/entrypoint.sh
@@ -6,7 +6,8 @@ doas install -v -m 0440 -o root -g root \
 	/etc/sudoers.d/99-uwscli
 
 doas install -v -d -m 0750 -o uws -g uws /home/uws
-doas /usr/sbin/adduser uws $(id -gn)
+
+doas /usr/sbin/adduser uws ${SCHROOT_GROUP}
 doas /usr/sbin/adduser uws docker
 
 PATH=/srv/home/uwscli/bin:${PATH}
