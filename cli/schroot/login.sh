ebf3c9b6a2bd7f7863f9d6e0d801709bb822a590
Jeremías <jeremias@talkingpts.org>
2022-02-08 00:53:15 -0300

    cli: schroot fix paths

--- cli/schroot/login.sh
+++ cli/schroot/login.sh
@@ -13,6 +13,6 @@ trap cleanup INT EXIT
 
 schroot_sess="schroot -c ${sess} -r"
 
-${schroot_sess} -d /srv/uws/deploy -- /srv/uws/deploy/cli/schroot/entrypoint.sh
+${schroot_sess} -d /srv/uws/deploy -- ./cli/schroot/entrypoint.sh
 
 exit 0
