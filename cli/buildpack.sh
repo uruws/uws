c3d07b904f1d6c7cc8dc680578b943ccab450073
Jeremías <jeremias@talkingpts.org>
2022-02-11 14:47:15 -0300

    cli: fix buildpack.sh

--- cli/buildpack.sh
+++ cli/buildpack.sh
@@ -1,27 +1,3 @@
 #!/bin/sh
 set -eu
-
-user=${1:?'user?'}
-cluster=${2:?'cluster?'}
-kind=${3:?'kind?'}
-action=${4:?'action?'}
-
-shift 4
-
-logs_dir=${HOME}/logs
-logf=${logs_dir}/app-ctl.log
-
-mkdir -vp "${logs_dir}"
-echo "$(date -R) [${user}] ${cluster} ${kind} ${action} ${*}" >>"${logf}"
-
-cmd=./pod/${kind}/${action}.sh
-
-cd /srv/uws/deploy
-if ! test -x "${cmd}"; then
-	echo "invalid action: ${action}" >&2
-	exit 1
-fi
-
-
-
 exec /srv/deploy/Buildpack/build.py "$@"
