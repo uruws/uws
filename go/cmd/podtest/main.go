5f99c174f205a9a65a795635a25b8b3188897877
Jeremías <jeremias@talkingpts.org>
2022-02-16 15:06:28 -0300

    podtest: generate some output for app-logs and such

--- go/cmd/podtest/main.go
+++ go/cmd/podtest/main.go
@@ -11,14 +11,18 @@ import (
 )
 
 func main() {
+	fmt.Println("start...")
 	http.HandleFunc("/", mainHandler)
-	http.ListenAndServe(":8699", nil)
+	http.ListenAndServe(":2800", nil)
+	fmt.Println("end!")
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	if hostname, err := os.Hostname(); err != nil {
+		fmt.Println("[ERROR]", err)
 		fmt.Fprintf(w, "ERROR: %s\n", err)
 	} else {
+		fmt.Println(r.RemoteAddr, r.RequestURI, hostname)
 		fmt.Fprintf(w, "%s\n", hostname)
 	}
 }
