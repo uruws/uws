f950f13b0d7ff62525bc19ab91ad2985dbe44d41
Jeremías <jeremias@talkingpts.org>
2022-02-16 13:46:48 -0300

    cli: do not check vendor every time

--- cli/test/check.sh
+++ cli/test/check.sh
@@ -4,8 +4,8 @@ set -eu
 echo '***** cli/test/run/shellcheck.sh'
 ./test/run/shellcheck.sh
 
-echo '***** cli/test/run/vendor.sh'
-./test/run/vendor.sh
+#~ echo '***** cli/test/run/vendor.sh'
+#~ ./test/run/vendor.sh
 
 echo '***** cli/test/run/typecheck.sh'
 ./test/run/typecheck.sh
