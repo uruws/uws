397f850818a301ffa0fc63dbd6e649c7ca0c5611
Jeremías <jeremias@talkingpts.org>
2022-02-23 17:50:41 -0300

    k8sctl: command ttl

--- go/k8s/ctl/cmd.go
+++ go/k8s/ctl/cmd.go
@@ -3,10 +3,22 @@
 
 package ctl
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os/exec"
+	"time"
+
+	"uws/log"
+	//~ "uws/wapp"
+)
+
 type ctlCmd struct {
 	name string
 	args []string
 	bindir string
+	ttl int
 }
 
 func newCmd(name string, args ...string) *ctlCmd {
@@ -14,5 +26,20 @@ func newCmd(name string, args ...string) *ctlCmd {
 		name: name,
 		args: args,
 		bindir: bindir,
+		ttl: 300, // 5min
 	}
+}
+
+func (c *ctlCmd) Run(w http.ResponseWriter, r *http.Request) {
+	//~ start := wapp.Start()
+	ttl, _ := time.ParseDuration(fmt.Sprintf("%ds", c.ttl))
+	ctx, cancel := context.WithTimeout(context.Background(), ttl)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, c.name, c.args...)
+	log.Debug("run: %s", cmd)
+	//~ if r.URL.Path != "/" {
+		//~ wapp.NotFound(w, r, start)
+	//~ } else {
+		//~ wapp.Write(w, r, start, "index\n")
+	//~ }
 }
