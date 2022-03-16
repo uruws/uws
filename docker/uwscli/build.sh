655c9a647aa86b23afc04a1a0823165a05049f32
Jeremías <jeremias@talkingpts.org>
2022-03-16 15:00:58 -0300

    uwscli: 2203 upgrade

--- docker/uwscli/build.sh
+++ docker/uwscli/build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 set -eu
-exec docker build --rm -t uws/cli ./docker/uwscli
+# uwscli
+docker build --rm -t uws/cli \
+	-f docker/uwscli/Dockerfile \
+	./docker/uwscli
+# uwscli-2203
+docker build --rm -t uws/cli-2203 \
+	-f docker/uwscli/Dockerfile.2203 \
+	./docker/uwscli
+exit 0
--- docs/infra/upgrades.md
+++ docs/infra/upgrades.md
@@ -101,7 +101,8 @@
     * `2203`: base-2203, devel golang-2203
     * 2109: base-2109
 * docker/uwscli
-    * `2109`: python-2109
+    * `2203`: python-2203
+    * 2109: python-2109
 * srv/proftpd
     * `2109`: base-2109
 * srv/munin
