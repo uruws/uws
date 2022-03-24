689136cb0273fdbaca1f24869f5325ad4018b908
Jeremías <jeremias@talkingpts.org>
2022-03-24 13:18:28 -0300

    awscli entrypoint

--- docker/awscli/run.sh
+++ docker/awscli/run.sh
@@ -2,6 +2,8 @@
 set -eu
 awsdir=${PWD}/secret/aws
 docker run -it --rm --name uws-awscli --hostname awscli.uws.local -u uws \
+	--entrypoint /bin/bash \
+	--workdir /home/uws \
 	-v ${awsdir}:/home/uws/.aws \
 	uws/awscli-2203 "$@"
 exit 0
