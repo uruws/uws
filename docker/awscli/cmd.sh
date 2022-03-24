1012173be9d03bbbf042468a29b1066f47d431a2
Jeremías <jeremias@talkingpts.org>
2022-03-24 11:48:51 -0300

    awscli: use secret aws credentials

--- docker/awscli/cmd.sh
+++ docker/awscli/cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
-mkdir -vp ~/.aws
+awsdir=${PWD}/secret/aws
 docker run --rm --name uws-awscli-cmd --hostname awscli-cmd.uws.local -u uws \
-	-v ~/.aws:/home/uws/.aws \
+	-v ${awsdir}:/home/uws/.aws \
 	uws/awscli-2203 "$@"
 exit 0
--- docker/awscli/run.sh
+++ docker/awscli/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
-mkdir -vp ~/.uws/aws
+awsdir=${PWD}/secret/aws
 docker run -it --rm --name uws-awscli --hostname awscli.uws.local -u uws \
-	-v ~/.uws/aws:/home/uws/.aws \
+	-v ${awsdir}:/home/uws/.aws \
 	uws/awscli-2203 "$@"
 exit 0
