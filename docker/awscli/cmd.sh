c8902243adadf7c0cd8baf24c0650a71083b5887
Jeremías <jeremias@talkingpts.org>
2022-03-24 18:14:10 -0300

    fix shellcheck

--- docker/awscli/cmd.sh
+++ docker/awscli/cmd.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -eu
 awsdir=${PWD}/secret/aws
-utils=${PWD}/docker/awscli/utils
 docker run --rm --name uws-awscli-cmd --hostname awscli-cmd.uws.local -u uws \
 	--env-file ${awsdir}/cli.env \
 	-v ${awsdir}:/home/uws/.aws:ro \
