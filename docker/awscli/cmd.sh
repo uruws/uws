dd67b9d0f342fdaade5253ff41d853a299b3d1a0
Jeremías <jeremias@talkingpts.org>
2022-03-24 17:23:08 -0300

    awscli: utils dir

--- docker/awscli/cmd.sh
+++ docker/awscli/cmd.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -eu
 awsdir=${PWD}/secret/aws
+utils=${PWD}/docker/awscli/utils
 docker run --rm --name uws-awscli-cmd --hostname awscli-cmd.uws.local -u uws \
 	--env-file ${awsdir}/cli.env \
 	-v ${awsdir}:/home/uws/.aws \
+	-v ${utils}:/home/uws/bin \
 	uws/awscli-2203 "$@"
 exit 0
--- docker/awscli/run.sh
+++ docker/awscli/run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -eu
 awsdir=${PWD}/secret/aws
+utils=${PWD}/docker/awscli/utils
 docker run -it --rm --name uws-awscli --hostname awscli.uws.local -u uws \
 	--entrypoint /bin/bash \
 	--workdir /home/uws \
 	--env-file ${awsdir}/cli.env \
 	-v ${awsdir}:/home/uws/.aws \
-	uws/awscli-2203 "$@"
+	-v ${utils}:/home/uws/bin \
+	uws/awscli-2203 -il
 exit 0
