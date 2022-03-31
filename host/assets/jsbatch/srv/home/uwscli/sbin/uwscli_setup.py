5ad85b7d9b9ebcb40f8be14e18fe8b6926f1e306
Jeremías <jeremias@talkingpts.org>
2022-03-31 17:41:17 -0300

    uwscli: run setup script

--- host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_setup.py
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_setup.py
@@ -12,6 +12,10 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 import uwscli
 
+_run = {
+	0: 'uwscli_setup.sh',
+}
+
 def main(argv = []):
 	flags = ArgumentParser(formatter_class = RawDescriptionHelpFormatter,
 		description = __doc__, epilog = uwscli.deploy_description())
@@ -19,6 +23,13 @@ def main(argv = []):
 		version = uwscli.version())
 
 	args = flags.parse_args(argv)
+
+	for i in sorted(_run.keys()):
+		cmd = f"/srv/home/uwscli/sbin/{_run[i]}"
+		rc = uwscli.system(cmd, env = {'PATH': '/bin:/usr/bin:/sbin:/usr/sbin'})
+		if rc != 0:
+			return rc
+
 	return 0
 
 if __name__ == '__main__':
