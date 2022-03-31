1ea4ac37aee4fbd95496f4e0b6bb79cdbcb79e18
Jeremías <jeremias@talkingpts.org>
2022-03-31 18:01:55 -0300

    uwscli: app setup script

---
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_app.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+umask 0027
+
+groupname=${1:?'group name?'}
+
+addgroup "${groupname}" || true
+
+for username in $*; do
+	adduser "${username}" "${groupname}"
+done
+
+exit 0
