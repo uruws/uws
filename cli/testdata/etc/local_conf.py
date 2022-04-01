f7eb680e0b00878fb1687b2f899de055f62a3b76
Jeremías <jeremias@talkingpts.org>
2022-04-01 00:12:32 -0300

    uwscli: fix local conf tests

--- cli/testdata/etc/local_conf.py
+++ cli/testdata/etc/local_conf.py
@@ -1,3 +1,6 @@
+import uwscli
+
 from uwscli_conf import App
-app = {'local_conf': App(False)}
-cluster = {'local_conf': {'region': 'testing'}}
+
+uwscli.app['local_conf'] = App(False)
+uwscli.cluster['local_conf'] = {'region': 'testing'}
