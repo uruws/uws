988db3926f9b79dd291fb7b696392a2c26a77e47
Jeremías <jeremias@talkingpts.org>
2022-04-06 23:06:26 -0300

    uwscli: test chroot local_conf.py

--- cli/schroot/start.sh
+++ cli/schroot/start.sh
@@ -15,12 +15,17 @@ cleanup() {
 	schroot -c ${sess} -e
 }
 
-trap cleanup INT EXIT
-
 schroot -c uwscli-${profile} -n ${sess} -b
 
-schroot_sess="schroot -c ${sess} -r"
+trap cleanup INT EXIT
 
-${schroot_sess} -d /root -u root -- /srv/home/uwscli/sbin/${service}_init.sh
+schroot_cmd="schroot -c ${sess} -d /root -u root -r"
+
+${schroot_cmd} -- install -v -d -o root -g uwscli -m 0750 /etc/uws/cli
+${schroot_cmd} -- install -v -C -o root -g uwscli -m 0640 \
+	/usr/local/etc/local_conf.py \
+	/etc/uws/cli/local_conf.py
+
+${schroot_cmd} -- /srv/home/uwscli/sbin/${service}_init.sh
 
 exit 0
