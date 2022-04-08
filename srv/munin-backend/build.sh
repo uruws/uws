c1d2ee9c443b3afe079770444f09027ef5124d96
Jeremías <jeremias@talkingpts.org>
2022-04-08 11:54:38 -0300

    docker: remove deps for uws/munin

--- srv/munin-backend/build.sh
+++ srv/munin-backend/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -eu
 # munin-backend
-docker build $@ --rm -t uws/munin-backend \
-	-f srv/munin-backend/Dockerfile \
-	./srv/munin-backend
+#docker build $@ --rm -t uws/munin-backend \
+#	-f srv/munin-backend/Dockerfile \
+#	./srv/munin-backend
+docker rmi uws/munin-backend || true
 # munin-backend-2203
 docker build $@ --rm -t uws/munin-backend-2203 \
 	-f srv/munin-backend/Dockerfile.2203 \
