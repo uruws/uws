20f567ae079cae1228821faa8ebe4ce72af8747d
Jeremías <jeremias@talkingpts.org>
2022-04-07 21:17:53 -0300

    docker: build latest base image only

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -5,18 +5,20 @@ if test 'X0' = "X$(id -u)"; then
 	exit 1
 fi
 # base
-docker build "$@" --rm -t uws/base \
-	--build-arg "UWS_UID=$(id -u)" \
-	--build-arg "UWS_GID=$(id -g)" \
-	--build-arg "UWS_UMASK=$(umask)" \
-	./docker/base
+#docker build "$@" --rm -t uws/base \
+#	--build-arg "UWS_UID=$(id -u)" \
+#	--build-arg "UWS_GID=$(id -g)" \
+#	--build-arg "UWS_UMASK=$(umask)" \
+#	./docker/base
+docker rmi uws/base || true
 # base.2109
-docker build "$@" --rm -t uws/base-2109 \
-	--build-arg "UWS_UID=$(id -u)" \
-	--build-arg "UWS_GID=$(id -g)" \
-	--build-arg "UWS_UMASK=$(umask)" \
-	-f docker/base/Dockerfile.2109 \
-	./docker/base
+#docker build "$@" --rm -t uws/base-2109 \
+#	--build-arg "UWS_UID=$(id -u)" \
+#	--build-arg "UWS_GID=$(id -g)" \
+#	--build-arg "UWS_UMASK=$(umask)" \
+#	-f docker/base/Dockerfile.2109 \
+#	./docker/base
+docker rmi uws/base-2109 || true
 # base.2203
 docker build "$@" --rm -t uws/base-2203 \
 	--build-arg "UWS_UID=$(id -u)" \
