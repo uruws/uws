4f5b723680facaab062f94ec19eccf7590093d87
Jeremías <jeremias@talkingpts.org>
2022-04-07 21:00:06 -0300

    uwscli: schroot login start/stop docker daemon

--- cli/schroot/login.sh
+++ cli/schroot/login.sh
@@ -9,15 +9,17 @@ if ! test -d ./cli/schroot/${profile}; then
 fi
 
 sess=$(schroot -c uwscli-${profile} -b)
+schroot_sess="schroot -c ${sess} -d /root -u root -r"
 
 cleanup() {
+	${schroot_sess} -- /etc/init.d/docker stop
 	schroot -c ${sess} -e
 }
 
 trap cleanup INT EXIT
 
-schroot_sess="schroot -c ${sess} -r"
-
-${schroot_sess} -d /root -u root -- /bin/bash -i
+${schroot_sess} -- /etc/init.d/docker start
+sleep 1
+${schroot_sess} -- /bin/bash -i
 
 exit 0
