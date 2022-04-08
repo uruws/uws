147fcd58e420bd326819a0f066bc0a3eca73eb47
Jeremías <jeremias@talkingpts.org>
2022-04-08 11:53:31 -0300

    docker: remove deps for uws/mailx

--- srv/crond/build.sh
+++ srv/crond/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -eu
 # crond
-docker build --rm -t uws/crond \
-	-f srv/crond/Dockerfile \
-	./srv/crond
+#docker build --rm -t uws/crond \
+#	-f srv/crond/Dockerfile \
+#	./srv/crond
+docker rmi uws/crond || true
 # crond-2203
 docker build --rm -t uws/crond-2203 \
 	-f srv/crond/Dockerfile.2203 \
