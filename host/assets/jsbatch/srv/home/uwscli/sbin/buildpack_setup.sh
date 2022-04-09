665bd0ff856593562fe027eb80e3f98c95358032
Jeremías <jeremias@talkingpts.org>
2022-04-09 02:00:41 -0300

    uwscli: do not fail with builpack repo update

--- host/assets/jsbatch/srv/home/uwscli/sbin/buildpack_setup.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/buildpack_setup.sh
@@ -9,7 +9,7 @@ uri=${2:?'repo uri?'}
 uwsrun='sudo -n -u uws'
 
 if test -d "${workdir}"; then
-	${uwsrun} git -C "${workdir}" pull --no-rebase
+	${uwsrun} git -C "${workdir}" pull --no-rebase || true
 else
 	${uwsrun} git clone "${uri}" "${workdir}"
 fi
