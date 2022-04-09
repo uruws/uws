8c69bf8a7f6b01d7ab2b20ef1a3f9246c833d353
Jeremías <jeremias@talkingpts.org>
2022-04-09 01:45:40 -0300

    uwscli: do not bootstrap Buildpack at init

--- host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_init.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_init.sh
@@ -4,6 +4,5 @@ set -eu
 uwsrun='sudo -n -u uws'
 
 ${uwsrun} make -C /srv/uws/deploy uwscli-setup-schroot
-${uwsrun} make -C /srv/deploy/Buildpack bootstrap
 
 exec /srv/home/uwscli/sbin/sshd_init.sh
