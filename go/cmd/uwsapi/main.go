d09c9ecdbbf051a3d69562f0b47ce3e04a4dd4a3
Jeremías <jeremias@talkingpts.org>
2022-04-12 18:15:37 -0300

    uwsapi: initial commit

---
+++ go/cmd/uwsapi/main.go
@@ -0,0 +1,52 @@
+// Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+// See LICENSE file.
+
+// Package main implements uwsapi web service.
+package main
+
+import (
+	"net/http"
+	"os"
+
+	//~ "uws/api"
+	"uws/log"
+	"uws/wapp"
+)
+
+var (
+	listenAndServe func(string, http.Handler) error
+	osHostname     func() (string, error)
+)
+
+func init() {
+	listenAndServe = http.ListenAndServe
+	osHostname = os.Hostname
+}
+
+func main() {
+	log.Init("uwsapi")
+	log.Debug("main init")
+
+	http.HandleFunc("/_/healthz", healthzHandler)
+	http.HandleFunc("/_", pingHandler)
+
+	log.Fatal("%s", listenAndServe(":3800", nil))
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	start := wapp.Start()
+	if _, err := osHostname(); err != nil {
+		wapp.Error(w, r, start, err)
+	} else {
+		wapp.Write(w, r, start, "ok\n")
+	}
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	start := wapp.Start()
+	if hostname, err := osHostname(); err != nil {
+		wapp.Error(w, r, start, err)
+	} else {
+		wapp.Write(w, r, start, "ctl@%s\n", hostname)
+	}
+}
---
+++ go/testing/run/03-api.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+TMPDIR=/go/tmp/uwsapi
+rm -rf ${TMPDIR}
+install -d -m 0750 ${TMPDIR}
+
+go test -coverprofile ${TMPDIR}/coverage.out ./cmd/uwsapi/...
+
+covd=${TMPDIR}/htmlcov
+mkdir -p ${covd}
+
+exec go tool cover -html ${TMPDIR}/coverage.out \
+	-o ${covd}/coverage.html
