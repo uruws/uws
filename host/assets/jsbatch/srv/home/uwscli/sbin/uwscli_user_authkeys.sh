eda9b962718fc01026946b8832f3678303211f64
Jeremías <jeremias@talkingpts.org>
2022-04-13 12:52:10 -0300

    uwscli: user authkeys use .new temp file

--- host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
@@ -6,12 +6,13 @@ homedir=${1:?'home dir?'}
 username=${2:?'username?'}
 keyid=${3:?'keyid?'}
 
+akfn="${homedir}/.ssh/authorized_keys"
+
 install -v -d -o "${username}" -g "${username}" -m 0550 "${homedir}/.ssh"
 
-akfn="${homedir}/.ssh/authorized_keys"
-ssh-import-id --output "${akfn}" "gh:${keyid}"
+ssh-import-id --output "${akfn}".new "gh:${keyid}"
 
-chown -v "${username}:${username}" "${akfn}"
-chmod -v 0400 "${akfn}"
+install -v -C -o "${username}" -g "${username}" -m 0400 "${akfn}".new "${akfn}"
 
+rm -f "${akfn}".new
 exit 0
