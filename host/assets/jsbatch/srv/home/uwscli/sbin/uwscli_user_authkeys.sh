bbc060dba32225967da27e69d6b71e5af1008fd4
Jeremías <jeremias@talkingpts.org>
2022-04-13 13:02:53 -0300

    uwscli: fix internal ssh pub key path

--- host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
@@ -12,7 +12,7 @@ install -v -d -o "${username}" -g "${username}" -m 0750 "${homedir}/${username}/
 
 rm -f "${akfn}.new"
 ssh-import-id --output "${akfn}.new" "gh:${keyid}"
-cat /srv/uwscli/test/secret/ssh/id_ed25519.pub >>"${akfn}.new"
+cat /usr/local/etc/ssh/id_ed25519.pub >>"${akfn}.new"
 
 install -v -C -o "${username}" -g "${username}" -m 0400 "${akfn}.new" "${akfn}"
 
