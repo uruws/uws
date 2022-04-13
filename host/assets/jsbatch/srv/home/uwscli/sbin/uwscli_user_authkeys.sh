2e47cd821d9b8676caaba774091f643beab7511e
Jeremías <jeremias@talkingpts.org>
2022-04-13 12:59:02 -0300

    uwscli: add internal ssh pub key to each user

--- host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
@@ -6,13 +6,15 @@ homedir=${1:?'home dir?'}
 username=${2:?'username?'}
 keyid=${3:?'keyid?'}
 
-akfn="${homedir}/.ssh/authorized_keys"
+akfn="${homedir}/${username}/.ssh/authorized_keys"
 
-install -v -d -o "${username}" -g "${username}" -m 0550 "${homedir}/.ssh"
+install -v -d -o "${username}" -g "${username}" -m 0750 "${homedir}/${username}/.ssh"
 
-ssh-import-id --output "${akfn}".new "gh:${keyid}"
+rm -f "${akfn}.new"
+ssh-import-id --output "${akfn}.new" "gh:${keyid}"
+cat /srv/uwscli/test/secret/ssh/id_ed25519.pub >>"${akfn}.new"
 
-install -v -C -o "${username}" -g "${username}" -m 0400 "${akfn}".new "${akfn}"
+install -v -C -o "${username}" -g "${username}" -m 0400 "${akfn}.new" "${akfn}"
 
-rm -f "${akfn}".new
+rm -f "${akfn}.new"
 exit 0
