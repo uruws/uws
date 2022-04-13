6fa8f7ecdc24c70a076f32eddbfd52eff2946e35
Jeremías <jeremias@talkingpts.org>
2022-04-13 11:35:05 -0300

    uwscli: fix user authkey

--- host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
@@ -9,7 +9,7 @@ keyid=${3:?'keyid?'}
 install -v -d -o "${username}" -g "${username}" -m 0750 "${homedir}/.ssh"
 
 akfn="${homedir}/.ssh/authorized_keys"
-ssh-import-id-gh --output "${akfn}" "${keyid}"
+ssh-import-id --output "${akfn}" "gh:${keyid}"
 
 install -v -d -o "${username}" -g "${username}" -m 0600 "${akfn}"
 exit 0
