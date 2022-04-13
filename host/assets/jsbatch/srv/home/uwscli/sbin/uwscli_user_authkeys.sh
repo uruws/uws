cf8d316bf47585c84585fb40b7987dfb7e1116b5
Jeremías <jeremias@talkingpts.org>
2022-04-13 11:49:57 -0300

    uwscli: fix authkeys file perms

--- host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_user_authkeys.sh
@@ -6,10 +6,12 @@ homedir=${1:?'home dir?'}
 username=${2:?'username?'}
 keyid=${3:?'keyid?'}
 
-install -v -d -o "${username}" -g "${username}" -m 0750 "${homedir}/.ssh"
+install -v -d -o "${username}" -g "${username}" -m 0550 "${homedir}/.ssh"
 
 akfn="${homedir}/.ssh/authorized_keys"
 ssh-import-id --output "${akfn}" "gh:${keyid}"
 
-install -v -d -o "${username}" -g "${username}" -m 0600 "${akfn}"
+chown -v "${username}:${username}" "${akfn}"
+chmod -v 0400 "${akfn}"
+
 exit 0
