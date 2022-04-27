43e8b271c43eb0869fd6a46d834471c9dc93deb5
Jeremías <jeremias@talkingpts.org>
2022-04-27 18:46:19 -0300

    munin: deploy cluster local conf if exists

--- k8s/mon/munin/configure.sh
+++ k8s/mon/munin/configure.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
+
 # smtps CA
 CA=${HOME}/ca/uws/smtps/211006/client
 uwskube delete secret smtps-ca -n mon || true
@@ -9,7 +10,14 @@ uwskube create secret generic smtps-ca -n mon \
 # cluster.conf
 conf=/tmp/mon.munin.cluster.conf
 envsubst <~/k8s/mon/munin/conf.d/cluster.conf >${conf}
+# cluster_local.conf
+local_conf=${HOME}/cluster/munin/conf.d/cluster_local.conf
+if test -s ${local_conf}; then
+	echo '# cluster_local.conf' >>${conf}
+	envsubst <${local_conf} >>${conf}
+fi
 uwskube delete configmap munin-confd -n mon || true
 uwskube create configmap munin-confd -n mon \
 	--from-file="cluster.conf=${conf}"
+
 exit 0
