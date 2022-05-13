db2031a99afd47f00179782d56f28b039923df88
Jeremías <jeremias@talkingpts.org>
2022-05-13 16:04:10 -0300

    awscli: env manager scripts

---
+++ docker/awscli/env-devel.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -eu
+envname=${1:?'env name?'}
+
+awsdir=${PWD}/secret/aws.env/${envname}
+if ! test -d ${awsdir}; then
+	echo "${awsdir}: env not found" >&2
+	exit 1
+fi
+
+utils=${PWD}/docker/awscli/utils
+cfgdir=${PWD}/secret/aws.config
+
+tmpdir=${PWD}/tmp/awscli
+install -v -d -m 0750 ${tmpdir}
+
+exec docker run -it --rm --name uws-awscli-env-${envname}-devel \
+	--hostname awscli-env-${envname}-devel.uws.local \
+	-u uws \
+	--entrypoint /bin/bash \
+	--workdir /home/uws \
+	--env-file ${awsdir}/cli.env \
+	-v ${awsdir}:/home/uws/.aws:ro \
+	-v ${cfgdir}:/home/uws/config:ro \
+	-v ${utils}:/home/uws/bin:ro \
+	-v ${tmpdir}:/home/uws/tmp \
+	uws/awscli-2203 -il
---
+++ docker/awscli/env-run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eu
+
+envname=${1:?'env name?'}
+CMD=${2:?'command?'}
+
+cd "$(dirname $0)"/../../
+
+awsdir=${PWD}/secret/aws.env/${envname}
+if ! test -d ${awsdir}; then
+	echo "${awsdir}: env not found" >&2
+	exit 1
+fi
+
+awsdir=${PWD}/secret/aws
+utils=${PWD}/docker/awscli/utils
+
+docker run --rm --name uws-awscli-env-${envname} \
+	--hostname awscli-env-${envname}.uws.local \
+	-u uws \
+	--entrypoint "/home/uws/bin/${CMD}" \
+	--workdir /home/uws \
+	--env-file ${awsdir}/cli.env \
+	-v ${awsdir}:/home/uws/.aws:ro \
+	-v ${utils}:/home/uws/bin:ro \
+	uws/awscli-2203
+
+exit 0
