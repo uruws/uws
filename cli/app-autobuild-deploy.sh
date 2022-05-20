c61fc962eade1a2694b8b52dfa5b9e2fee9dbb7d
Jeremías <jeremias@talkingpts.org>
2022-05-20 14:06:32 -0300

    uwscli: debug autobuild deploys

--- cli/app-autobuild-deploy.sh
+++ cli/app-autobuild-deploy.sh
@@ -6,6 +6,9 @@ version=${2:?'version?'}
 
 /srv/uws/deploy/cli/auth.py --user "${SUDO_USER}" --ops "${app}"
 
+export UWSCLI_LOG=on
+export UWSCLI_DEBUG=on
+
 /srv/home/uwscli/bin/app-autobuild --deploy "${app}" "${version}"
 
 exit 0
