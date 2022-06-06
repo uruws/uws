2c73c31a382c6eee4c934f627997741baa764f69
Jeremías <jeremias@talkingpts.org>
2022-06-06 18:03:17 -0300

    infra-ui: official domains

--- pod/meteor/infra-ui/gateway-setup.sh
+++ pod/meteor/infra-ui/gateway-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eu
 
-if test "X${INFRA_UI_ENV}" = 'Xtest'; then
-	~/k8s/ca/uws/opstest/setup.sh
-fi
+#if test "X${INFRA_UI_ENV}" = 'Xtest'; then
+#	~/k8s/ca/uws/opstest/setup.sh
+#fi
 
 envsubst <${HOME}/pod/meteor/infra-ui/gateway.yaml | uwskube apply -f -
 
