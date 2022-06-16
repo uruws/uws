29dbc64a794f04de1267d902242a85fffc15d994
Jeremías <jeremias@talkingpts.org>
2022-06-16 18:22:10 -0300

    app: fix cluster create script

--- cluster/app/create.sh
+++ cluster/app/create.sh
@@ -7,13 +7,12 @@ if test "X${ec2_spot}" = 'Xtrue'; then
 	EXTRA_ARGS='--spot'
 fi
 
-#~ uwseks-cluster-create --profile "${AWS_PROFILE}" --region "${AWS_REGION}"
-echo -- --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
+uwseks-cluster-create --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
 	--zones "${AWS_ZONES}" --k8s-version "${K8S_VERSION}" \
 	--nodes 10 --nodes-min 5 --nodes-max 100 \
 	--instance-types "${AWS_INSTANCE_TYPES}" \
 	${EXTRA_ARGS} "${UWS_CLUSTER}"
 
-#~ sleep 3
+sleep 3
 
-#~ exec uwseks-cluster-setup
+exec uwseks-cluster-setup
