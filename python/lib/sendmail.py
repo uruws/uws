a77043e77f221687ef1287ab2bcbaaf29fcaed76
Jeremías <jeremias@talkingpts.org>
2022-07-05 18:27:39 -0300

    sendmail: fix timeout int type

--- python/lib/sendmail.py
+++ python/lib/sendmail.py
@@ -11,8 +11,8 @@ from email.parser import BytesParser
 from smtplib import SMTP_SSL
 
 SMTPS_SERVER  = os.getenv('UWS_SMTPS',         '127.0.0.1')
-SMTPS_PORT    = os.getenv('UWS_SMTPS_PORT',    465)
-SMTPS_TIMEOUT = os.getenv('UWS_SMTPS_TIMEOUT', 7)
+SMTPS_PORT    = os.getenv('UWS_SMTPS_PORT',    '465')
+SMTPS_TIMEOUT = os.getenv('UWS_SMTPS_TIMEOUT', '7')
 SMTPS_CERT    = os.getenv('UWS_SMTPS_CERT',    '/etc/ssl/certs/ssl-cert-snakeoil.pem')
 SMTPS_KEY     = os.getenv('UWS_SMTPS_KEY',     '/etc/ssl/private/ssl-cert-snakeoil.key')
 SMTPS_USER    = os.getenv('UWS_SMTPS_USER',    '')
@@ -22,7 +22,7 @@ def _smtpServer():
 	s = SMTP_SSL(
 		host = SMTPS_SERVER,
 		port = SMTPS_PORT,
-		timeout = SMTPS_TIMEOUT,
+		timeout = int(SMTPS_TIMEOUT),
 		certfile = SMTPS_CERT,
 		keyfile = SMTPS_KEY,
 	)
