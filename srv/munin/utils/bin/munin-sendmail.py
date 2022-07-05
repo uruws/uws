0112e8fef9b033b45febfa3fbbe49ef9c77e0218
Jeremías <jeremias@talkingpts.org>
2022-07-05 15:55:20 -0300

    munin: sendmail fix QDIR

--- srv/munin/utils/bin/munin-sendmail.py
+++ srv/munin/utils/bin/munin-sendmail.py
@@ -6,10 +6,10 @@
 import sys
 
 sys.path.insert(0, '/opt/munin/lib')
-import alerts
+import alerts_conf as conf
 
 sys.path.insert(0, '/opt/uws/lib')
 import sendmail
 
 if __name__ == '__main__':
-	sys.exit(sendmail.qdir(alerts.QDIR))
+	sys.exit(sendmail.qdir(conf.QDIR))
