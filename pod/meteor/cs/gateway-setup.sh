2d7bc5b480e41046f782b2425e09f480599490df
Jeremías <jeremias@talkingpts.org>
2022-08-10 12:27:29 -0300

    cs: web gateway setup offline-page

--- pod/meteor/cs/gateway-setup.sh
+++ pod/meteor/cs/gateway-setup.sh
@@ -3,4 +3,6 @@ set -eu
 cat ~/pod/meteor/cs/gateway-${METEOR_CS_ENV}.yaml \
 	~/pod/meteor/cs/gateway-common.yaml |
 	envsubst | uwskube apply -f -
+./k8s/offline-page/deploy.sh
+./k8s/offline-page/setup.sh meteor-cs-gateway
 exit 0
