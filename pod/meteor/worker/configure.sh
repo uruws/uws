7da49730ca80c387c712e182e7b71be3979e8069
Jeremías <jeremias@talkingpts.org>
2022-08-11 15:29:33 -0300

    meteor: worker configure settings.json file

--- pod/meteor/worker/configure.sh
+++ pod/meteor/worker/configure.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 set -eu
+
 appenv=${HOME}/secret/meteor/app/${APP_ENV}.env
 echo "app.env: ${appenv}"
+
+appset=${HOME}/secret/meteor/app/${APP_ENV}-settings.json
+echo "app-settings.json: ${appset}"
+
 uwskube delete secret -n worker meteor-app-env || true
-uwskube create secret generic -n worker meteor-app-env --from-file="app.env=${appenv}"
+
+if test -s "${appset}"; then
+	uwskube create secret generic -n worker meteor-app-env \
+		--from-file="app.env=${appenv}" \
+		--from-file="app-settings.json=${appset}"
+else
+	uwskube create secret generic -n worker meteor-app-env \
+		--from-file="app.env=${appenv}"
+fi
+
 exit 0
