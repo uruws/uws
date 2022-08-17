385fca42dce354ec2af62995070d33bc204a7877
Jeremías <jeremias@talkingpts.org>
2022-08-16 21:08:48 -0300

    pod: last state format output

--- pod/lib/last-state.sh
+++ pod/lib/last-state.sh
@@ -3,7 +3,7 @@ set -eu
 ns=${1:?'namespace?'}
 for pod in $(uwskube get pods -n "${ns}" | cut -d ' ' -f 1 | tail -n +2)
 do
-	echo "*** ${pod}"
+	echo "* ${pod}"
 	uwskube describe "pod/${pod}" -n "${ns}" | grep -E 'Last State:|Reason:'
 done
 exit 0
