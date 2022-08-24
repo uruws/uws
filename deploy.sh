581d7b135265cae974cf6010b5b390deb6fc2d9a
Jeremías <jeremias@talkingpts.org>
2022-08-24 15:02:17 -0300

    improve deploy script and its doc

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,44 @@
 #!/bin/sh
 set -eu
-nice ionice make check
+servername='ops.uws.talkingpts.org'
 if ! git remote | grep -q deploy; then
-	git remote add deploy uws@ops.uws.talkingpts.org:/srv/uws/deploy.git
+	git remote add deploy "uws@${servername}:/srv/uws/deploy.git"
 fi
-git push deploy
+deploy_run='TRUE'
+check_run='TRUE'
+tail_run='TRUE'
+for arg in $@; do
+	case ${arg} in
+		--no-test|-T)
+			check_run='FALSE'
+		;;
+		--no-logs|-L)
+			tail_run='FALSE'
+		;;
+		--no-deploy|-D)
+			deploy_run='FALSE'
+		;;
+		--debug)
+			set -x
+		;;
+	esac
+done
+if test "X${check_run}" = 'XTRUE'; then
+	check_cmd=""
+	if which nice >/dev/null; then
+		check_cmd="nice"
+	fi
+	if which ionice >/dev/null; then
+		check_cmd="${check_cmd} ionice"
+	fi
+	check_cmd="${check_cmd} make check"
+	${check_cmd}
+fi
+if test "X${deploy_run}" = 'XTRUE'; then
+	git push
+	git push deploy
+	if test "X${tail_run}" = 'XTRUE'; then
+		exec ssh -a -C -n -x -l uws "${servername}" tail -f /var/tmp/uws-deploy.log
+	fi
+fi
 exit 0
--- docs/deploy.md
+++ docs/deploy.md
@@ -1,5 +1,7 @@
 # Infrastructure Deploy
 
+## Manually
+
 If not done already, configure the deploy repository first:
 
 	$ git remote add deploy uws@ops.uws.talkingpts.org:/srv/uws/deploy.git
@@ -17,3 +19,21 @@ To check the deploy log SSH to the ops server:
 And check the log file under `/var/tmp/uws-deploy.log`
 
 	$ tail -f /var/tmp/uws-deploy.log
+
+## Automated
+
+Or you can run the `deploy.sh` script which kind of automates the steps above.
+
+To run the tests locally first, deploy and follow the log, run:
+
+	$ ./deploy.sh
+
+You have to finish it with *CTRL+C*.
+
+Avoid running tests locally with:
+
+	$ ./deploy.sh --no-test
+
+Avoid following the deploy log with:
+
+	$ ./deploy.sh --no-logs
