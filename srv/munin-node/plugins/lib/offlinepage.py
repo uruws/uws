8d972bc7ec82fc3f173b1f648ce918d03a2715cf
Jeremías <jeremias@talkingpts.org>
2022-09-26 15:01:02 -0300

    munin-node: offline page config

--- srv/munin-node/plugins/lib/offlinepage.py
+++ srv/munin-node/plugins/lib/offlinepage.py
@@ -3,10 +3,40 @@
 
 import sys
 
-def config() -> int:
+from os import getenv
+
+import mnpl_utils as utils
+
+_category: str = getenv('category', 'network').strip()
+_app:      str = getenv('app',      'app').strip()
+_target:   str = getenv('target',   'http://localhost').strip()
+
+def config(t: str) -> int:
+	utils.println('graph_title offline page', _target)
+	utils.println('graph_args --base 1000 -l 0')
+	utils.println('graph_category', _category)
+	utils.println('graph_vlabel', _app)
+	utils.println('a_running.label running')
+	utils.println('a_running.colour COLOUR0')
+	utils.println('a_running.draw AREA')
+	utils.println('a_running.min 0')
+	utils.println('a_running.critical 1')
+	utils.println('b_offline.label offline')
+	utils.println('b_offline.colour COLOUR1')
+	utils.println('b_offline.draw AREA')
+	utils.println('b_offline.min 0')
+	utils.println('b_offline.critical 1')
+	utils.println('c_error.label error')
+	utils.println('c_error.colour COLOUR2')
+	utils.println('c_error.draw AREA')
+	utils.println('c_error.min 0')
+	utils.println('c_error.critical 1')
 	return 0
 
-def report() -> int:
+def report(t: str) -> int:
+	utils.println('a_running.value U')
+	utils.println('b_offline.value U')
+	utils.println('c_error.value U')
 	return 0
 
 def main(argv: list[str]) -> int:
@@ -14,9 +44,10 @@ def main(argv: list[str]) -> int:
 		action = argv[0]
 	except IndexError:
 		action = 'report'
+	t = _target.strip()
 	if action == 'config':
-		return config()
-	return report()
+		return config(t)
+	return report(t)
 
 if __name__ == '__main__': # pragma: no cover
 	sys.exit(main(sys.argv[1:]))
--- srv/munin-node/test/plugins/offlinepage_test.py
+++ srv/munin-node/test/plugins/offlinepage_test.py
@@ -4,8 +4,10 @@
 # See LICENSE file.
 
 import unittest
+from unittest.mock import call
 
 import mnpl_t
+import mnpl_utils
 import offlinepage
 
 class Test(unittest.TestCase):
@@ -21,6 +23,29 @@ class Test(unittest.TestCase):
 
 	def test_main_config(t):
 		t.assertEqual(offlinepage.main(['config']), 0)
+		calls = [
+			call('graph_title offline page', 'http://localhost'),
+			call('graph_args --base 1000 -l 0'),
+			call('graph_category', 'network'),
+			call('graph_vlabel', 'app'),
+			call('a_running.label running'),
+			call('a_running.colour COLOUR0'),
+			call('a_running.draw AREA'),
+			call('a_running.min 0'),
+			call('a_running.critical 1'),
+			call('b_offline.label offline'),
+			call('b_offline.colour COLOUR1'),
+			call('b_offline.draw AREA'),
+			call('b_offline.min 0'),
+			call('b_offline.critical 1'),
+			call('c_error.label error'),
+			call('c_error.colour COLOUR2'),
+			call('c_error.draw AREA'),
+			call('c_error.min 0'),
+			call('c_error.critical 1'),
+		]
+		mnpl_utils.println.assert_has_calls(calls)
+		t.assertEqual(mnpl_utils.println.call_count, len(calls))
 
 if __name__ == '__main__':
 	unittest.main()
