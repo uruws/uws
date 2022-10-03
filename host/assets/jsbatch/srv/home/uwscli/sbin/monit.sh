3df240bb94f3c341c0657e4f84eada2cc57d58e5
Jeremías <jeremias@talkingpts.org>
2022-10-03 17:27:49 -0300

    uwscli: schroot rsyslog setup

--- host/assets/jsbatch/srv/home/uwscli/sbin/monit.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/monit.sh
@@ -2,7 +2,9 @@
 set -eu
 
 ln -svf /etc/monit/conf-available/openssh-server /etc/monit/conf-enabled
+ln -svf /etc/monit/conf-available/rsyslog        /etc/monit/conf-enabled
 
 ln -svf /srv/home/uwscli/etc/monit/conf/docker /etc/monit/conf-enabled
 
-exec /usr/bin/monit -I -c /etc/monit/monitrc
+/etc/init.d/monit start
+exit 0
--- host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_init.sh
+++ host/assets/jsbatch/srv/home/uwscli/sbin/uwscli_init.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eu
 
-uwsrun='sudo -n -u uws'
-
 # docker
 
 echo 'export DOCKER_RAMDISK=true' >/etc/default/docker
@@ -17,8 +15,17 @@ sleep 1
 
 # make setup
 
+uwsrun='sudo -n -u uws'
+
 ${uwsrun} make -C /srv/uws/deploy uwscli-setup-schroot
 
 # monit
 
-exec /srv/home/uwscli/sbin/monit.sh
+/srv/home/uwscli/sbin/monit.sh
+
+# rsyslogd
+
+install -v /srv/home/uwscli/etc/rsyslog.conf /etc/
+install -v /srv/home/uwscli/etc/rsyslog.d/uws.conf /etc/rsyslog.d/
+
+exec /usr/sbin/rsyslogd -n
