0d54b2594a96d1d4e2c6dfe6396a93de6a1f7d65
Jeremías <jeremias@talkingpts.org>
2022-10-20 20:21:32 -0300

    nginx: upgrade to 1.4.1

--- k8s/nginx-ingress/upstream-get.sh
+++ k8s/nginx-ingress/upstream-get.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-VERSION='1.2.1'
+VERSION='1.4.0'
 wget -O - \
 	https://github.com/kubernetes/ingress-nginx/raw/controller-v${VERSION}/deploy/static/provider/aws/deploy.yaml |
 	grep -vF '#GENERATED FOR K8S' >upstream-deploy.yaml
