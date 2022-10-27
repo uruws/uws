859e057a3dea137f5c0a67c48ea540196838890c
Jeremías <jeremias@talkingpts.org>
2022-10-27 17:33:49 -0300

    host: deploy cloud-init and assets dir

--- host/deploy.sh
+++ host/deploy.sh
@@ -8,25 +8,31 @@ TMP=${PWD}/tmp/host/deploy/${HOST}
 rm -rf ${TMP}
 mkdir -vp ${TMP}
 
+CLOUDD=${TMP}/cloud-init
+mkdir -vp ${CLOUDD}
+
 # gen config files
 
 for fn in $(ls ./host/config/??_all_*.cfg ./host/config/??_${HOST}_*.cfg); do
-	dst="${TMP}/99zzzuws_$(basename ${fn})"
+	dst="${CLOUDD}/99zzzuws_$(basename ${fn})"
 	cp -vf ${fn} ${dst}
 done
 
 # gen assets
 
-init="${TMP}/99zzzuws_deploy.sh"
+init="${CLOUDD}/99zzzuws_deploy.sh"
 cat ./host/cloud-init.sh >${init}
 
-sush="${TMP}/99zzzuws_setup.sh"
+sush="${CLOUDD}/99zzzuws_setup.sh"
 cat ./host/setup.sh >${sush}
 
 # create assets archive
 
 SHAR='shar --compactor=xz --no-timestamp --no-i18n --quiet'
-ASSETS=${PWD}/host/assets/${HOST}
+ASSETS_SRC=${PWD}/host/assets/${HOST}
+ASSETS=${TMP}/assets
+
+rsync -ax "${ASSETS_SRC}/" "${ASSETS}/"
 
 afn="${TMP}/99zzzuws_assets.sh"
 if test -d ${ASSETS}; then
@@ -48,17 +54,17 @@ if test "X${FQDN}" = 'Xlocal'; then
 	echo "i - local deploy ${HOST}"
 
 	sudo rm -vf /etc/cloud/cloud.cfg.d/99zzzuws_*.cfg
-	sudo rsync -vrx ${TMP}/*.* /etc/cloud/cloud.cfg.d/
+	sudo rsync -vrx ${CLOUDD}/*.* /etc/cloud/cloud.cfg.d/
 
 	sudo chmod -v 0755 /etc/cloud/cloud.cfg.d/99zzzuws_deploy.sh
 	sudo /etc/cloud/cloud.cfg.d/99zzzuws_deploy.sh
 else
 	${SSH} ${FQDN} 'sudo chgrp -v admin /etc/cloud/cloud.cfg.d && sudo chmod -v g+w /etc/cloud/cloud.cfg.d && sudo rm -vf /etc/cloud/cloud.cfg.d/99zzzuws_*.cfg'
 
-	rsync -vrx -e "${SSH}" ${TMP}/*.* ${FQDN}:/etc/cloud/cloud.cfg.d/
+	rsync -vrx -e "${SSH}" ${CLOUDD}/*.* ${FQDN}:/etc/cloud/cloud.cfg.d/
 
 	${SSH} ${FQDN} 'sudo chmod -v 0755 /etc/cloud/cloud.cfg.d/99zzzuws_deploy.sh && nq -c sudo /etc/cloud/cloud.cfg.d/99zzzuws_deploy.sh'
 fi
 
-rm -vrf ${TMP}
+rm -rf ${TMP}
 exit 0
