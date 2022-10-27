5f3f30ca5aa641f45bf0b2e7ca1394825ba835e2
Jeremías <jeremias@talkingpts.org>
2022-10-27 15:17:37 -0300

    janis: uws cleanup

--- host/deploy/janis.sh
+++ host/deploy/janis.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -eu
-exec ./host/deploy.sh janis.uws.talkingpts.org janis
+./host/deploy.sh janis.uws.talkingpts.org janis
+ssh -a -n -x -l uws janis.uws.talkingpts.org tail -f /var/log/cloud-init-output.log
+exit 0
