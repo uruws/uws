a97229c49f571493257eb014ca2ed80de59c6505
Jeremias <jeremias@talkingpts.org>
2022-11-03 20:58:01 +0000

    nlpsvc: setup update

--- pod/nlpsvc/category/deploy.sh
+++ pod/nlpsvc/category/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
 version=${1:-''}
+~/pod/nlpsvc/configure.sh "${NLPSVC_ENV}"
 exec ~/pod/lib/deploy.sh nlpsvc nlpsvc/category ${version}
--- pod/nlpsvc/category/restart.sh
+++ pod/nlpsvc/category/restart.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -eu
+~/pod/nlpsvc/configure.sh "${NLPSVC_ENV}"
 exec uwskube rollout -n nlpsvc restart deploy/category
