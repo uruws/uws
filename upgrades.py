c6cf73ba951e44f7a22efe076f4f2e5d133b2dee
Jeremías <jeremias@talkingpts.org>
2022-11-16 14:29:06 -0300

    upgrades: create docker build script

--- upgrades.py
+++ upgrades.py
@@ -9,8 +9,11 @@ from subprocess import check_output
 
 __doc__ = "uws upgrades helper"
 
-FROM_VERSION = '2203'
-TO_VERSION   = '2211'
+FROM_VERSION   = '2203'
+TO_VERSION     = '2211'
+REMOVE_VERSION = ['2109']
+
+BUILD_SCRIPT = 'build.sh'
 
 # utils
 
@@ -40,6 +43,28 @@ def copyfn(src: str, dst: str):
 	check_output(f"/usr/bin/cp -va {src} {dst}",
 		timeout = 15, shell = True, text = True)
 
+def build_script(repo: str, dstdir: str, tag: str, vfrom: str, vto: str):
+	fn = Path(dstdir, BUILD_SCRIPT)
+	srctag = '%s-%s' % (tag, vfrom)
+	dsttag = '%s-%s' % (tag, vto)
+	with fn.open('w') as fh:
+		fh.write('#!/bin/sh\n')
+		fh.write('set -eu\n')
+		fh.write('# remove old versions\n')
+		for vrm in REMOVE_VERSION:
+			fh.write('docker rmi %s-%s || true\n' % (tag, vrm))
+		fh.write('# %s\n' % srctag)
+		fh.write('docker build --rm -t %s \\\n' % srctag)
+		fh.write('\t-f %s/Dockerfile.%s \\\n' % (repo, vfrom))
+		fh.write('\t./%s\n' % repo)
+		fh.write('# %s\n' % dsttag)
+		fh.write('docker build --rm -t %s \\\n' % dsttag)
+		fh.write('\t-f %s/Dockerfile.%s \\\n' % (repo, vto))
+		fh.write('\t./%s\n' % repo)
+		fh.write('exit 0\n')
+	print(fn)
+	return 0
+
 # commands
 
 def fslist(repo):
@@ -63,7 +88,7 @@ def check(repo, vfrom, vto):
 			continue
 		print(Path(repo, fn))
 
-def upgrade_docker(repo, vfrom, vto, tag):
+def upgrade_docker(repo, vfrom, vto, tag, srctag):
 	for fn in git_ls(repo, '*Dockerfile.%s' % vfrom):
 		srcfn = Path(repo, fn)
 		dstfn = Path(repo, fn.replace(vfrom, vto, 1))
@@ -71,10 +96,13 @@ def upgrade_docker(repo, vfrom, vto, tag):
 			continue
 		print(srcfn)
 		copyfn(srcfn, dstfn)
-		srctag = '%s-%s' % (tag, vfrom)
-		dsttag = '%s-%s' % (tag, vto)
-		replace(dstfn, srctag, dsttag)
+		src = '%s-%s' % (srctag, vfrom)
+		dst = '%s-%s' % (srctag, vto)
+		replace(dstfn, src, dst)
 		replace_docker_version(dstfn, date_version())
+		dstdir = dstfn.parent
+		build_script(repo, dstdir, tag, vfrom, vto)
+		print(dstfn)
 
 # main
 
@@ -88,6 +116,8 @@ def main(argv: list[str]) -> int:
 
 	flags.add_argument('-t', '--tag', metavar = 'tag', default = '',
 		help = 'docker tag')
+	flags.add_argument('-s', '--source-tag', metavar = 'srctag', default = 'uws/base',
+		help = 'docker source tag')
 
 	flags.add_argument('-C', '--check', action = 'store_true', default = False,
 		help = 'check upgrades')
@@ -104,7 +134,10 @@ def main(argv: list[str]) -> int:
 		if args.check is True:
 			return check(args.repo, args.from_version, args.to_version)
 		elif args.upgrade is True:
-			return upgrade_docker(args.repo, args.from_version, args.to_version, args.tag)
+			if args.tag == '':
+				print('[ERROR] no tag', file = sys.stderr)
+				return 1
+			return upgrade_docker(args.repo, args.from_version, args.to_version, args.tag, args.source_tag)
 		elif args.tag != '':
 			return upgrade_from_to(args.repo, args.tag, args.from_version, args.to_version)
 		else:
