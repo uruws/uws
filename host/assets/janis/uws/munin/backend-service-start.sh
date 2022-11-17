b48f93f062b7258e7ad0f1946fcae1df5fb99c51
Jeremías <jeremias@talkingpts.org>
2022-11-17 15:01:24 -0300

    docker: munin-backend 2211 upgrade

--- host/assets/janis/uws/munin/backend-service-start.sh
+++ host/assets/janis/uws/munin/backend-service-start.sh
@@ -8,6 +8,6 @@ set -eu
 	-v /srv/munin/var/lib:/var/lib/munin:ro \
 	-v /srv/munin/var/lib/cgi-tmp:/var/lib/munin/cgi-tmp \
 	-v /srv/munin/cache/www:/var/cache/munin/www:ro \
-	uws/munin-backend-2203
+	uws/munin-backend-2211
 
 exit 0
