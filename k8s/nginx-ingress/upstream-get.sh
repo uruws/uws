83799607718db3a4f8694835bc9cc39854a0e477
Jeremías <jeremias@talkingpts.org>
2022-11-23 11:16:03 -0300

    k8s: nginx-ingress 1.22 upstream

--- k8s/nginx-ingress/upstream-get.sh
+++ k8s/nginx-ingress/upstream-get.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -eu
 VERSION='1.5.1'
+K8S_VERSION='1.22'
 BASE=https://github.com/kubernetes/ingress-nginx/raw
 FPATH=deploy/static/provider/aws/deploy.yaml
-exec wget -O ./k8s/nginx-ingress/upstream-deploy.yaml \
-	"${BASE}/controller-v${VERSION}/${FPATH}"
+DST=./k8s/nginx-ingress/${K8S_VERSION}/upstream.yaml
+exec wget -O "${DST}" "${BASE}/controller-v${VERSION}/${FPATH}"
