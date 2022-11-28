716295951cc76fcd3d5b669020a14fc073c35148
Jeremías <jeremias@talkingpts.org>
2022-11-28 17:10:54 -0300

    k8s: shark deploy namespaces arg

--- k8s/mon/kubeshark/deploy.sh
+++ k8s/mon/kubeshark/deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -eu
 
+nss=${1:?'namespaces?'}
+
 envsubst '${CLUSTER_HOST}' <~/k8s/kubeshark/gateway.yaml | uwskube apply -f -
 
 #~/k8s/mon/k8s/configure.sh
 
 VERSION="$(cat ~/k8s/mon/VERSION)"
 export VERSION
+
+KUBESHARK_NAMESPACES="${nss}"
+export KUBESHARK_NAMESPACES
+
 envsubst <~/k8s/mon/kubeshark/deploy.yaml | uwskube apply -f -
 
 exit 0
