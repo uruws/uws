7d83c0214e50e81be9778bbcdd1284b9c092124e
Jeremías <jeremias@talkingpts.org>
2022-11-28 16:55:17 -0300

    k8s: shark logs

--- k8s/mon/kubeshark/deploy.sh
+++ k8s/mon/kubeshark/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-~/k8s/mon/k8s/configure.sh
+#~/k8s/mon/k8s/configure.sh
 VERSION="$(cat ~/k8s/mon/VERSION)"
 export VERSION
 envsubst <~/k8s/mon/kubeshark/deploy.yaml | uwskube apply -f -
