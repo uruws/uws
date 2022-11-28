6549cdc946a2f314d86d4057c9e85641bfc049da
Jeremías <jeremias@talkingpts.org>
2022-11-28 17:08:12 -0300

    k8s: deploy kubeshark gateway

--- k8s/mon/kubeshark/deploy.sh
+++ k8s/mon/kubeshark/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -eu
+
+envsubst '${CLUSTER_HOST}' <~/k8s/kubeshark/gateway.yaml | uwskube apply -f -
+
 #~/k8s/mon/k8s/configure.sh
+
 VERSION="$(cat ~/k8s/mon/VERSION)"
 export VERSION
 envsubst <~/k8s/mon/kubeshark/deploy.yaml | uwskube apply -f -
+
 exit 0
--- k8s/mon/kubeshark/rollin.sh
+++ k8s/mon/kubeshark/rollin.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -u
+uwskube delete ingress kubeshark-gateway
 exec uwskube delete deploy kubeshark -n mon
