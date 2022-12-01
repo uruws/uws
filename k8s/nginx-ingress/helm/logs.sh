28efe2743cbee342f438daeb0cd20e4ba1f7299e
Jeremías <jeremias@talkingpts.org>
2022-12-01 17:53:42 -0300

    k8s: nginx remove helm setup

--- k8s/nginx-ingress/helm/logs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-profile=${1:?'profile?'}
-shift
-exec uwskube -n "ingress-${profile}" logs \
-	-l "app=${profile}-nginx-ingress" \
-	--ignore-errors "$@"
--- k8s/nginx-ingress/helm/status.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-profile=${1:?'profile?'}
-uwskube get all -n "ingress-${profile}"
-exit 0
