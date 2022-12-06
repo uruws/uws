6a5bb36aee6bf3902ce6a729ee4347fdd5043867
Jeremías <jeremias@talkingpts.org>
2022-12-06 14:24:11 -0300

    meteor-vanilla: fix deploy script

--- pod/meteor/vanilla/deploy.sh
+++ pod/meteor/vanilla/deploy.sh
@@ -2,4 +2,4 @@
 set -eu
 appver=${1:-''}
 ${HOME}/pod/meteor/vanilla/configure.sh
-exec ${HOME}/pod/meteor/deploy.sh meteor-vanilla meteor-vanilla "${appver}"
+exec ${HOME}/pod/meteor/deploy.sh meteor-vanilla vanilla "${appver}"
