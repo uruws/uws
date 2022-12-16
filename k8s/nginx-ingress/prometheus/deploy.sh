d385e8465374a1eb75379501435d1fdcd3432f51
Jeremías <jeremias@talkingpts.org>
2022-12-16 16:17:12 -0300

    k8s: prometheus service patch

--- k8s/nginx-ingress/prometheus/deploy.sh
+++ k8s/nginx-ingress/prometheus/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 set -eu
-exec uwskube apply --kustomize github.com/kubernetes/ingress-nginx/deploy/prometheus/
+uwskube apply --kustomize github.com/kubernetes/ingress-nginx/deploy/prometheus/
+uwskube patch service prometheus-server \
+	--patch-file=${HOME}/k8s/nginx-ingress/prometheus/patch.yaml
+exit 0
