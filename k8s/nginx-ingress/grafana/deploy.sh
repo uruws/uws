b4586353fcdda8e5604a6b5429389f0448d2fb27
Jeremías <jeremias@talkingpts.org>
2022-12-16 16:20:01 -0300

    k8s: grafana service patch

--- k8s/nginx-ingress/grafana/deploy.sh
+++ k8s/nginx-ingress/grafana/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 set -eu
-exec uwskube apply --kustomize github.com/kubernetes/ingress-nginx/deploy/grafana/
+uwskube apply --kustomize github.com/kubernetes/ingress-nginx/deploy/grafana/
+uwskube patch service grafana -n ingress-nginx \
+	--patch-file=${HOME}/k8s/nginx-ingress/grafana/patch.yaml
+exit 0
