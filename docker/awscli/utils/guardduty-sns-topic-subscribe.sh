931c729633d49b20be1e4f2af7cfff3ce63e389d
Jeremías <jeremias@talkingpts.org>
2022-12-22 15:37:07 -0300

    vanta: guardduty notification endpoint env var

--- docker/awscli/utils/guardduty-sns-topic-subscribe.sh
+++ docker/awscli/utils/guardduty-sns-topic-subscribe.sh
@@ -5,4 +5,4 @@ exec aws sns subscribe \
 	--region "${region}" \
 	--topic-arn "arn:aws:sns:${region}:${AWS_ACCOUNT_ID}:GuardDutyEmail" \
 	--protocol email \
-	--notification-endpoint aws-guardduty-aaaaijaxjz3ej7ed6ly7y5rrru@talkingpoints.slack.com
+	--notification-endpoint "${GUARDDUTY_NOTIFICATION_ENDPOINT}"
