8291ce3e3f2ba44071e0dea61b58baed0445ea65
Jeremías <jeremias@talkingpts.org>
2022-12-22 23:58:37 -0300

    chatbot: docker base webapp image

--- srv/chatbot/devel.sh
+++ srv/chatbot/devel.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 set -eu
-
-tmpdir=${PWD}/tmp/chatbot
-install -v -d -m 0750 "${tmpdir}"
-
-exec docker run -it --rm --read-only \
-	--name uws-chatbot-devel \
-	--hostname chatbot-devel.uws.local \
-	--entrypoint /usr/local/bin/uws-login.sh \
-	-v "${tmpdir}:/home/uws/tmp" \
-	--workdir /home/uws \
-	uws/chatbot-2211
+exec ./docker/webapp/devel.sh chatbot
