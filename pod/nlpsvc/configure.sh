a314d6ba583429cb0695ba5deac1f0ed40bc7cde
aramgugusian <aram@uruworks.com>
2022-12-27 14:37:50 -0300

    reverted configure sh

--- pod/nlpsvc/configure.sh
+++ pod/nlpsvc/configure.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -eu
 appenv=${1:?'app env?'}
-envf=${HOME}/secret/eks/files/nlpsvc/${appenv}.env
+envf=${HOME}/secret/nlpsvc/${appenv}.env
 
 uwskube delete secret -n nlpsvc nlpsvc-env || true
 uwskube create secret generic -n nlpsvc nlpsvc-env --from-env-file="${envf}"
 
 uwskube delete secret -n nlpsvc etcdir || true
 uwskube create secret generic -n nlpsvc etcdir \
-	--from-file=${HOME}/secret/eks/files/nlpsvc/etc/
+	--from-file=${HOME}/secret/nlpsvc/etc/
 
 exit 0
