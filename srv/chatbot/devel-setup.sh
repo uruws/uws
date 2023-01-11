c63f2e885dcac76cb50966e42ed132bdefa0d2ba
Jeremías <jeremias@talkingpts.org>
2023-01-11 17:38:01 -0300

    chabot: devel setup no need for root

--- srv/chatbot/devel-setup.sh
+++ srv/chatbot/devel-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-echo 'chatbotdev.uws.talkingpts.org' | sudo -n tee -a /uws/acme-certs.list
+echo 'chatbotdev.uws.talkingpts.org' | tee -a /uws/acme-certs.list
 /uws/acme-certs.sh
 
 sudo -n install -v -m 0644 ./srv/chatbot/etc/nginx-chatbot.conf /etc/nginx/sites-enabled/chatbotdev
