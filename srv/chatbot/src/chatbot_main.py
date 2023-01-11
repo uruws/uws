335e4e4b3b374ff6c13d8e0aee8c72c2f091a863
Jeremías <jeremias@talkingpts.org>
2023-01-11 18:24:16 -0300

    chatbot: reply to slack challenge request

--- srv/chatbot/src/chatbot_main.py
+++ srv/chatbot/src/chatbot_main.py
@@ -30,12 +30,14 @@ handler = SlackRequestHandler(bot)
 def slack_events():
 	return handler.handle(bottle.request, bottle.response)
 
-def _setLogLevel():
-	logging.basicConfig(level = logging.DEBUG)
-
-def _listenPort():
-	return int(os.getenv('UWS_WEBAPP_PORT', '2741'))
+@bottle.post('/')
+def index():
+	# FIXME: validate Slack request
+	d = dict(bottle.request.json)
+	c = d.get('challenge', '')
+	return c.strip()
 
 if __name__ == '__main__':
-	_setLogLevel()
-	bottle.run(host = '0.0.0.0', port = _listenPort(), reloader = True)
+	logging.basicConfig(level = logging.DEBUG)
+	listen_port = int(os.getenv('UWS_WEBAPP_PORT', '2741'))
+	bottle.run(host = '0.0.0.0', port = listen_port, reloader = True)
