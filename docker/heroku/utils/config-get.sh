4930527e05ebd14ac9148f54b5da4b223b68a55d
Jeremías <jeremias@talkingpts.org>
2023-01-18 12:28:18 -0300

    heroku: fix config get script

--- docker/heroku/utils/config-get.sh
+++ docker/heroku/utils/config-get.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 APP=${1:?'app name?'}
@@ -10,11 +10,11 @@ heroku config -s -a "${APP}" >"${APP}.env.new"
 grep -vE '^METEOR_SETTINGS=' "${APP}.env.new" |
 	sed 's#^#export #' >"${APP}.env"
 
-grep -vE '^METEOR_SETTINGS=' "${APP}.env.new" "${APP}-settings.env"
+grep -vE '^METEOR_SETTINGS=' "${APP}.env.new" >"${APP}-settings.env"
 
 rm -vf "${APP}.env.new"
 
-. "${APP}-settings.env"
+source "${APP}-settings.env"
 rm -vf "${APP}-settings.env"
 
 echo -ne "${METEOR_SETTINGS}" >"${APP}-settings.json"
