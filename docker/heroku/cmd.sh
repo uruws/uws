3602e784d6af2b0a9653ff87dec9be897d808e0c
Jeremías <jeremias@talkingpts.org>
2023-01-18 12:05:44 -0300

    heroku: docker env file

--- docker/heroku/cmd.sh
+++ docker/heroku/cmd.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -eu
-HEROKU_API_KEY=${HEROKU_API_KEY:-''}
+
+envf=${PWD}/secret/heroku.env
+
 srvdir=/srv/heroku/download
-mkdir -vp ${srvdir}
-exec docker run -it --rm --name uws-heroku-cmd \
-	--hostname heroku-cmd.uws.local -u uws \
-	-e HEROKU_API_KEY=${HEROKU_API_KEY} \
-	-v ${srvdir}:/home/uws/download \
+install -v -d -m 0750 ${srvdir}
+
+exec docker run --rm --name uws-heroku-cmd \
+	--hostname heroku-cmd.uws.local \
+	-u uws \
+	--env-file "${envf}" \
+	-v "${srvdir}:/home/uws/download" \
 	uws/heroku "$@"
--- docker/heroku/devel.sh
+++ docker/heroku/devel.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -eu
-HEROKU_API_KEY=${HEROKU_API_KEY:-''}
+
+envf=${PWD}/secret/heroku.env
+
 userdir=${HOME}/uws/heroku
-mkdir -vp ${userdir}
+install -v -d -m 0750 ${userdir}
+
 exec docker run -it --rm --name uws-heroku-devel \
-	--hostname heroku-devel.uws.local -u uws \
-	-e HEROKU_API_KEY=${HEROKU_API_KEY} \
-	-v ${userdir}:/home/uws/download \
-	uws/heroku "$@"
+	--hostname heroku-devel.uws.local \
+	-u uws \
+	--env-file "${envf}" \
+	-v "${userdir}:/home/uws/download" \
+	uws/heroku
