16ad38913edd91699e7721935ee6300c6d16fee5
Jeremías <jeremias@talkingpts.org>
2023-01-19 18:42:25 -0300

    chatbot: slack connect at startup

--- srv/chatbot/src/chatbot_main.py
+++ srv/chatbot/src/chatbot_main.py
@@ -13,12 +13,19 @@ def healthz():
 		return 'OK'
 	raise RuntimeError('slack chatbot not healthy')
 
+def start():
+	if os.getenv('UWS_WEBAPP_DEBUG', 'off') == 'on':
+		logging.basicConfig(level = logging.DEBUG)
+	logging.debug('slack connect')
+	chatbot_slack.connect()
+
 def getapp():
+	start()
 	return bottle.app
 
 if __name__ == '__main__':
-	if os.getenv('UWS_WEBAPP_DEBUG', 'off') == 'on':
-		logging.basicConfig(level = logging.DEBUG)
+	logging.debug('main')
 	listen_port = int(os.getenv('UWS_WEBAPP_PORT', '2741'))
-	chatbot_slack.connect()
-	bottle.run(host = '0.0.0.0', port = listen_port, reloader = True)
+	start()
+	logging.debug('bottle run')
+	bottle.run(host = '0.0.0.0', port = listen_port, reloader = True, debug = True)
