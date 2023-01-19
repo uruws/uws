68ae726d85e8bf820a79cb643ca620748e7450c7
Jeremías <jeremias@talkingpts.org>
2023-01-19 11:52:49 -0300

    chatbot: slack socket mode

---
+++ srv/chatbot/src/chatbot_slack.py
@@ -0,0 +1,23 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+# https://github.com/slackapi/bolt-python/blob/main/examples/socket_mode.py
+
+import logging
+import os
+
+from slack_bolt                     import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+app = App(
+	token = os.getenv('SLACK_BOT_TOKEN'),
+)
+
+@app.event('app_mention')
+def event_app_mention(event, say):
+	user_id = event['user']
+	say(f"Hello <@{user_id}>!!")
+
+if __name__ == '__main__':
+	logging.basicConfig(level = logging.DEBUG)
+	SocketModeHandler(app, os.getenv('SLACK_APP_TOKEN')).start()
---
+++ srv/chatbot/src/run-slack.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -eu
+exec /opt/uws/venv/bin/python3 /opt/uws/chatbot/chatbot_slack.py
