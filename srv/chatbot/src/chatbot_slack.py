6e0e5fc577277164a108a2fd79fe29360660a920
Jeremías <jeremias@talkingpts.org>
2023-01-19 15:49:21 -0300

    chatbot: new manifest

--- srv/chatbot/src/chatbot_slack.py
+++ srv/chatbot/src/chatbot_slack.py
@@ -13,11 +13,16 @@ app = App(
 	token = os.getenv('SLACK_BOT_TOKEN'),
 )
 
-@app.event('app_mention')
+@app.event("app_mention")
 def event_app_mention(event, say):
 	user_id = event['user']
 	say(f"Hello <@{user_id}>!!")
 
+@app.event("app_home_opened")
+def event_app_home_opened(body, logger):
+	logger.debug(body)
+
 if __name__ == '__main__':
 	logging.basicConfig(level = logging.DEBUG)
+	logging.debug('start')
 	SocketModeHandler(app, os.getenv('SLACK_APP_TOKEN')).start()
