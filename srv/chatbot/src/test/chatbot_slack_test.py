da3d71c2a55ca96a19805c5b985ca7261fa8deb4
Jeremías <jeremias@talkingpts.org>
2023-01-20 12:53:58 -0300

    chatbot: test slack socket mode handler

--- srv/chatbot/src/test/chatbot_slack_test.py
+++ srv/chatbot/src/test/chatbot_slack_test.py
@@ -25,7 +25,29 @@ class MockSlack(object):
 		s.event = None
 		s.say = None
 
-class Test(unittest.TestCase):
+class MockApp(object):
+
+	def __init__(a):
+		a._bup_app = chatbot_slack.app
+		a._bup_smh = chatbot_slack.smh
+		a.smh         = MagicMock()
+		a.smh.client  = MagicMock()
+
+	def _destroy(a):
+		a._bup_app = None
+		a._bup_smh = None
+
+def mock_app_setup() -> MockApp:
+	chatbot_slack.app = MockApp()
+	chatbot_slack.smh = chatbot_slack.app.smh
+	return chatbot_slack.app
+
+def mock_app_teardown(app: MockApp):
+	chatbot_slack.app = app._bup_app
+	chatbot_slack.smh = app._bup_smh
+	app._destroy()
+
+class TestEvents(unittest.TestCase):
 
 	def setUp(t):
 		t.slack = MockSlack()
@@ -43,6 +65,19 @@ class Test(unittest.TestCase):
 
 	def test_event_message(t):
 		chatbot_slack.event_message(t.slack.body)
+
+class TestSocketMode(unittest.TestCase):
+
+	def setUp(t):
+		t.app = mock_app_setup()
+
+	def tearDown(t):
+		mock_app_teardown(t.app)
+		t.app = None
+
+	def test_connect(t):
+		chatbot_slack.connect()
+		t.app.smh.connect.assert_called_once_with()
 
 if __name__ == '__main__':
 	unittest.main()
