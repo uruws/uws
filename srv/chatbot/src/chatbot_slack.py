f020f38021eaadb28e16b0555ed18d9f388129bd
Jeremías <jeremias@talkingpts.org>
2023-01-20 17:59:11 -0300

    chatbot: reploy messages in thread

--- srv/chatbot/src/chatbot_slack.py
+++ srv/chatbot/src/chatbot_slack.py
@@ -21,15 +21,28 @@ app = App(
 def event_app_mention(event, say):
 	logging.debug('app_mention: %s', event)
 	user_id = event['user']
-	say(f"Hello <@{user_id}>!!")
+	text = ' '.join(event.get('text', '').split(' ')[1:])
+	thread_ts = event.get('thread_ts', None) or event['ts']
+	logging.debug('app_mention reply: %s', thread_ts)
+	say(f"<@{user_id}>: {text}", thread_ts = thread_ts)
 
 @app.event('app_home_opened')
 def event_app_home_opened(body):
 	logging.debug('app_home_opened: %s', body)
 
+# https://api.slack.com/events/message
+# https://github.com/slackapi/bolt-python/blob/main/examples/message_events.py
+
 @app.event('message')
-def event_message(body):
+def event_message(body, say):
 	logging.debug('message: %s', body)
+	event = body['event']
+	thread_ts = event.get('thread_ts', None)
+	if thread_ts is not None:
+		logging.debug('message reply: %s', thread_ts)
+		say(text = "Sorry, I'am not that clever.", thread_ts = thread_ts)
+	else:
+		logging.info('message ignored')
 
 #
 # socket mode handler
@@ -58,4 +71,5 @@ channel_id: str = os.getenv('UWS_CHATBOT_CHANNEL_ID', '')
 def msg(text: str):
 	logging.debug('send message: %s', channel_id)
 	res = app.client.chat_postMessage(channel = channel_id, text = text)
-	logging.info(res)
+	if not res.get('ok', False):
+		logging.error('send message failed: %s', res)
