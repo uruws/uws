59b42dc28084f061838be9352f969915f4d31577
Jeremías <jeremias@talkingpts.org>
2023-01-21 01:02:55 -0300

    chatbot: uwscli ssh key file

--- srv/chatbot/src/libexec/uwscli.sh
+++ srv/chatbot/src/libexec/uwscli.sh
@@ -4,4 +4,6 @@ hostname=${1:?'hostname?'}
 username=${2:?'username?'}
 shift
 shift
-exec /usr/bin/ssh -a -C -n -x -o BatchMode=yes -l "${username}" "${hostname}" "$@"
+exec /usr/bin/ssh -a -C -n -x -o BatchMode=yes \
+	-i /etc/opt/uws/chatbot/uwscli_id \
+	-l "${username}" "${hostname}" "$@"
