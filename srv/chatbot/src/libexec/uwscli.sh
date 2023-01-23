bfbd990f19ee57a1af2a9c9f753eff2e239ea8b2
Jeremías <jeremias@talkingpts.org>
2023-01-23 19:20:37 -0300

    chatbot: ssh config

--- srv/chatbot/src/libexec/uwscli.sh
+++ srv/chatbot/src/libexec/uwscli.sh
@@ -4,6 +4,7 @@ hostname=${1:?'hostname?'}
 username=${2:?'username?'}
 shift
 shift
-exec /usr/bin/ssh -a -C -n -x -o BatchMode=yes \
-	-i /etc/opt/uws/chatbot/uwscli_id \
+exec /usr/bin/ssh -a -C -n -x \
+	-F /etc/opt/uws/chatbot/ssh/config \
+	-i /etc/opt/uws/chatbot/ssh/ecdsa_id \
 	-l "${username}" "${hostname}" "$@"
