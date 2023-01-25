b1a903c4fbbbc5fcd502176b17e075a063843691
Jeremías <jeremias@talkingpts.org>
2023-01-25 15:44:46 -0300

    k8s: autoscaler 1.25.0

--- k8s/autoscaler/upstream-get.sh
+++ k8s/autoscaler/upstream-get.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
-VERSION='1.22.2'
+# https://github.com/kubernetes/autoscaler/tags
+VERSION='1.25.0'
 wget -q -O - https://raw.githubusercontent.com/kubernetes/autoscaler/cluster-autoscaler-${VERSION}/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml
 exit 0
