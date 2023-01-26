3fcf68acf4f3aaad8fd259d5a2d0e90b0be2638c
Jeremías <jeremias@talkingpts.org>
2023-01-26 14:26:01 -0300

    k8s: upgrades steps

--- k8s/upgrades.py
+++ k8s/upgrades.py
@@ -7,8 +7,13 @@ import json
 import sys
 
 from dataclasses import dataclass
+from os          import makedirs
 
 __doc__ = 'k8s upgrades helper'
+
+#
+# config
+#
 
 @dataclass
 class Config(object):
@@ -29,8 +34,40 @@ cfg: dict[str, Config] = {
 	),
 }
 
+#
+# utils
+#
+
+def mkdir(name: str):
+	makedirs(name, mode = 0o750, exist_ok = True)
+	print(f"{name}: dir created")
+
+#
+# k8s
+#
+
+def docker_k8s(version: str, cfg: Config):
+	print('docker/k8s:', version)
+
+def k8s_autoscaler(version: str, cfg: Config):
+	print('k8s/autoscaler:', version)
+
+#
+# eks
+#
+
+def docker_eks(version: str, cfg: Config):
+	print('docker/eks:', version)
+
+#
+# main
+#
+
 def main(argv: list[str]) -> int:
-	print(cfg)
+	for v in sorted(cfg.keys()):
+		docker_k8s(v, cfg[v])
+		docker_eks(v, cfg[v])
+		k8s_autoscaler(v, cfg[v])
 	return 0
 
 if __name__ == '__main__':
