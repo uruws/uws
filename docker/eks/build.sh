6fc317d05fc938ca00195f85dd461ead2e5facc0
Jeremías <jeremias@talkingpts.org>
2023-01-30 14:26:52 -0300

    k8s: fix dir names

--- docker/eks/build.sh
+++ docker/eks/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -eu
 # remove old
-# 125
+# 1.25
 docker rmi uws/eks-125-2211 || true
 # build
-# 122
+# 1.22
 # eks-122-2211
 docker build --rm -t uws/eks-122-2211 \
     -f docker/eks/122/Dockerfile.2211 \
     ./docker/eks/122
-# 124
+# 1.24
 # eks-124-2211
 docker build --rm -t uws/eks-124-2211 \
     -f docker/eks/124/Dockerfile.2211 \
