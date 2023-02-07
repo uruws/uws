b74041bb345b48423936c8181d14d0346eb405d0
Jeremías <jeremias@talkingpts.org>
2023-02-07 17:13:07 -0300

    k8s: mount sites-enabled dir

--- k8s/nginx/configure.sh
+++ k8s/nginx/configure.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -eu
+
 # nginx.env
 uwskube delete secret proxy-env -n nginx || true
-uwskube create secret generic proxy-env -n nginx --from-env-file=${HOME}/cluster/nginx.env
+uwskube create secret generic proxy-env -n nginx \
+	--from-env-file=${HOME}/cluster/nginx.env
+
+# sites-enabled
+uwskube delete secret sites-enabled -n nginx || true
+uwskube create secret generic sites-enabled -n nginx \
+	--from-file=${HOME}/cluster/nginx/sites-enabled
+
 exit 0
