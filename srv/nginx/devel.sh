18c88dd4e2c70736ddce9f095c282d46c2f4369e
Jeremías <jeremias@talkingpts.org>
2023-02-08 14:42:37 -0300

    k8s: nginx install ssl-certs

--- srv/nginx/devel.sh
+++ srv/nginx/devel.sh
@@ -10,6 +10,8 @@ exec docker run -it --rm --name uws-nginx-devel \
 	--tmpfs /var/log/nginx \
 	-v ${PWD}/srv/nginx/utils:/usr/local/bin:ro \
 	-v ${PWD}/srv/nginx/etc/conf.d:/etc/nginx/conf.d:ro \
+	-v ${PWD}/srv/nginx/etc/snippets:/etc/nginx/snippets:ro \
+	-v ${PWD}/srv/nginx/etc/sites-enabled:/etc/nginx/sites-enabled:ro \
 	-p 127.0.0.1:0:80 \
 	-p 127.0.0.1:0:443 \
 	uws/nginx-2211
