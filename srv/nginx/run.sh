e43d04f3e07640ab36d79aa8820ff960ed06a89e
Jeremías <jeremias@talkingpts.org>
2023-02-08 17:48:03 -0300

    k8s: nginx include uws custom sites

---
+++ srv/nginx/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+exec docker run -it --rm --name uws-nginx \
+	--hostname nginx.uws.local \
+	--read-only \
+	--tmpfs /run \
+	--tmpfs /tmp \
+	--tmpfs /var/lib/nginx \
+	--tmpfs /var/log/nginx \
+	-p 127.0.0.1:0:80 \
+	-p 127.0.0.1:0:443 \
+	uws/nginx-2211
