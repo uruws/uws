e115fa34510411e85fee7c36ef040713681b0991
Jeremías <jeremias@talkingpts.org>
2023-02-09 14:45:17 -0300

    k8s: nginx deploy resources

--- k8s/nginx/deploy.sh
+++ k8s/nginx/deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -eu
 
-NGINX_VERSION=$(cat ~/k8s/nginx/VERSION)
-export NGINX_VERSION
+PROXY_VERSION=$(cat ~/k8s/nginx/VERSION)
+export PROXY_VERSION
+
+PROXY_REPLICAS=${NGINX_REPLICAS:-3}
+export PROXY_REPLICAS
+PROXY_CPU=${NGINX_CPU:-400}
+export PROXY_CPU
+PROXY_MEM=${NGINX_MEM:-1500}
+export PROXY_MEM
 
 ~/k8s/nginx/configure.sh
 
