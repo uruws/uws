8bfe64497ccfd250c7fa205aaad1c40c24b64ef2
Jeremías <jeremias@talkingpts.org>
2023-02-09 15:46:59 -0300

    k8s: nginx publish to all regions

--- srv/nginx/publish.sh
+++ srv/nginx/publish.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -eu
-#~ REGIONS='sa-east-1 us-east-1 us-east-2 us-west-1 us-west2'
-REGIONS='sa-east-1'
+REGIONS='sa-east-1 us-east-1 us-east-2 us-west-1 us-west2'
 NGINX_VERSION=$(cat ./k8s/nginx/VERSION)
 for region in ${REGIONS}; do
 	./docker/ecr-login.sh "${region}"
