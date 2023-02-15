c317cb997546657b1a685370ba2ee0c112d9798d
Jeremías <jeremias@talkingpts.org>
2023-02-15 15:53:39 -0300

    eks: efs fix helm install

--- k8s/efs/install.sh
+++ k8s/efs/install.sh
@@ -3,7 +3,7 @@ set -eu
 
 # https://docs.aws.amazon.com/eks/latest/userguide/efs-csi.html
 
-helm upgrade -i aws-efs-csi-driver aws-efs-csi-driver/aws-efs-csi-driver \
+helm upgrade -i aws-efs-csi-driver aws-efs-csi-driver \
 	--repo https://kubernetes-sigs.github.io/aws-efs-csi-driver/ \
 	--namespace kube-system \
 	--set "image.repository=602401143452.dkr.ecr.${AWS_REGION}.amazonaws.com/eks/aws-efs-csi-driver" \
