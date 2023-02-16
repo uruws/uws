b0bf1cb85bcb823d09a7658a01bd0a0f5c3192ba
Jeremías <jeremias@talkingpts.org>
2023-02-16 17:07:37 -0300

    efs: rmfs umount it first

--- k8s/efs/rmfs.sh
+++ k8s/efs/rmfs.sh
@@ -10,6 +10,9 @@ UWSEFS_NAME="${fsns}-${fsname}"
 
 fsid=$(./k8s/efs/getcfg.sh "${UWSEFS_NAME}")
 
+~/k8s/efs/umount.sh "${fsns}" "${fsname}"
+sleep 3
+
 aws efs delete-file-system \
 	--output text \
 	--region "${AWS_REGION}" \
