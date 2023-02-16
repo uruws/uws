13dfa9a448248fc6d75b1846cf96afb2b7c2a916
Jeremías <jeremias@talkingpts.org>
2023-02-16 12:18:08 -0300

    eks: efs rm fs

---
+++ k8s/efs/rmfs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+# https://docs.aws.amazon.com/eks/latest/userguide/efs-csi.html
+
+fsname=${1:?'fs name?'}
+
+fs_id=$(./k8s/efs/getcfg.sh "fs-${fsname}")
+
+aws efs delete-file-system \
+	--output text \
+	--region "${AWS_REGION}" \
+	--file-system-id "${fs_id}"
+
+echo "${fsname} deleted: ${fsid}"
+~/k8s/efs/delcfg.sh "fs-${fsname}"
+
+exit 0
