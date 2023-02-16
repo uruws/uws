67895267e4ac793f17000f62b19000aed8e900b7
Jeremías <jeremias@talkingpts.org>
2023-02-16 12:27:51 -0300

    eks: efs show fs number of mount targets

--- k8s/efs/fs-status.sh
+++ k8s/efs/fs-status.sh
@@ -3,4 +3,4 @@ set -eu
 exec aws efs describe-file-systems \
 	--output table \
 	--region "${AWS_REGION}" \
-	--query 'FileSystems[*].{FileSystemId: FileSystemId, Name: Tags[0].Value, SizeInBytes: SizeInBytes.Value, Status: LifeCycleState}'
+	--query 'FileSystems[*].{FileSystemId: FileSystemId, Name: Tags[0].Value, SizeInBytes: SizeInBytes.Value, Status: LifeCycleState, MountTargets: NumberOfMountTargets}'
