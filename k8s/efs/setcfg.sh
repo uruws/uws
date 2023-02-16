06f18c30899f5a5dcc36cc1ab3f536b09c1008e7
Jeremías <jeremias@talkingpts.org>
2023-02-16 10:24:08 -0300

    eks: efs set cfg do not override

--- k8s/efs/setcfg.sh
+++ k8s/efs/setcfg.sh
@@ -2,5 +2,4 @@
 set -eu
 name=${1:?'key name?'}
 val=${2:?'value?'}
-uwskube delete secret "uwsefs-cfg-${name}" || true
 exec uwskube create secret generic "uwsefs-cfg-${name}" --from-literal="value=${val}"
