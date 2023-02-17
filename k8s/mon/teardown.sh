9566e912a21f2d7619b592ef5cca67b8501367fc
Jeremías <jeremias@talkingpts.org>
2023-02-17 12:38:28 -0300

    munin: try to improve rollin and teardown

--- k8s/mon/teardown.sh
+++ k8s/mon/teardown.sh
@@ -11,7 +11,10 @@ read -r -p 'are you sure? [N/y]: ' CONFIRM
 
 if test 'Xy' = "X${CONFIRM}"; then
 	echo "delete mon namespace and all its resources..."
-	~/k8s/mon/rollin.sh
+	~/k8s/mon/munin/rollin.sh
+	~/k8s/mon/umount.sh
+	~/k8s/mon/rollin.sh --no-munin
+	~/k8s/mon/kubeshark/teardown.sh
 	~/k8s/mon/rmfs.sh
 	exec uwskube delete namespace mon
 else
