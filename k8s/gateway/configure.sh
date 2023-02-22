7b85911673c4ab55edc3869b850007f6ca359a91
Jeremías <jeremias@talkingpts.org>
2023-02-22 12:59:49 -0300

    k8s: gateway less verbose configure

--- k8s/gateway/configure.sh
+++ k8s/gateway/configure.sh
@@ -4,8 +4,8 @@ gateway_configure() (
 	cfgdir="${1}"
 
 	install -v -d -m 0750 "${cfgdir}"
-	install -v -d -m 0750 "${cfgdir}/nginx"
-	install -v -d -m 0750 "${cfgdir}/nginx/sites-enabled"
+	install -d -m 0750 "${cfgdir}/nginx"
+	install -d -m 0750 "${cfgdir}/nginx/sites-enabled"
 
 	envsubst <~/k8s/gateway/cluster.conf >"${cfgdir}/nginx/sites-enabled/cluster-gateway"
 )
