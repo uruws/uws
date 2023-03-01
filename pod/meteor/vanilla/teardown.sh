7b0961c4a4e8063d0b45546904b522f45c027108
Jeremías <jeremias@talkingpts.org>
2023-03-01 18:00:07 -0300

    meteor: vanilla fix shellcheck

--- pod/meteor/vanilla/teardown.sh
+++ pod/meteor/vanilla/teardown.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -u
 uwskube delete secret -n meteor-vanilla appenv || true
-exec uwskube delete service meteor -n meteor-vanilla
+uwskube delete service meteor -n meteor-vanilla
 exec uwskube delete namespace meteor-vanilla
