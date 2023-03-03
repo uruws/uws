75b89c5b6127a7c61d8e6370ed3c771bf55de736
Jeremías <jeremias@talkingpts.org>
2023-03-03 16:26:58 -0300

    k8s: remove meteor-vanilla setup from default gateway

--- k8s/gateway/rollin.sh
+++ k8s/gateway/rollin.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 set -eu
-
-# meteor-vanilla
-if test "X${METEOR_VANILLA_HOST:-NONE}" != 'XNONE'; then
-	uwskube delete -f "${HOME}/pod/meteor/vanilla/gateway-service.yaml"
-fi
-
 exec ~/k8s/nginx/rollin.sh default k8s/gateway
