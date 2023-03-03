57f2e3b45cb242ca75d9e341199c15f013ea78d7
Jeremías <jeremias@talkingpts.org>
2023-03-03 18:40:05 -0300

    meteor: improve pod check

--- pod/meteor/check.sh
+++ pod/meteor/check.sh
@@ -1,18 +1,63 @@
 #!/bin/sh
-set -eux
+set -eu
+
+ignore_file() (
+	fn=${1}
+	case "${fn}" in
+		./pod/meteor/api/*)
+			return 0
+		;;
+		./pod/meteor/gw/*)
+			return 0
+		;;
+		./pod/meteor/web/*)
+			return 0
+		;;
+		./pod/meteor/worker/*)
+			return 0
+		;;
+	esac
+	return 1
+)
 
 for fn in ./pod/meteor/*/deploy.sh; do
+	if ignore_file "${fn}"; then
+		continue
+	fi
+	echo "grep -qF gw/deploy.sh ${fn}"
 	grep -qF gw/deploy.sh "${fn}"
 done
 
 for fn in ./pod/meteor/*/restart.sh; do
+	if ignore_file "${fn}"; then
+		continue
+	fi
+	echo "grep -qF gw/restart.sh ${fn}"
 	grep -qF gw/restart.sh "${fn}"
 done
 
 for fn in ./pod/meteor/*/rollin.sh; do
+	if ignore_file "${fn}"; then
+		continue
+	fi
+	echo "grep -qF gw/rollin.sh ${fn}"
 	grep -qF gw/rollin.sh "${fn}"
 done
 
 for fn in ./pod/meteor/*/scale.sh; do
+	if ignore_file "${fn}"; then
+		continue
+	fi
+	echo "grep -qF gw/scale.sh ${fn}"
 	grep -qF gw/scale.sh "${fn}"
 done
+
+for fn in ./pod/meteor/*/teardown.sh; do
+	if ignore_file "${fn}"; then
+		continue
+	fi
+	echo "grep -qF gw/teardown.sh ${fn}"
+	grep -qF gw/teardown.sh "${fn}"
+done
+
+exit 0
