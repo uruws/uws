16d405d1e74d166b7147a2b7f289281217cfc643
Jeremías <jeremias@talkingpts.org>
2023-03-06 18:56:09 -0300

    meteor: web custom nginx conf

--- pod/meteor/web/gw/deploy.sh
+++ pod/meteor/web/gw/deploy.sh
@@ -6,4 +6,6 @@ export METEOR_TLS=${APP_TLS}
 export METEOR_HOST=${APP_HOSTNAME}
 NGINX_REPLICAS=$(expr ${APP_REPLICAS} + ${METEOR_API_REPLICAS})
 export NGINX_REPLICAS
+METEOR_NGINX_CONF=${HOME}/pod/meteor/web/nginx.conf
+export METEOR_NGINX_CONF
 exec ~/pod/meteor/gw/deploy.sh "${ns}"
