d22a7fea089996327aef15a63837829e31eb9edc
Jeremías <jeremias@talkingpts.org>
2023-03-06 12:51:48 -0300

    meteor: fix web gw deploy shellcheck

--- pod/meteor/web/gw/deploy.sh
+++ pod/meteor/web/gw/deploy.sh
@@ -4,5 +4,6 @@ ns=web
 export METEOR_NAMESPACE=${ns}
 export METEOR_TLS=${APP_TLS}
 export METEOR_HOST=${APP_HOSTNAME}
-export NGINX_REPLICAS=$(expr ${APP_REPLICAS} + ${METEOR_API_REPLICAS})
+NGINX_REPLICAS=$(expr ${APP_REPLICAS} + ${METEOR_API_REPLICAS})
+export NGINX_REPLICAS
 exec ~/pod/meteor/gw/deploy.sh "${ns}"
