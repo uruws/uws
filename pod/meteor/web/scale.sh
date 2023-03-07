4126860ee1f56ed4808b7e7b45d6681b281ef2e4
Jeremias <jeremias@talkingpts.org>
2023-03-07 18:48:23 +0000

    meteor: fix web and api scale

--- pod/meteor/web/scale.sh
+++ pod/meteor/web/scale.sh
@@ -2,7 +2,7 @@
 set -eu
 replicas=${1:?'replicas?'}
 api_replicas=$(uwskube get deployment/meteor -n api --output jsonpath='{.spec.replicas}')
-gw_replicas=$(expr ${replicas} ${api_replicas})
+gw_replicas=$(expr ${replicas} + ${api_replicas})
 ~/pod/lib/scale.sh web meteor "${replicas}"
 ~/pod/meteor/web/gw/scale.sh "${gw_replicas}"
 exit 0
