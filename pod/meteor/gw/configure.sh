08f9c3677649d1fcb6ba177f34ca0f0582db42b5
Jeremías <jeremias@talkingpts.org>
2023-03-09 16:33:59 -0300

    meteor: fix web nginx conf

--- pod/meteor/gw/configure.sh
+++ pod/meteor/gw/configure.sh
@@ -4,12 +4,16 @@ gateway_configure() (
 	ns="${1}"
 	cfgdir="${2}"
 
-	nginx_conf=${METEOR_NGINX_CONF:-${HOME}/pod/meteor/gw/nginx.conf}
+	nginx_conf=${HOME}/pod/meteor/gw/nginx.conf
+	if test -s "${cfgdir}/nginx.conf"; then
+		nginx_conf="${cfgdir}/nginx.conf"
+	fi
 
 	install -d -m 0750 "${cfgdir}"
 	install -d -m 0750 "${cfgdir}/nginx"
 	install -v -d -m 0750 "${cfgdir}/nginx/sites-enabled"
 
+	echo "nginx conf: ${nginx_conf}"
 	envsubst <"${nginx_conf}" >"${cfgdir}/nginx/sites-enabled/meteor-${ns}"
 
 	install -v -d -m 0750 "${cfgdir}/nginx/service"
