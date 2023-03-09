2a86a942abdae6a405a5d9d06812ef207e20d72f
Jeremías <jeremias@talkingpts.org>
2023-03-09 14:19:03 -0300

    meteor: api do not tune web gw

--- pod/meteor/api/scale.sh
+++ pod/meteor/api/scale.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -eu
 replicas=${1:?'replicas?'}
-web_replicas=$(uwskube get deployment/meteor -n web --output jsonpath='{.spec.replicas}')
-gw_replicas=$(expr ${replicas} + ${web_replicas})
 ~/pod/lib/scale.sh api meteor "${replicas}"
-~/pod/meteor/web/gw/scale.sh "${gw_replicas}"
 exit 0
