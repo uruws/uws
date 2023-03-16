0015ee995b1fda087855f2fdd2e5ecafd35ca721
Jeremías <jeremias@talkingpts.org>
2023-03-16 16:20:04 -0300

    meteor: worker TLS config

--- pod/meteor/worker/gw/deploy.sh
+++ pod/meteor/worker/gw/deploy.sh
@@ -2,7 +2,7 @@
 set -eu
 ns=worker
 export METEOR_NAMESPACE=${ns}
-export METEOR_TLS=uwsgd
+export METEOR_TLS=${METEOR_WORKER_TLS}
 export METEOR_HOST=${METEOR_WORKER_HOST}
 export NGINX_REPLICAS=${METEOR_WORKER_REPLICAS}
 exec ~/pod/meteor/gw/deploy.sh "${ns}"
