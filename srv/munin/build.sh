4d7d8258470787bb6398666ef36c040b39a8259c
Jeremías <jeremias@talkingpts.org>
2023-03-20 15:56:57 -0300

    munin: docker remove version 2203

--- srv/munin/build.sh
+++ srv/munin/build.sh
@@ -2,10 +2,7 @@
 set -eu
 # remove old versions
 docker rmi uws/munin-2109 || true
-# uws/munin-2203
-docker build --rm -t uws/munin-2203 \
-	-f srv/munin/Dockerfile.2203 \
-	./srv/munin
+docker rmi uws/munin-2203 || true
 # uws/munin-2211
 docker build --rm -t uws/munin-2211 \
 	-f srv/munin/Dockerfile.2211 \
