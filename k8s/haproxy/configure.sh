37df32b1332d62b42a2382a786860d5200708df2
Jeremías <jeremias@talkingpts.org>
2023-04-14 13:29:31 -0300

    k8s: haproxy autoscaling true as default

--- k8s/haproxy/configure.sh
+++ k8s/haproxy/configure.sh
@@ -9,15 +9,15 @@ haproxy_configure() (
 	export HPX_NAMESPACE=default
 	export HPX_DEFAULT_BACKEND=default-backend
 	export HPX_ENABLE_DEFAULT_BACKEND=false
-	export HPX_ENABLE_AUTOSCALING=false
+	export HPX_ENABLE_AUTOSCALING=true
 	export HPX_REPLICAS=3
 
 	# shellcheck disable=SC1090
 	. "${envfn}"
 
-	envsubst '${HPX_NAMESPACE}' <"${srcfn}" |
-		envsubst '${HPX_DEFAULT_BACKEND}' |
+	envsubst '${HPX_NAMESPACE}' <"${srcfn}"      |
+		envsubst '${HPX_DEFAULT_BACKEND}'        |
 		envsubst '${HPX_ENABLE_DEFAULT_BACKEND}' |
-		envsubst '${HPX_ENABLE_AUTOSCALING}' |
+		envsubst '${HPX_ENABLE_AUTOSCALING}'     |
 		envsubst '${HPX_REPLICAS}' >"${dstfn}"
 )
