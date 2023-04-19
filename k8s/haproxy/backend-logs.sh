698e9d0a17bb36bc256b8214a447bbc79072f693
Jeremías <jeremias@talkingpts.org>
2023-04-19 17:18:16 -0300

    haproxy: fix default backend logs

--- k8s/haproxy/backend-logs.sh
+++ k8s/haproxy/backend-logs.sh
@@ -3,5 +3,5 @@ set -eu
 ns=${1:?'haproxy namespace?'}
 shift
 exec ~/pod/lib/logs.py -n "${ns}" \
-	-l 'app.kubernetes.io/name=haproxy-ingress-haproxy-backend' \
+	-l 'app.kubernetes.io/name=haproxy-ingress-default-backend' \
 	--max 200 "$@"
