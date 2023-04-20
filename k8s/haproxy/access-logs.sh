fa206a1550320b5971acc54e100b367dbb9e01e8
Jeremías <jeremias@talkingpts.org>
2023-04-20 16:18:56 -0300

    k8s: haproxy logs no timestamp

--- k8s/haproxy/access-logs.sh
+++ k8s/haproxy/access-logs.sh
@@ -2,6 +2,6 @@
 set -eu
 ns="${1:?'haproxy namespace?'}hpx"
 shift
-exec ~/pod/lib/logs.py -n "${ns}" \
+exec ~/pod/lib/logs.py -n "${ns}" --no-timestamps \
 	-l 'app.kubernetes.io/name=haproxy-ingress' \
 	--max 200 -c access-logs "$@"
--- k8s/haproxy/backend-logs.sh
+++ k8s/haproxy/backend-logs.sh
@@ -2,6 +2,6 @@
 set -eu
 ns="${1:?'haproxy namespace?'}hpx"
 shift
-exec ~/pod/lib/logs.py -n "${ns}" \
+exec ~/pod/lib/logs.py -n "${ns}" --no-timestamps \
 	-l 'app.kubernetes.io/name=haproxy-ingress-default-backend' \
 	--max 200 "$@"
