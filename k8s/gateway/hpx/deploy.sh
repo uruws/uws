b3a584d85acf43951a2526dc69cd2e9ba91a31e5
Jeremías <jeremias@talkingpts.org>
2023-04-21 19:22:51 -0300

    k8s: fix default gateway ingress deploy/rollin

--- k8s/gateway/hpx/deploy.sh
+++ k8s/gateway/hpx/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 ~/pod/test/deploy.sh
-exec ~/k8s/haproxy/deploy.sh \
+exec ~/k8s/haproxy/ingress/deploy.sh \
 	${HOME}/k8s/gateway/hpx/haproxy.env \
 	${HOME}/k8s/gateway/hpx/ingress.yaml
--- k8s/gateway/hpx/rollin.sh
+++ k8s/gateway/hpx/rollin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -u
 ~/pod/test/rollin.sh
-exec ~/k8s/haproxy/rollin.sh \
+exec ~/k8s/haproxy/ingress/rollin.sh \
 	${HOME}/k8s/gateway/hpx/haproxy.env \
 	${HOME}/k8s/gateway/hpx/ingress.yaml
