e83bf5064a3bfcc732bc31e3494112f322d2fa11
Jeremías <jeremias@talkingpts.org>
2023-04-24 19:27:59 -0300

    k8s: haproxy status format

--- k8s/haproxy/status.sh
+++ k8s/haproxy/status.sh
@@ -6,4 +6,5 @@ envfn="${HOME}/${prof}/haproxy.env"
 # shellcheck disable=SC1090
 . "${envfn}"
 uwskube get all -n "${HPX_NAMESPACE}" -l 'app.kubernetes.io/name=haproxy-ingress'
+echo ''
 exec uwskube get ingress -n "${HPX_NAMESPACE}" -l "uws.t.o/ingress-class=${HPX_NAMESPACE}hpx"
