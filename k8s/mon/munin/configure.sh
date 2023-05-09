7c3cc75b86d6dffe52fb18212663a1a579fc563f
Jeremías <jeremias@talkingpts.org>
2023-05-09 18:29:53 -0300

    k8smon: munin deploy no smtps setup

--- k8s/mon/munin/configure.sh
+++ k8s/mon/munin/configure.sh
@@ -2,16 +2,10 @@
 set -eu
 
 # smtps CA
-CA=${HOME}/ca/uws/smtps/230503
 uwskube delete secret smtps-ca -n mon || true
-uwskube create secret generic smtps-ca -n mon \
-	--from-file="${CA}"
 
 # smtps CA client
-CACLI=${HOME}/ca/uws/smtps/230503/client
 uwskube delete secret smtps-ca-client -n mon || true
-uwskube create secret generic smtps-ca-client -n mon \
-	--from-file="${CACLI}"
 
 # cluster.conf
 conf=/tmp/mon.munin.cluster.conf
--- srv/munin/devel.sh
+++ srv/munin/devel.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 set -eu
-CA=${PWD}/secret/ca/uws/smtps/230503
 mkdir -vp ${PWD}/tmp
 exec docker run -it --rm --name uws-munin-devel \
 	--hostname munin-devel.uws.local \
 	--read-only \
 	-u uws --entrypoint /usr/local/bin/uws-login.sh \
 	-e USER=uws -e HOME=/home/uws \
-	-e UWS_SMTPS_CERT='/etc/opt/uws/ca/client/08082dca-8d77-5c81-9a44-94642089b3b1.pem' \
-	-e UWS_SMTPS_KEY='/etc/opt/uws/ca/client/08082dca-8d77-5c81-9a44-94642089b3b1.key' \
 	-v ${PWD}/srv/munin/utils:/opt/munin \
 	-v ${PWD}/tmp:/home/uws/tmp \
 	-v ${PWD}/python:/opt/uws \
-	-v ${CA}:/etc/opt/uws/ca:ro \
 	--tmpfs /var/opt/munin-alert \
 	--workdir /opt/munin \
 	$@ uws/munin-2211
