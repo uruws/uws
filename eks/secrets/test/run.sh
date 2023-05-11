0c38b708b281aa07fee0cb116238a42b99327971
Jeremías <jeremias@talkingpts.org>
2023-05-11 16:02:50 -0300

    secret: check json files format

--- eks/secrets/test/run.sh
+++ eks/secrets/test/run.sh
@@ -7,4 +7,7 @@ echo '** ./eks/secrets/test/shellcheck.sh'
 echo '** ./eks/secrets/test/shellcheck-env-files.sh'
 ./eks/secrets/test/shellcheck-env-files.sh
 
+echo '** ./eks/secrets/test/json-files.sh'
+./eks/secrets/test/json-files.sh
+
 exit 0
