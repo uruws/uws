0ac5b446d4d4246f6e4d0a090a7648b24fc19da8
Jeremías <jeremias@talkingpts.org>
2023-05-11 15:55:53 -0300

    secret: shellcheck env files

--- eks/secrets/test/run.sh
+++ eks/secrets/test/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -eu
+
 echo '** ./eks/secrets/test/shellcheck.sh'
 ./eks/secrets/test/shellcheck.sh
+
+echo '** ./eks/secrets/test/shellcheck-env-files.sh'
+./eks/secrets/test/shellcheck-env-files.sh
+
 exit 0
---
+++ eks/secrets/test/shellcheck-env-files.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+
+# SC2034: UWS_WEBAPP_DEBUG appears unused. Verify use (or export if used externally).
+# SC2016: Expressions don't expand in single quotes, use double quotes for that.
+
+find ./secret -type f -name '*.env' -print0 |
+	xargs --null -- shellcheck --color=auto --shell=sh --exclude=SC2034,SC2016
+
+exit 0
