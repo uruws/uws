6e9053015a6c98ab9c145f6ab3a08cbbf156f1e7
Jeremías <jeremias@talkingpts.org>
2023-05-11 15:50:33 -0300

    secret: fix all shellcheck

--- eks/secrets/test/shellcheck.sh
+++ eks/secrets/test/shellcheck.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -eu
-shellcheck ./eks/secrets/test/*.sh
-shellcheck --exclude=SC1090 ./eks/secrets/*.sh
+
+shellcheck --color=auto ./eks/secrets/test/*.sh
+
+shellcheck --color=auto --exclude=SC1090 ./eks/secrets/*.sh
+
+find ./secret -type f -name '*.sh' -print0 |
+	xargs --null -- shellcheck --color=auto
+
 exit 0
