5f3eced26a3307b84e8cca4b941799a9dc394e90
Jeremías <jeremias@talkingpts.org>
2023-05-24 18:11:22 -0300

    meteor: App worker fix proxy setup

--- pod/meteor/worker/gw/deploy.sh
+++ pod/meteor/worker/gw/deploy.sh
@@ -4,5 +4,5 @@ ns=worker
 export METEOR_NAMESPACE=${ns}
 export METEOR_TLS=${METEOR_WORKER_TLS}
 export METEOR_HOST=${METEOR_WORKER_HOST}
-export NGINX_REPLICAS=${METEOR_WORKER_REPLICAS}
+export NGINX_REPLICAS=${METEOR_WORKER_NGINX_REPLICAS}
 exec ~/pod/meteor/gw/deploy.sh "${ns}"
