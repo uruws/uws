2f21f5eb83c0f4a104475e3ae9a59c4939b97273
Jeremías <jeremias@talkingpts.org>
2023-05-29 19:34:29 -0300

    k8smon: deploy timestamp

--- k8s/mon/munin-node/deploy.sh
+++ k8s/mon/munin-node/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -eu
 ~/k8s/mon/munin-node/configure.sh
+
 VERSION="$(cat ~/k8s/mon/munin/VERSION)"
 export VERSION
+
+UWS_DEPLOY=$(date '+%y%m%d.%H%M%S')
+export UWS_DEPLOY
+
 envsubst <~/k8s/mon/munin-node/deploy.yaml | uwskube apply -f -
 exit 0
--- k8s/mon/munin/deploy.sh
+++ k8s/mon/munin/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -eu
 ~/k8s/mon/munin/configure.sh
+
 VERSION="$(cat ~/k8s/mon/munin/VERSION)"
 export VERSION
+
+UWS_DEPLOY=$(date '+%y%m%d.%H%M%S')
+export UWS_DEPLOY
+
 envsubst <~/k8s/mon/munin/deploy.yaml | uwskube apply -f -
 exit 0
