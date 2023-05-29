80c7a771330f1c287b4a47d657779682773972e7
Jeremías <jeremias@talkingpts.org>
2023-05-29 20:12:02 -0300

    k8smon: munin server logs

---
+++ k8s/mon/munin/server-logs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -eu
+exec uwskube exec pod/munin-0 -i -t -c munin -n mon -- tail "${@}" /var/log/munin/munin-graph.log /var/log/munin/munin-html.log /var/log/munin/munin-limits.log /var/log/munin/munin-update.log
