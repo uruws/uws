0ae7a27ee3a3e9c14e5c40b21a2361d86373290d
Jeremías <jeremias@talkingpts.org>
2023-06-09 20:18:22 -0300

    k8smon: mnppl self config and report (broken)

--- k8s/mon/munin-node/plugins/mnppl.py
+++ k8s/mon/munin-node/plugins/mnppl.py
@@ -11,8 +11,43 @@ from pathlib    import Path
 from subprocess import PIPE
 from subprocess import Popen
 
+import mon
+
 plugins_bindir = '/usr/local/bin'
 plugins_suffix = '.mnppl'
+
+#
+# configs and reports
+#
+
+def _print(*args):
+	print(*args)
+
+def _config(sts):
+	mon.dbg('mnppl config')
+	cluster = mon.cluster()
+	_print('multigraph mnppl')
+	_print(f"graph_title {cluster} mnppl")
+	_print('graph_args --base 1000 -l 0')
+	_print('graph_category deploy')
+	_print('graph_vlabel number')
+	_print('graph_printf %3.0lf')
+	_print('graph_scale yes')
+	# FIXME: loop sts keys
+	_print('a_total.label total')
+	_print('a_total.colour COLOUR0')
+	_print('a_total.draw AREA')
+	_print('a_total.min 0')
+
+def _report(sts):
+	mon.dbg('mnppl report')
+	_print('multigraph mnppl')
+	# FIXME: loop sts keys
+	_print('a_total.value', sts.get('total', 'U'))
+
+#
+# run parallel
+#
 
 def _listPlugins(bindir: str) -> list[str]:
 	l = []
@@ -22,16 +57,12 @@ def _listPlugins(bindir: str) -> list[str]:
 			l.append(fn)
 	return sorted(l)
 
-#
-# run parallel
-#
-
 def _newProc(cmd: list[str]) -> Popen:
 	return Popen(cmd, text = True, stdout = PIPE, stderr = PIPE)
 
 def _pprint(p: Popen):
 	if p.returncode != 0:
-		print('[ERROR]', ' '.join(p.args), 'failed:', p.returncode, file = sys.stderr)
+		print('[ERROR]', p.args, 'failed:', p.returncode, file = sys.stderr)
 	if p.stderr is not None:
 		sys.stderr.write(p.stderr.read())
 		sys.stderr.flush()
@@ -40,18 +71,24 @@ def _pprint(p: Popen):
 		sys.stdout.flush()
 
 def _run(bindir: str, action: str) -> int:
+	sts: dict[str, str] = {}
 	pwait: list[Popen] = []
 	for pl in _listPlugins(bindir):
 		cmd = [Path(bindir, pl).as_posix()]
 		if action == 'config':
 			cmd.append(action)
 		pwait.append(_newProc(cmd))
+		sts[pl] = ''
 	rc = 0
 	for proc in pwait:
 		st = proc.wait()
 		if st != 0:
 			rc = st
 		_pprint(proc)
+	if action == 'config':
+		_config(sts)
+	else:
+		_report(sts)
 	return rc
 
 #
@@ -77,16 +114,14 @@ def main(argv: list[str]) -> int:
 	if isinstance(args.action, list):
 		action = args.action[0].strip()
 
-	bindir = Path(args.bindir)
-
 	rc = 0
 	if args.serial:
-		for pl in _listPlugins(bindir):
-			st = os.system(Path(bindir, pl))
+		for pl in _listPlugins(args.bindir):
+			st = os.system(Path(args.bindir, pl))
 			if st != 0:
 				rc = st
 	else:
-		return _run(bindir, action)
+		return _run(args.bindir, action)
 	return rc
 
 if __name__ == '__main__': # pragma no cover
