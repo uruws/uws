a58af8c7836996c0aae2b334e12c53c400715db0
Jeremías <jeremias@talkingpts.org>
2023-06-09 19:17:40 -0300

    k8smon: mnppl list plugins to run

--- k8s/mon/munin-node/plugins/mnppl.py
+++ k8s/mon/munin-node/plugins/mnppl.py
@@ -3,9 +3,38 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
+import os
 import sys
 
+from argparse import ArgumentParser
+from pathlib  import Path
+
+plugins_bindir = '/usr/local/bin'
+plugins_suffix = '.mnppl'
+
+def _listPlugins(bindir: str = '') -> list[str]:
+	l = []
+	for fn in os.listdir(bindir):
+		fn = fn.strip()
+		if fn.endswith(plugins_suffix):
+			l.append(fn)
+	return sorted(l)
+
+__doc__ = 'munin-node parallel plugins runner'
+
 def main(argv: list[str]):
+
+	flags = ArgumentParser(description = __doc__)
+
+	flags.add_argument('-b', '--bindir', default = plugins_bindir, help = 'plugins bindir')
+
+	args = flags.parse_args(argv)
+
+	bindir = Path(args.bindir)
+
+	for pl in _listPlugins(bindir):
+		print(pl)
+
 	return 0
 
 if __name__ == '__main__': # pragma no cover
--- k8s/test/mon/munin-node/plugins/mnppl_test.py
+++ k8s/test/mon/munin-node/plugins/mnppl_test.py
@@ -9,6 +9,19 @@ import mnppl
 
 class Test(unittest.TestCase):
 
+	def test_globals(t):
+		t.assertEqual(mnppl.plugins_bindir, '/usr/local/bin')
+		t.assertEqual(mnppl.plugins_suffix, '.mnppl')
+
+	def test_listPlugins(t):
+		t.assertListEqual(mnppl._listPlugins('./k8s/test/mon/munin-node/plugins/mnppl'), [
+			't0.mnppl',
+			't1.mnppl',
+			't2.mnppl',
+			't3.mnppl',
+			't4.mnppl',
+		])
+
 	def test_main(t):
 		t.assertEqual(mnppl.main([]), 0)
 
