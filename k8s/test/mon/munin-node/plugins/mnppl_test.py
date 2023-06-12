43c76c6108501e63f2645b38f72f20f59551e872
Jeremías <jeremias@talkingpts.org>
2023-06-12 16:33:22 -0300

    k8smon: mnppl tests setup

--- k8s/test/mon/munin-node/plugins/mnppl_test.py
+++ k8s/test/mon/munin-node/plugins/mnppl_test.py
@@ -4,14 +4,34 @@
 # See LICENSE file.
 
 import unittest
+from   unittest.mock import MagicMock, call
+
+import mon_t
 
 import mnppl
 
+_bup_print = mnppl._print
+
 class Test(unittest.TestCase):
+
+	def setUp(t):
+		mon_t.setUp()
+		mnppl._print = MagicMock()
 
 	def test_globals(t):
 		t.assertEqual(mnppl.plugins_bindir, '/usr/local/bin')
 		t.assertEqual(mnppl.plugins_suffix, '.mnppl')
+
+	#
+	# configs and reports
+	#
+
+	def test_print(t):
+		_bup_print('testing', '...')
+
+	#
+	# run parallel
+	#
 
 	def test_listPlugins(t):
 		t.assertListEqual(mnppl._listPlugins('./k8s/test/mon/munin-node/plugins/mnppl'), [
