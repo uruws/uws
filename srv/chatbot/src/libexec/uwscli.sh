22b1ceb021f94519d288d4a39f1d5d43b13da4f8
Jeremías <jeremias@talkingpts.org>
2023-06-15 15:40:46 -0300

    chatbot: uwscli ssh quiet mode

--- srv/chatbot/src/libexec/uwscli.sh
+++ srv/chatbot/src/libexec/uwscli.sh
@@ -4,7 +4,7 @@ hostname=${1:?'hostname?'}
 username=${2:?'username?'}
 shift
 shift
-exec /usr/bin/ssh -a -C -n -x \
+exec /usr/bin/ssh -q -a -C -n -x \
 	-F /etc/opt/uws/chatbot/ssh/config \
 	-i /etc/opt/uws/chatbot/ssh/ecdsa_id \
 	-l "${username}" "${hostname}" "$@"
