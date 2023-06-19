14e7041c81f81eaa4bac49756d8d6da1c8e8c864
Jeremías <jeremias@talkingpts.org>
2023-06-19 16:32:09 -0300

    meteor: revert deploy webcdn

--- pod/meteor/web/deploy.sh
+++ pod/meteor/web/deploy.sh
@@ -2,7 +2,7 @@
 set -eu
 appver=${1:-''}
 
-~/pod/meteor/web/cdn/deploy.sh "${appver}"
+# ~/pod/meteor/webcdn/deploy.sh "${appver}"
 
 ~/pod/meteor/web/configure.sh
 exec ~/pod/meteor/deploy.sh web web "${appver}"
--- pod/meteor/web/rollin.sh
+++ pod/meteor/web/rollin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 uwskube delete deploy meteor -n web
-~/pod/meteor/web/cdn/rollin.sh
+#~ ~/pod/meteor/webcdn/rollin.sh
 exit 0
