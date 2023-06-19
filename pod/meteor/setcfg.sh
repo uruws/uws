841ea634d42594db50a59bb8f759d06459b297da
Jeremías <jeremias@talkingpts.org>
2023-06-19 16:22:10 -0300

    meteor: deploy setcfg fix shellcheck

--- pod/meteor/setcfg.sh
+++ pod/meteor/setcfg.sh
@@ -3,28 +3,28 @@ set -eu
 
 ns=${1:?'app namespace?'}
 appver=${2:?'app version?'}
-envf=$(mktemp -p /tmp deploy-meteor-${ns}-env.XXXXXXXX)
+envf=$(mktemp -p /tmp "deploy-meteor-${ns}-env.XXXXXXXX")
 
 # deploy-meteor-env (used internally)
 
 uwskube delete configmap deploy-meteor-env -n "${ns}" || true
 
-echo "export AWS_REGION='${AWS_REGION}'" >${envf}
-echo "export METEOR_APP='${appver}'" >>${envf}
+echo "export AWS_REGION='${AWS_REGION}'" >"${envf}"
+echo "export METEOR_APP='${appver}'" >>"${envf}"
 
 uwskube create configmap deploy-meteor-env -n "${ns}" \
 	--from-file="deploy-env=${envf}"
-rm -f ${envf}
+rm -f "${envf}"
 
 # meteor-deploy-env (used for deploy)
 
 envf=$(mktemp -p /tmp "meteor-deploy-${ns}-env.XXXXXXXX")
-echo "UWS_APP_ENV=${APP_ENV}" >${envf}
+echo "UWS_APP_ENV=${APP_ENV}" >"${envf}"
 if test "X${APP_ENV}" = 'Xstaging'; then
-	echo "STAGING_APP_VERSION=${appver}" >>${envf}
+	echo "STAGING_APP_VERSION=${appver}" >>"${envf}"
 fi
 uwskube delete secret -n "${ns}" meteor-deploy-env || true
 uwskube create secret generic -n "${ns}" meteor-deploy-env --from-env-file="${envf}"
-rm -vf ${envf}
+rm -vf "${envf}"
 
 exit 0
