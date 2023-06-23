842a9008347d469a93eed5577349268d0fb1ebf6
Jeremías <jeremias@talkingpts.org>
2023-06-23 15:44:20 -0300

    docker: fix munin mailx deps

--- srv/munin-backend/build.sh
+++ srv/munin-backend/build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -eu
 # remove old versions
-docker rmi uws/munin-backend-2203 || true
-# uws/munin-backend-2211
-docker build --rm -t uws/munin-backend-2211 \
-	-f srv/munin-backend/Dockerfile.2211 \
-	./srv/munin-backend
+docker rmi uws/munin-backend-2211 || true
 # uws/munin-backend-2305
 docker build --rm -t uws/munin-backend-2305 \
 	-f srv/munin-backend/Dockerfile.2305 \
--- srv/munin/build.sh
+++ srv/munin/build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -eu
 # remove old versions
-docker rmi uws/munin-2203 || true
-# uws/munin-2211
-docker build --rm -t uws/munin-2211 \
-	-f srv/munin/Dockerfile.2211 \
-	./srv/munin
+docker rmi uws/munin-2211 || true
 # uws/munin-2305
 docker build --rm -t uws/munin-2305 \
 	-f srv/munin/Dockerfile.2305 \
