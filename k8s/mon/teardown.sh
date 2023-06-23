bdb55762b8c7af0ad874f08c09012f5b60b5e162
Jeremías <jeremias@talkingpts.org>
2023-06-23 17:37:08 -0300

    k8smon: no more efs volumes for munin

--- k8s/mon/teardown.sh
+++ k8s/mon/teardown.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 set -u
-
-echo 'WARNING: deleting mon namespace will destroy storage devices!!!'
-echo
-uwskube get pvc -n mon
-echo
-
-CONFIRM='N'
-read -r -p 'are you sure? [N/y]: ' CONFIRM
-
-if test 'Xy' = "X${CONFIRM}"; then
-	echo "delete mon namespace and all its resources..."
-	~/k8s/mon/munin/rollin.sh
-	~/k8s/mon/umount.sh
-	~/k8s/mon/rollin.sh --no-munin
-	~/k8s/mon/kubeshark/teardown.sh
-	~/k8s/mon/rmfs.sh
-	~/k8s/mon/munin/teardown.sh
-	~/k8s/mon/munin-node/teardown.sh
-	exec uwskube delete namespace mon
-else
-	echo 'abort!'
-	exit 1
-fi
+~/k8s/mon/rollin.sh
+~/k8s/mon/kubeshark/teardown.sh
+~/k8s/mon/munin/teardown.sh
+~/k8s/mon/munin-node/teardown.sh
+exec uwskube delete namespace mon
