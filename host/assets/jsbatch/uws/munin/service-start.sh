1dad473930c60689ebfd5bdf5ca5ef52b61bcc88
Jeremías <jeremias@talkingpts.org>
2023-06-23 15:48:53 -0300

    munin: jsbatch service mailx aws.ses config

--- host/assets/jsbatch/uws/munin/service-start.sh
+++ host/assets/jsbatch/uws/munin/service-start.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 set -eu
+
 CROND=/srv/uws/deploy/secret/eks/files/munin/cron.d
 CONFD=/srv/uws/deploy/secret/eks/files/munin/conf
+MAILX=/srv/uws/deploy/secret/eks/files/mailx/aws.ses
 
 HOSTIP=$(/uws/docker-hostip.sh)
 
 exec /usr/bin/docker run --rm -u root \
 	--name uws-munin-service \
 	--hostname munin.uws.local \
-	--add-host docker.uws.local:${HOSTIP} \
+	--add-host "docker.uws.local:${HOSTIP}" \
 	-v /srv/munin/var/lib:/var/lib/munin \
 	-v /srv/munin/var/alert:/var/opt/munin-alert \
 	-v /srv/munin/var/log:/var/log/munin \
 	-v /srv/munin/cache/www:/var/cache/munin/www \
 	-v /srv/etc/munin:/srv/etc/munin \
-	-v ${CROND}:/srv/etc/cron.d:ro \
-	-v ${CONFD}:/etc/uws/conf:ro \
+	-v "${CROND}:/srv/etc/cron.d:ro" \
+	-v "${CONFD}:/etc/uws/conf:ro" \
+	-v "${MAILX}:/srv/mailx/etc:ro" \
 	uws/munin-2305
