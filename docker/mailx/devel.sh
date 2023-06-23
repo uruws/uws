9cd10bd5ea2cd888f7240e461dd35fe52f5869dd
Jeremías <jeremias@talkingpts.org>
2023-06-22 22:33:47 -0300

    mailx: custom configs

--- docker/mailx/devel.sh
+++ docker/mailx/devel.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 set -eu
-CA=${PWD}/secret/ca/uws/smtps/230503
-mkdir -vp ${PWD}/tmp
+install -v -d -m 0750 ${PWD}/tmp/mailx
+chmod -v 0600 ${PWD}/secret/eks/files/aws.ses/msmtprc
 exec docker run -it --rm --name uws-mailx-devel \
 	--hostname mailx-devel.uws.local \
 	--read-only \
 	-v ${PWD}/secret/eks/files/aws.ses:/etc/opt/uws:ro \
 	-v ${PWD}/docker/mailx/utils:/usr/local/uws:ro \
-	-v ${PWD}/tmp:/home/uws/tmp \
-	-v ${CA}:/srv/etc/ca:ro \
+	-v ${PWD}/tmp/mailx:/home/uws/tmp \
 	-u uws \
 	-e USER=uws \
 	-e HOME=/home/uws \
