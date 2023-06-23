d3f2f2ec993e4778fa873e7ed9b3090929e7bda7
Jeremías <jeremias@talkingpts.org>
2023-06-23 15:37:59 -0300

    secrets: update mailx msmtprc file

--- eks/secrets/aws.ses-update.py
+++ eks/secrets/aws.ses-update.py
@@ -29,6 +29,20 @@ def shUpdate(fn, key, secret):
 			else:
 				fh.write('%s\n' % line)
 
+def msmtprcUpdate(key, secret):
+	f = Path('./secret/eks/files/mailx/aws.ses/msmtprc')
+	print(f)
+	content = f.read_text()
+	with f.open('w') as fh:
+		for line in content.splitlines():
+			if line.startswith('user AKIA'):
+				fh.write('user %s\n' % key)
+				fh.write('password %s\n' % secret)
+			elif line.startswith('password '):
+				continue
+			else:
+				fh.write('%s\n' % line)
+
 def main(argv):
 	flags = ArgumentParser(description = __doc__)
 	args = flags.parse_args(argv)
@@ -37,9 +51,16 @@ def main(argv):
 	except FileNotFoundError as err:
 		print(err)
 		return 1
+	# sh files
 	for fn in gitFiles('^UWS_SMTPS_USER='):
 		print(fn)
 		shUpdate(fn, key, secret)
+	# msmtprc
+	try:
+		msmtprcUpdate(key, secret)
+	except FileNotFoundError as err:
+		print(err)
+		return 2
 	return 0
 
 if __name__ == '__main__':
