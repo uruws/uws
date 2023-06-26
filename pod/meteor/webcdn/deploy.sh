c684504d36031e0b5a6e98fe0f33c456173b1eb9
Jeremías <jeremias@talkingpts.org>
2023-06-26 18:21:00 -0300

    meteor: webcdn config number of replicas

--- pod/meteor/webcdn/deploy.sh
+++ pod/meteor/webcdn/deploy.sh
@@ -2,6 +2,6 @@
 set -eu
 appver=${1:-''}
 ~/pod/meteor/webcdn/configure.sh
-export APP_REPLICAS=3
+export APP_REPLICAS=${APPCDN_REPLICAS}
 ~/pod/meteor/deploy.sh webcdn web "${appver}"
 exit 0
--- pod/meteor/webcdn/gw/rollin.sh
+++ pod/meteor/webcdn/gw/rollin.sh
@@ -4,6 +4,6 @@ ns=webcdn
 export METEOR_NAMESPACE=${ns}
 export METEOR_TLS=${APPCDN_TLS}
 export METEOR_HOST=${APPCDN_HOSTNAME}
-export NGINX_REPLICAS=1
+export NGINX_REPLICAS=${APPCDN_NGINX_REPLICAS}
 export METEOR_NGINX_CONF=${HOME}/pod/meteor/webcdn/nginx.conf
 exec ~/pod/meteor/gw/rollin.sh "${ns}"
