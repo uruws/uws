b6aa715192dfe1908e0295992af434cb6ef287b9
Jeremías <jeremias@talkingpts.org>
2023-07-03 18:16:19 -0300

    docker: webapp uwsgi no reload workers

---
+++ docker/webapp/devel-login.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+
+tmpdir="${PWD}/tmp/webapp"
+install -v -d -m 0750 "${tmpdir}"
+
+exec docker run -it --rm --read-only \
+	--name uws-webapp-devel \
+	--hostname webapp-devel.uws.local \
+	-p 127.0.0.1:0:2741 \
+	-v "${PWD}/docker/webapp/utils:/opt/webapp/utils:ro" \
+	-v "${tmpdir}:/home/uws/tmp" \
+	-u uws \
+	-e USER=uws \
+	-e HOME=/home/uws \
+	-e UWS_WEBAPP=devel \
+	--tmpfs /tmp \
+	--workdir /home/uws \
+	--entrypoint /usr/local/bin/uws-login.sh \
+	"uws/webapp-2305"
--- docker/webapp/utils/entrypoint.sh
+++ docker/webapp/utils/entrypoint.sh
@@ -2,31 +2,30 @@
 set -eu
 
 echo "*** webapp: ${UWS_WEBAPP}"
+echo "***   port: ${UWS_WEBAPP_PORT}"
 
-exec uwsgi \
-	--master \
-	--no-orphans \
-	--reload-on-exception \
-	--vacuum \
-	--enable-threads \
-	--thunder-lock \
-	--need-plugin           python3 \
-	--max-apps              1 \
-	--reload-mercy          30 \
-	--worker-reload-mercy   10 \
-	--max-requests          5000 \
-	--min-worker-lifetime   180 \
-	--max-worker-lifetime   28800 \
-	--evil-reload-on-rss    1024 \
-	--module                "${UWS_WEBAPP}_wsgi:application" \
-	--env                   LANG=en_US.UTF-8 \
-	--env                   LANGUAGE=en_US.UTF-8 \
-	--env                   LC_ALL=en_US.UTF-8 \
-	--env                   LC_CTYPE=UTF-8 \
-	--env                   PYTHONUTF8=1 \
-	--env                   PYTHONIOENCODING=utf-8 \
-	--env                   "PYTHONPATH=/etc/opt/uws/${UWS_WEBAPP}" \
-	--workers               "${UWS_WEBAPP_WORKERS}" \
-	--http11-socket         "0.0.0.0:${UWS_WEBAPP_PORT}" \
-	--chdir                 "/opt/uws/${UWS_WEBAPP}" \
-	--venv                  /opt/uws/venv
+exec uwsgi                                                        \
+	--master                                                      \
+	--no-orphans                                                  \
+	--die-on-term                                                 \
+	--exit-on-reload                                              \
+	--reload-on-exception                                         \
+	--vacuum                                                      \
+	--enable-threads                                              \
+	--thunder-lock                                                \
+	--need-plugin         python3                                 \
+	--max-apps            1                                       \
+	--worker-reload-mercy 5                                       \
+	--min-worker-lifetime 3                                       \
+	--workers             "${UWS_WEBAPP_WORKERS}"                 \
+	--module              "${UWS_WEBAPP}_wsgi:application"        \
+	--env                 LANG=en_US.UTF-8                        \
+	--env                 LANGUAGE=en_US.UTF-8                    \
+	--env                 LC_ALL=en_US.UTF-8                      \
+	--env                 LC_CTYPE=UTF-8                          \
+	--env                 PYTHONUTF8=1                            \
+	--env                 PYTHONIOENCODING=utf-8                  \
+	--env                 "PYTHONPATH=/etc/opt/uws/${UWS_WEBAPP}" \
+	--http11-socket       "0.0.0.0:${UWS_WEBAPP_PORT}"            \
+	--chdir               "/opt/uws/${UWS_WEBAPP}"                \
+	--venv                /opt/uws/venv
