1a743b9154840ede7f45a51cc29a79f9d592bbc7
Jeremías <jeremias@talkingpts.org>
2023-07-10 15:50:16 -0300

    docker: fix mkcert upgrade

--- docker/mkcert/build.sh
+++ docker/mkcert/build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -eu
 # remove old versions
-docker rmi uws/mkcert-2203 || true
-# uws/mkcert-2211
-docker build --rm -t uws/mkcert-2211 \
-	-f docker/mkcert/Dockerfile.2211 \
-	./docker/mkcert
+docker rmi uws/mkcert-2211 || true
 # uws/mkcert-2305
 docker build --rm -t uws/mkcert-2305 \
 	-f docker/mkcert/Dockerfile.2305 \
