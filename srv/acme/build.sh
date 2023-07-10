1233c2c4d0623197baf5b148b19819d5199fa52c
Jeremías <jeremias@talkingpts.org>
2023-07-10 16:03:22 -0300

    docker: fix acme upgrade

--- srv/acme/build.sh
+++ srv/acme/build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -eu
 # remove old versions
-docker rmi uws/acme-2203 || true
-# uws/acme-2211
-docker build --rm -t uws/acme-2211 \
-	-f srv/acme/Dockerfile.2211 \
-	./srv/acme
+docker rmi uws/acme-2211 || true
 # uws/acme-2305
 docker build --rm -t uws/acme-2305 \
 	-f srv/acme/Dockerfile.2305 \
