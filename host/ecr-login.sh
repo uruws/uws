ad84a681c374eebea7febd7eac9280b17f79f8c0
Jeremías <jeremias@talkingpts.org>
2023-07-11 16:49:38 -0300

    fix aws ecr login

--- host/ecr-login.sh
+++ host/ecr-login.sh
@@ -4,7 +4,7 @@ set -eu
 AWS_REGION=${1:-'us-east-1'}
 
 echo "i - ECR login aws region: ${AWS_REGION}"
-aws ecr get-login-password --region ${AWS_REGION} |
-	docker login --username AWS --password-stdin 789470191893.dkr.ecr.${region}.amazonaws.com
+aws ecr get-login-password --region "${AWS_REGION}" |
+	docker login --username AWS --password-stdin "789470191893.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
 exit 0
