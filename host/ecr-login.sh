77733ca6c491540cc4a2c8eb27573a39a127a027
Jeremías <jeremias@talkingpts.org>
2023-07-11 11:18:31 -0300

    aws: fix ecr login

--- host/ecr-login.sh
+++ host/ecr-login.sh
@@ -3,14 +3,8 @@ set -eu
 
 AWS_REGION=${1:-'us-east-1'}
 
-rm -vf /var/tmp/uws-docker-login.*
-tmpfn=$(mktemp /var/tmp/uws-docker-login.XXXXXXXX)
-
 echo "i - ECR login aws region: ${AWS_REGION}"
-aws ecr get-login --region ${AWS_REGION} | sed 's/-e none //' >${tmpfn}
-
-chmod -v 0755 ${tmpfn}
-/bin/sh ${tmpfn}
-rm -vf ${tmpfn}
+aws ecr get-login-password --region ${AWS_REGION} |
+	docker login --username AWS --password-stdin 789470191893.dkr.ecr.${region}.amazonaws.com
 
 exit 0
