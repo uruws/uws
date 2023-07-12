050a718c185faadad7808efdb0bc5f3ba5c6dd7b
Jeremías <jeremias@talkingpts.org>
2023-07-12 13:15:16 -0300

    ab: htmlcov publish

--- srv/ab/publish.sh
+++ srv/ab/publish.sh
@@ -6,4 +6,12 @@ ABENCH_TAG=$(cat ./srv/ab/VERSION)
 ./host/ecr-login.sh us-east-1
 ./cluster/ecr-push.sh us-east-1 uws/ab-2305 "uws:ab-${ABENCH_TAG}"
 
+if test -d /srv/www/ssl/htmlcov; then
+	if test -d ./tmp/ab/htmlcov; then
+		umask 0022
+		cp -r ./tmp/ab/htmlcov /srv/www/ssl/htmlcov/ab
+		echo '/srv/www/ssl/htmlcov/ab done!'
+	fi
+fi
+
 exit 0
