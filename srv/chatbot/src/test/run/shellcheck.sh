6d7ffd00b86a0a5f7c9242bedc9ba19795350007
Jeremías <jeremias@talkingpts.org>
2023-07-11 23:58:14 -0300

    chatbot: use common test utils

--- srv/chatbot/src/test/run/shellcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-webapp=${UWS_WEBAPP}
-find "/opt/uws/${webapp}" -type f -name '*.sh' -print0 |
-	xargs --null -- \
-	shellcheck --check-sourced --color=auto --norc --severity=warning
-exit 0
--- srv/chatbot/src/test/run/typecheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-webapp=${UWS_WEBAPP}
-PYTHONPATH=/opt/uws/${webapp}:/etc/opt/uws/${webapp}:/opt/uws/${webapp}/test
-export PYTHONPATH
-exec /opt/uws/venv/bin/python3 -m mypy "/opt/uws/${webapp}" "/etc/opt/uws/${webapp}"
