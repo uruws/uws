1f3c67a559241ff03e59f4eb33c488f9fe39bf73
Jeremías <jeremias@talkingpts.org>
2023-07-13 15:06:46 -0300

    secrets: tapoS3 update srmnt settings

--- eks/secrets/tapoS3-all.sh
+++ eks/secrets/tapoS3-all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
+./eks/secrets/tapoS3-update.py Test ./secret/eks/files/meteor/app/srmnt-settings.json
 ./eks/secrets/tapoS3-update.py Test ./secret/eks/files/meteor/app/staging-settings.json
 ./eks/secrets/tapoS3-update.py Live ./secret/eks/files/meteor/app/prod-settings.json
 exit 0
