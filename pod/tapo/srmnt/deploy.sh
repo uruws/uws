f3dde14181bc043a215837f5a46c8efe42bd2ada
Jeremías <jeremias@talkingpts.org>
2023-07-27 13:38:14 -0300

    tapo: sarmiento replicas

--- pod/tapo/srmnt/deploy.sh
+++ pod/tapo/srmnt/deploy.sh
@@ -7,6 +7,9 @@ set -x
 
 export TAPO_ENV="${SRMNT_ENV}"
 export TAPO_NAMESPACE="${SRMNT_NAMESPACE}"
+export TAPO_REPLICAS="${SRMNT_WEB_REPLICAS}"
+export TAPO_CDN_REPLICAS="${SRMNT_CDN_REPLICAS}"
+export TAPO_API_REPLICAS="${SRMNT_API_REPLICAS}"
 
 ~/pod/tapo/api/deploy.sh "${appver}"
 ~/pod/tapo/web/deploy.sh "${appver}"
