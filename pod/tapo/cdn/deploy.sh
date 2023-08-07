d91d59dc80c2765bf1e48bf4f549552e81c565a9
Jeremías <jeremias@talkingpts.org>
2023-08-07 18:29:42 -0300

    tapo: cdn resources

--- pod/tapo/cdn/deploy.sh
+++ pod/tapo/cdn/deploy.sh
@@ -6,10 +6,10 @@ appver=${1:?'app version?'}
 METEOR_REPLICAS="${TAPO_CDN_REPLICAS}"
 export METEOR_REPLICAS
 
-METEOR_CPU="${TAPO_CPU}"
+METEOR_CPU="${TAPO_CDN_CPU}"
 export METEOR_CPU
 
-METEOR_MEMORY="${TAPO_MEMORY}"
+METEOR_MEMORY="${TAPO_CDN_MEMORY}"
 export METEOR_MEMORY
 
 exec ~/pod/tapo/deploy.sh "${TAPO_NAMESPACE}" cdn "${appver}"
