defc8bdd74c52cd7d8f049483ec308a2aa2eca82
Jeremías <jeremias@talkingpts.org>
2023-08-07 13:08:34 -0300

    cluster: fix k8s version and tag settings

--- cluster/new.sh
+++ cluster/new.sh
@@ -13,32 +13,44 @@ eksenv=${PWD}/eks/env/${cluster}.env
 secret=${PWD}/secret/eks/files
 sshdir=${secret}/ssh/${cluster}
 
-install -v -d -m 0750 ${admin_dir}
-install -v -d -m 0750 ${client_dir}
-install -v -d -m 0750 ${cluster_dir}
-install -v -d -m 0750 ${kubedir}
-install -v -d -m 0750 ${sshdir}
+install -v -d -m 0750 "${admin_dir}"
+install -v -d -m 0750 "${client_dir}"
+install -v -d -m 0750 "${cluster_dir}"
+install -v -d -m 0750 "${kubedir}"
+install -v -d -m 0750 "${sshdir}"
 
-touch ${admin_dir}/config
-touch ${client_dir}/config
+touch "${admin_dir}/config"
+touch "${client_dir}/config"
 
-echo '[default]'           >${admin_dir}/config
-echo "region = ${region}" >>${admin_dir}/config
-echo ''                   >>${admin_dir}/config
-echo "[${prof}]"          >>${admin_dir}/config
-echo "region = ${region}" >>${admin_dir}/config
+{
+	echo '[default]'
+	echo "region = ${region}"
+	echo ''
+	echo "[${prof}]"
+	echo "region = ${region}"
+} >"${admin_dir}/config"
 
-cat ${admin_dir}/config >${client_dir}/config
+cat "${admin_dir}/config" >"${client_dir}/config"
 
-echo "UWS_CLUSTER=${cluster}"                      >${eksenv}
-echo "CLUSTER_HOST=${cluster}"                    >>${eksenv}
-echo "AWS_PROFILE=${prof}"                        >>${eksenv}
-echo "AWS_REGION=${region}"                       >>${eksenv}
-echo "AWS_ZONES=${region}a,${region}b,${region}c" >>${eksenv}
+k8s_version=$(cat "${PWD}/docker/k8s/VERSION")
+k8s_tag=$(cat "${PWD}/docker/k8s/TAG")
+
+{
+	echo "UWS_CLUSTER=${cluster}"
+	echo "CLUSTER_HOST=${cluster}"
+	echo ''
+	echo "AWS_PROFILE=${prof}"
+	echo "AWS_REGION=${region}"
+	echo "AWS_ZONES=${region}a,${region}b,${region}c"
+	echo ''
+	echo "K8S_VERSION=${k8s_version}"
+	echo "K8S_IMAGE=k8s-${k8s_tag}"
+	echo "EKS_IMAGE=eks-${k8s_tag}"
+} >"${eksenv}"
 
 ./secret/eks/files/ssh/gen.sh "${cluster}"
 
-touch ${admin_dir}/credentials
-touch ${client_dir}/credentials
+touch "${admin_dir}/credentials"
+touch "${client_dir}/credentials"
 
 exec ./eks/secrets/cluster.py --profile "${prof}" "${cluster}"
