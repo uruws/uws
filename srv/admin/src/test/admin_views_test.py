4771b774edfdb5399ae11c70cdfb8586e305b80c
Jeremías <jeremias@talkingpts.org>
2023-08-10 20:26:01 -0300

    admin: test home view

--- srv/admin/src/test/admin_views_test.py
+++ srv/admin/src/test/admin_views_test.py
@@ -22,5 +22,13 @@ class TestViews(unittest.TestCase):
 	def test_healthz(t):
 		t.assertEqual(admin_views.healthz(), 'ok')
 
+	#---------------------------------------------------------------------------
+	# /
+
+	def test_home(t):
+		with wapp_t.mock() as m:
+			admin_views.home()
+			m.template.assert_called_once_with('admin/home.html')
+
 if __name__ == '__main__':
 	unittest.main()
