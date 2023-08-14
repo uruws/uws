80b699b1bda6d40244519df1e515e769635c2eaf
Jeremías <jeremias@talkingpts.org>
2023-08-14 11:26:00 -0300

    admin: navbar layouts

--- srv/admin/src/admintpl.py
+++ srv/admin/src/admintpl.py
@@ -8,7 +8,7 @@ from wapptpl import url
 
 log = wapp.getLogger(__name__)
 
-button_class   = 'w3-button w3-border'
+button_class   = 'w3-button w3-margin-top w3-margin-bottom w3-border'
 button_color   = 'w3-border-gray'
 button_current = 'w3-border-blue w3-text-green'
 
