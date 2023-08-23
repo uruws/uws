00aa3ddbb49a39224668612cd1703d1731d2f74e
Jeremías <jeremias@talkingpts.org>
2023-08-23 18:37:08 -0300

    tapo: fix sarmiento namespaces

--- pod/tapo/setup.sh
+++ pod/tapo/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 uwskube create namespace "${TAPO_NAMESPACE}"
-uwskube create namespace "${TAPO_API_NAMESPACE}"
-uwskube create namespace "${TAPO_CDN_NAMESPACE}"
+uwskube create namespace "${TAPO_API_NAMESPACE}" || true
+uwskube create namespace "${TAPO_CDN_NAMESPACE}" || true
 if test 'X' != "X${TAPO_WORKER_NAMESPACE}"; then
 	uwskube create namespace "${TAPO_WORKER_NAMESPACE}"
 fi
