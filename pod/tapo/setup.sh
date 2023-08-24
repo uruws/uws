453f3669de24d49dbd73731f76cd97031f2c68a2
Jeremías <jeremias@talkingpts.org>
2023-08-24 14:00:31 -0300

    tapo: fix setup/teardown namespaces

--- pod/tapo/setup.sh
+++ pod/tapo/setup.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 set -eu
+
 uwskube create namespace "${TAPO_NAMESPACE}"
-uwskube create namespace "${TAPO_API_NAMESPACE}" || true
-uwskube create namespace "${TAPO_CDN_NAMESPACE}" || true
+
+if test "X${TAPO_API_NAMESPACE}" != "X${TAPO_NAMESPACE}"; then
+	uwskube create namespace "${TAPO_API_NAMESPACE}"
+fi
+
+if test "X${TAPO_CDN_NAMESPACE}" != "X${TAPO_NAMESPACE}"; then
+	uwskube create namespace "${TAPO_CDN_NAMESPACE}"
+fi
+
 if test 'X' != "X${TAPO_WORKER_NAMESPACE}"; then
 	uwskube create namespace "${TAPO_WORKER_NAMESPACE}"
 fi
+
 exit 0
--- pod/tapo/teardown.sh
+++ pod/tapo/teardown.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 set -u
+
 if test 'X' != "X${TAPO_WORKER_NAMESPACE}"; then
 	uwskube delete namespace "${TAPO_WORKER_NAMESPACE}"
 fi
-uwskube delete namespace "${TAPO_CDN_NAMESPACE}"
-uwskube delete namespace "${TAPO_API_NAMESPACE}"
+
+if test "X${TAPO_CDN_NAMESPACE}" != "X${TAPO_NAMESPACE}"; then
+	uwskube delete namespace "${TAPO_CDN_NAMESPACE}"
+fi
+
+if test "X${TAPO_API_NAMESPACE}" != "X${TAPO_NAMESPACE}"; then
+	uwskube delete namespace "${TAPO_API_NAMESPACE}"
+fi
+
 uwskube delete namespace "${TAPO_NAMESPACE}"
+
 exit 0
