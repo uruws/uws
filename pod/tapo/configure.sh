2b2a489c44d09c818e8f87eaec2e0441e9a2eff9
Jeremías <jeremias@talkingpts.org>
2023-08-29 15:56:12 -0300

    tapo: fix env vars shell syntax

--- pod/tapo/configure.sh
+++ pod/tapo/configure.sh
@@ -7,52 +7,57 @@ appver=${3:-NO_VERSION}
 
 appenv=${HOME}/secret/meteor/app/${TAPO_ENV}.env
 
-# remove old app-env
-uwskube delete secret app-env -n "${ns}" || true
-
-# create an empty one for compatibility with the buildpack
-uwskube delete secret empty-app-env -n "${ns}" || true
-uwskube create secret generic empty-app-env -n "${ns}" \
-	--from-file="app.env=${HOME}/secret/meteor/app/empty.env"
-
-uwskube delete secret "meteor-${app}-env" -n "${ns}" || true
-
 if test 'XNO_VERSION' = "X${appver}"; then
 	appver=$(~/pod/tapo/deploy-getver.sh "${ns}" "${app}")
 fi
 
+# remove old
+uwskube delete secret app-env -n "${ns}" || true
+uwskube delete secret empty-app-env -n "${ns}" || true
+
+#-------------------------------------------------------------------------------
+# meteor env
+
 if test -s "${appenv}"; then
-	appset=${HOME}/secret/meteor/app/${TAPO_ENV}-settings.json
-
 	echo "app.env: ${appenv}"
-	echo "app-settings.json: ${appset}"
-
-	envfn=$(mktemp -p /tmp "configure-${ns}-${app}-env.XXXXXXXXXX")
-
-	cat "${appenv}" >"${envfn}"
-
-	printf '%s' 'METEOR_SETTINGS=' >>"${envfn}"
-	python3 -m json.tool --compact "${appset}" >>"${envfn}"
-
-	if test 'Xworker' != "X${app}"; then
-		echo 'DISABLE_JOBS=TRUE' >>"${envfn}"
-	fi
-
-	if test 'Xstaging' = "X${TAPO_ENV}"; then
-		echo "STAGING_APP_VERSION=${appver}" >>"${envfn}"
-	fi
-	if test 'Xsrmnt' = "X${TAPO_ENV}"; then
-		echo "STAGING_APP_VERSION=${appver}" >>"${envfn}"
-	fi
-	if test 'Xsrmnt-ngx' = "X${TAPO_ENV}"; then
-		echo "STAGING_APP_VERSION=${appver}" >>"${envfn}"
-	fi
-
-	uwskube create secret generic "meteor-${app}-env" -n "${ns}" --from-env-file="${envfn}"
-	rm -vf "${envfn}"
+	uwskube delete secret "meteor-${app}-env" -n "${ns}" || true
+	uwskube create secret generic "meteor-${app}-env" -n "${ns}" \
+		--from-file="${appenv}"
 else
-	echo "[ERROR] ${appenv}: file not found!" >&2
+	echo "[ERROR] ${appenv}: file not found or empty!" >&2
 	exit 1
 fi
+
+#-------------------------------------------------------------------------------
+# deploy env
+
+uwskube delete secret "deploy-${app}-env" -n "${ns}" || true
+
+appset=${HOME}/secret/meteor/app/${TAPO_ENV}-settings.json
+
+echo "app-settings.json: ${appset}"
+
+envfn=$(mktemp -p /tmp "configure-${ns}-${app}-env.XXXXXXXXXX")
+
+printf '%s' 'METEOR_SETTINGS=' >"${envfn}"
+python3 -m json.tool --compact "${appset}" >>"${envfn}"
+
+if test 'Xworker' != "X${app}"; then
+	echo 'DISABLE_JOBS=TRUE' >>"${envfn}"
+fi
+
+if test 'Xstaging' = "X${TAPO_ENV}"; then
+	echo "STAGING_APP_VERSION=${appver}" >>"${envfn}"
+fi
+if test 'Xsrmnt' = "X${TAPO_ENV}"; then
+	echo "STAGING_APP_VERSION=${appver}" >>"${envfn}"
+fi
+if test 'Xsrmnt-ngx' = "X${TAPO_ENV}"; then
+	echo "STAGING_APP_VERSION=${appver}" >>"${envfn}"
+fi
+
+uwskube create secret generic "deploy-${app}-env" -n "${ns}" \
+	--from-env-file="${envfn}"
+rm -vf "${envfn}"
 
 exit 0
