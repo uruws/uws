1da748b74dc354d3215edc081f79cb2cee38e6d7
Jeremías <jeremias@talkingpts.org>
2023-09-06 19:06:19 -0300

    tapo3: service setup/teardown

--- pod/lib/tapo3/setup.sh
+++ pod/lib/tapo3/setup.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 set -eu
-
-uwskube create namespace "${TAPO3_NAMESPACE}"
-
-if test 'X' != "X${TAPO3_API_NAMESPACE}"; then
-	uwskube create namespace "${TAPO3_API_NAMESPACE}"
-fi
-
-if test 'X' != "X${TAPO3_CDN_NAMESPACE}"; then
-	uwskube create namespace "${TAPO3_CDN_NAMESPACE}"
-fi
-
-if test 'X' != "X${TAPO3_WORKER_NAMESPACE}"; then
-	uwskube create namespace "${TAPO3_WORKER_NAMESPACE}"
-fi
-
+ns=${1:?'namespace?'}
+uwskube create namespace "${ns}"
+~/k8s/nginx/setup-ca.sh "${ns}"
 exit 0
---
+++ pod/tapo3/api/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -eu
+exec ~/pod/lib/tapo3/setup.sh "${TAPO3_API_NAMESPACE}"
