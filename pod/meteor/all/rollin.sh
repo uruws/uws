fb2be01e628f60782e4ff1e7eeb45f8aaa4077c8
Jeremías <jeremias@talkingpts.org>
2023-09-12 16:17:06 -0300

    tapo: meteor/all helper scripts

---
+++ pod/meteor/all/rollin.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -u
+~/pod/meteor/api/rollin.sh
+~/pod/meteor/web/rollin.sh
+exit 0
---
+++ pod/meteor/all/top.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -u
+~/pod/meteor/api/top.sh
+~/pod/meteor/webcdn/top.sh
+~/pod/meteor/web/top.sh
+exit 0
